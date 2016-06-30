9534c6e66ccc745aa5dccf3decb36823cfff9ebe
therealsamf <srf.000@hotmail.com>
2016-06-30 13:49:19 -0400

    Finished initial implementation of TranslationWords

--- src/js/index.js
+++ src/js/index.js
@@ -2,15 +2,34 @@
 (function() {
   const ReactDOM = require('react-dom');
   const React = require('react');
+  const Well = require('react-bootstrap/lib/Well.js');
+
 // var db = require('./db-init');
   const remote = window.electron.remote;
   const {Menu} = remote;
   const LexicalChecker = require('./modules/lexical_checker/LexicalChecker');
   const TranslationNotesComponent = require('./modules/translation_notes/TranslationNotesComponent');
+  const TranslationWordsDisplay = require('./modules/translation_words/TranslationWordsDisplay');
+
+  var Lexical = React.createClass({
+  	render: function() {
+  		return (
+  			<Well
+  				style={{
+  					maxWidth: "50%"
+  				}}
+  			>
+  				<TranslationWordsDisplay />
+  				<LexicalChecker />
+  			</Well>
+  		);
+  	}
+  })
 
   var App = {
     init: function() {
-    	ReactDOM.render(<TranslationNotesComponent />,
+    	ReactDOM.render(
+    		<Lexical />,
     		document.getElementById('content'));
     }
   };
--- src/js/modules/translation_words/TranslationWordsDisplay.js
+++ src/js/modules/translation_words/TranslationWordsDisplay.js
@@ -3,11 +3,23 @@
 
 //Node Modules
 var React = require('react');
-var Remarkable = require('remarkable');
-var md = new Remarkable();
+var Markdown = require('react-remarkable');
+
+//Bootstrap
+var FormGroup = require('react-bootstrap/lib/FormGroup.js');
+var Well = require('react-bootstrap/lib/Well.js');
+var ControlLabel = require('react-bootstrap/lib/ControlLabel.js');
+var FormControl = require('react-bootstrap/lib/FormControl.js');
+
 
 //Components
 var TranslationWordsScraper = require('./TranslationWordsScraper');
+
+
+//english constants
+var DEFAULT = 'translationWords',
+	WORD_QUERY = 'Type in word from translationWords',
+	DEFAULT_PLACEHOLDER = 'ex: aaron'
 
 var TranslationWordsDisplay = React.createClass({
 
@@ -15,22 +27,33 @@ var TranslationWordsDisplay = React.createClass({
 
 	wordList: null,
 
+	currentMarkdown: null,
+
 	getInitialState: function() {
 	    return {
-	    	wordListEmpty: true
+	    	wordListEmpty: true,
+	    	currentWord: null,
+	    	dropdownPushed: false,
+	    	placeHolder: DEFAULT_PLACEHOLDER,
+	    	value: '',
+	    	markdownToggle: false
 	    };
 	},
 
 	componentWillMount: function() {
-	      this.tWHtmlScraper = new TranslationWordsScraper();
-	      this.tWHtmlScraper.getWordList(null, //link
+		var _this = this;
+	    this.tWHtmlScraper = new TranslationWordsScraper();
+	    
+	    this.tWHtmlScraper.getWordList(
+	    	null, //link
+
 			function(data) { //assignCallback
-				console.log('Done');
 				_this.wordList = data;
 				_this.setState({
 					wordListEmpty: false
 				});
-			}, 
+				console.log('done');
+			},
 
 			function() { //errorCallback
 				console.error('Words Scraper failed');
@@ -39,11 +62,100 @@ var TranslationWordsDisplay = React.createClass({
 	},
 
 	render: function() {
-		<span>
+		var _this = this;
+		return (
 			<Well>
-				<div>
+				<div
+					style={{
+						textAlign: "center"
+					}}
+				>
+					<form
+						onSubmit={function(e) {
+							e.preventDefault();
+							_this.displayWord(_this.state.value)
+							
+						}}
+		    		>
+						<FormGroup
+		    				validationState={this.getValidationState()}
+						>
+							<ControlLabel>{WORD_QUERY}</ControlLabel>
+							<FormControl
+		    					type="text"
+		    					value={this.state.value}
+		    					placeholder={this.state.placeHolder}
+		    					onChange={this.handleChange}
+		    					disabled={this.state.disabled}
+							/>
+							<FormControl.Feedback />
+						</FormGroup>
+					</form>
+				</div>
+				<br />
+				<div
+					style={{
+						minHeight: "200px",
+						maxHeight: "300px",
+						overflowY: "scroll"
+					}}
+				>
+					{this.currentMarkdown}
+
 				</div>
 			</Well>
-		</span>
+		);
+	},
+
+	displayWord: function(word) {
+		this.setState({
+			currentWord: word
+		});
+		var rawMarkdown = null;
+		var _this = this;
+		this.tWHtmlScraper.getWord(word + '.md',
+			function(file) { //assign callback
+				rawMarkdown = file;
+				_this.setCurrentMarkdown(
+					<Markdown
+						source={rawMarkdown}
+					/>
+				);
+			}
+		);
+	},
+
+
+	setCurrentMarkdown: function(markdownComponent) {
+		this.currentMarkdown = markdownComponent;
+		this.setState({
+			markdownToggle: !this.state.markdownToggle
+		});
+	},
+
+	handleChange: function(e) {
+		this.setState({
+			value: e.target.value
+		});
+	},
+
+	changePlaceHolder: function(value) {
+		this.setState({
+			placeHolder: value
+		});
+	},
+
+	getValidationState: function() {
+		if (this.state.wordListEmpty) {
+			return 'warning';
+		}
+		if (this.state.value + '.md' in this.wordList) {
+			return 'success';
+		}
+		return 'error';
 	}
+
 });
+
+
+module.exports = TranslationWordsDisplay;
