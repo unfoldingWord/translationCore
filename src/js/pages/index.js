68fa54428c1cc2ef987d01af2cf526aa74cfcb38
mannycolon <colonmanuel7@gmail.com>
2016-07-06 09:28:56 -0400

    added Loginmodal to index.js

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -9,12 +9,15 @@
 // var db = require('./db-init');
   const UploadModal = require('../components/UploadModal');
   const MenuBar = require('../components/MenuBar');
+  const LoginModal = require('../components/LoginModal');
+
   var App = {
     init: function() {
       var menu = Menu.buildFromTemplate(MenuBar.template);
       Menu.setApplicationMenu(menu);
       var Application = (
         <div>
+        <LoginModal />
           <TPane />
           <UploadModal />
         </div>
