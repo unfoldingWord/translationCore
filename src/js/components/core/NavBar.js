244433383d72eb0cc652eee266f7a4cc3552b2ed
mannycolon <colonmanuel7@gmail.com>
2016-07-11 13:54:36 -0400

    meged with develop and fix directory errors

---
+++ src/js/components/core/NavBar.js
@@ -0,0 +1,31 @@
+const React = require('react');
+
+const Navbar = require('react-bootstrap/lib/Navbar');
+const NavItem = require('react-bootstrap/lib/NavItem');
+const Nav = require('react-bootstrap/lib/Nav');
+const Toggle= require('./Toggle');
+const style = require('../../styles/loginStyle');
+
+class NavBar extends React.Component{
+  render(){
+    return(
+      <div>
+      <Navbar inverse>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <img src="../8woc/images/TC_Icon.png" style={style.logo}/>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+          <Toggle />
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      </div>
+    );
+  }
+}
+
+
+module.exports = NavBar;
