72736af7eeb4c3260a4ccaba3fc7764c950d6817
Nate Ruiz <nateruiz1997@yahoo.com>
2016-07-20 14:09:18 -0400

    Initial commit for nr-fix-boxes

--- modules/t_pane/Pane.js
+++ modules/t_pane/Pane.js
@@ -19,7 +19,7 @@ class Pane extends React.Component {
     }
     render() {
         return (
-            <Col md={4} sm={4} xs={12}>
+            <Col md={3} sm={3} xs={12} lg={3}>
                 <h3 style={style.pane.header}>{this.props.title}</h3>
                 <Well style={style.pane.content}>
                     <Book input={this.props.content} />
