37ded00ef025e2d5497d569534448cecbe336202
mannycolon <colonmanuel7@gmail.com>
2016-07-22 10:52:08 -0400

    fixed the onlinestatus component: this component will display a green online circle if internet connection is found otherwise red offline circle will display

--- src/js/components/core/onlineStatus.js
+++ src/js/components/core/onlineStatus.js
@@ -1,13 +1,29 @@
 const React = require('react');
 const Button = require('react-bootstrap/lib/Button');
+const style = require('../../styles/loginStyle');
 
 class OnlineStatus extends React.Component{
+  constructor(){
+    super();
+    this.state ={
+      online: window.navigator.onLine
+    };
+  }
+  componentWillMount(){
+    window.addEventListener("offline", () => {this.setState({online: false});});
+    window.addEventListener("online", () => {this.setState({online: true});});
+  }
+  componentWillUnmount(){
+    window.removeEventListener("offline", () => {this.setState({online: false});});
+    window.removeEventListener("online", () => {this.setState({online: true});});
+  }
   render(){
-    /*const text = this.state.online ? 'Online' : 'Offline';
-    const statusColor = this.state.online ? 'success' : 'danger';*/
+    const text = this.state.online ? 'Online' : 'Offline';
+    const statusColor = this.state.online ? style.online : style.offline;
+
     return(
       <div>
-          <Button bsStyle="success">Hello</Button>
+          <div style={statusColor}></div>
       </div>
       );
   }
