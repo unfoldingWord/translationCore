3fbfbd6515d160bb184e0ccdc5547ce0c49f2d4e
mannycolon <colonmanuel7@gmail.com>
2016-07-22 09:45:14 -0400

    fixing OnlineStatus component

--- src/js/components/core/onlineStatus.js
+++ src/js/components/core/onlineStatus.js
@@ -1,13 +1,25 @@
 const React = require('react');
 const Button = require('react-bootstrap/lib/Button');
 
+
+
 class OnlineStatus extends React.Component{
+  constructor(){
+    super();
+    this.state ={
+      online: false;
+    };
+  }
+  componentWillMount(){
+    window.addEventListener("offline", function() {this.setState({online: false});});
+    window.addEventListener("online", function() {this.setState({online: true});});
+  }
   render(){
-    /*const text = this.state.online ? 'Online' : 'Offline';
-    const statusColor = this.state.online ? 'success' : 'danger';*/
+    const text = this.state.online ? 'Online' : 'Offline';
+    const statusColor = this.state.online ? 'success' : 'danger';
     return(
       <div>
-          <Button bsStyle="success">Hello</Button>
+          <Button bsStyle={statusColor} style={{borderRadius: '3%'}}>{text}</Button>
       </div>
       );
   }
