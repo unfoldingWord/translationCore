e5934907d99ffbdbe9293b515750a3b16f1fa4bb
Evan Wiederspan <evan@wiederspan.org>
2016-07-26 10:16:11 -0400

    Very basic report generation and intial API documentation

--- modules/lexical_check_module/ReportView.js
+++ modules/lexical_check_module/ReportView.js
@@ -2,17 +2,17 @@ var React = require('react');
 
 var ReportObject = {};
 
-var LexicalReport = React.createClass({
-  getInitialState: function(){
-    return {
-    }
-  },
-  render: function(){
-    }
-    return (
+// var LexicalReport = React.createClass({
+//   getInitialState: function(){
+//     return {
+//     }
+//   },
+//   render: function(){
+//     return (<div></div>);
+//   }
+// });
 
-    );
-  }
-});
-
-module.exports = LexicalReport;
+//module.exports = LexicalReport;
+module.exports = function(chap, verse) {
+  return (<div>{`Lexical check for ${chap}-${verse}`}</div>);
+}
---
+++ src/js/components/core/reports/ReportGenerator.js
@@ -0,0 +1,113 @@
+/**
+ * @author Evan "Vegan and Proud" Wiederspan
+ * This function will generate and display a report when called. It first opens
+ * a local report template HTML file, reads that text into a Node object, then renders
+ * the gathered JSX from the above class into that page and saves it to another file in the root
+ * directory of the project. Using electron-remote, that file is then opened in a new window
+ * where the user can then save the rendered report. The main reason it was done this way is
+ * that I wanted the report to be displayed in a new Browser window, but browser windows live in
+ * separate threads and do not have access to each other's data without a lot of hacky code. Originally
+ * I wanted the new window to open and then populate itself, but it did not have access to any of the
+ * stores or modules from the other thread. Doing it this way, the data was able to be generated from the
+ * main renderer process while still being able to be displayed in a separate window.
+**/
+
+const React = require("react");
+const ReactDOM = require("react-dom");
+const fs = require('fs');
+const {BrowserWindow} = require('electron').remote;
+
+class Report extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    // get all of the checks associated with this project
+    const listOfChecks = ModuleApi.getDataFromCommon("arrayOfChecks");
+    const targetLang = ModuleApi.getDataFromCommon("targetLanguage");
+    if (!listOfChecks) {
+      return (<div>{"No project data for checks found"}</div>);
+    }
+    if (!targetLang) {
+      return (<div>{"No target language found"}</div>);
+    }
+    // array of the functions in the ReportView.js's for the project
+    let reportViews = [];
+    for (let i in listOfChecks){
+      let check = listOfChecks[i];
+      try {
+        let reportView = require(check.location + "\\ReportView" );
+        if (typeof reportView != "function") {
+          console.log(check.location + "\\ReportView.js did not export a function");
+        }
+        else {
+          reportViews.push(reportView);
+        }
+      }
+      catch(e) {
+        console.log(e);
+        console.log("No report view found in " + check.location);
+      }
+    }
+    if (reportViews.length == 0) {
+      return (<div>{"No report views found"}</div>);
+    }
+    // array of JSX to be rendered
+    // loop through all verses and chapters in the target language
+    // and pass them into the ReortView functions
+    let verses = [];
+    for (let ch in targetLang) {
+      // skip if its not a chapter (key should be a number)
+      if (/^[0-9]+$/.test(ch) == false) {
+        continue;
+      }
+      for (let v in targetLang[ch]) {
+        let reports = [];
+        for (let view of reportViews) {
+          let viewResult = view(ch, v);
+          if (viewResult) {
+            reports.push(viewResult);
+          }
+        }
+        if (reports.length > 0) {
+          verses.push(<span><h3>{`-bookname- ${ch}:${v}`}</h3><div>{reports}</div></span>);
+        }
+      }
+    }
+    // TODO: Get name of book and authors
+    return (
+      <div className="page-header">
+      <h1>{"Report for -bookName- "}<small>{"By -authors-, created on " + new Date().toDateString()}</small></h1>
+      {verses}
+      </div>
+    );
+  }
+}
+
+module.exports = function(callback = (err) => {}) {
+  fs.readFile("./src/js/components/core/reports/report-template.html", 'utf-8', (err, data) => {
+    if (err) {
+      console.log(err, "Report template seems to be missing");
+      callback(err);
+      return;
+    }
+    let reportHTML = document.createElement("html");
+    reportHTML.innerHTML = data;
+    ReactDOM.render(<Report />, reportHTML.getElementsByTagName('div')[0]);
+    fs.writeFile(`${__dirname}\\report.html`, reportHTML.innerHTML, 'utf-8', (err) => {
+      if (err) {
+        console.log("Error writing rendered report to disk");
+        callback(err);
+        return;
+      }
+      window.reportView = new BrowserWindow({width: 600, height: 600, icon: 'images/TC_Icon.png'});
+      window.reportView.loadURL(`file://${__dirname}\\report.html`);
+      window.reportView.on('closed', () => {
+        window.reportView = null;
+      });
+      callback(null);
+    });
+  });
+
+}
