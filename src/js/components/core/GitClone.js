216da11ff190c5dae0aa7c10427f2a2b702a3644
Gfuen <fuentesg@spu.edu>
2016-07-26 13:35:16 -0400

    Ahh push it...push it real good

--- src/js/components/core/GitClone.js
+++ src/js/components/core/GitClone.js
@@ -8,6 +8,11 @@ git.mirror(url, path, function(err, callback) {
   if (err)
     dialog.showErrorBox(err);
   callback();
+  git.pull(remoteRepo, branch, function(err) {
+    if (err) {
+      dialog.showErrorBox('Error', err);
+    }
+});
 });
 
 
