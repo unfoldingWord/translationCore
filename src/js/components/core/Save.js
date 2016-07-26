3446045c09af94bdc354e7a66cdb64e16395cba6
Gfuen <fuentesg@spu.edu>
2016-07-26 09:27:01 -0400

    Edit files for updating TC

--- src/js/components/core/Save.js
+++ src/js/components/core/Save.js
@@ -1,7 +1,21 @@
 const CheckStore = require('../../stores/CheckStore.js')
 const api = require('../../ModuleApi.js');
+const remote = window.electron.remote;
+const {dialog} = remote;
+const GitApi = require('./GitApi.js');
+
 
 module.exports = function() {
   var path = api.getDataFromCommon('saveLocation');
   CheckStore.saveAllToDisk(path);
+  GitApi.add(function(err, data) {
+    if (err)
+      dialog.showErrorBox(err);
+  });
+
+  var message = "Updating TC project from: " + directory;
+  GitApi.commit(message, function(err) {
+    if (err)
+      dialog.showErrorBox(err);
+  });
 };
--- src/js/components/core/UpdateTCProject.js
+++ src/js/components/core/UpdateTCProject.js
@@ -10,17 +10,6 @@ GitApi.pull(remote, branch, function(err) {
     dialog.showErrorBox(err);
 });
 
-GitApi.add(function(err, data) {
-  if (err)
-    dialog.showErrorBox(err);
-});
-
-var message = "Updating TC project from " + directory;
-GitApi.commit(message, function(err) {
-  if (err)
-    dialog.showErrorBox(err);
-});
-
 GitApi.push(remote, branch, function(err) {
   if (err)
     dialog.showErrorBox(err);
