760c5158f165e13753cda5fc188238aa17196f77
Gfuen <fuentesg@spu.edu>
2016-07-26 08:53:09 -0400

    Fixing files

---
+++ src/js/components/core/UpdateTCProject.js
@@ -0,0 +1,12 @@
+var UpdateTC = (function(directory) {
+
+var git = require('simple-git')(directory);
+var GitApi = require('./GitApi.js');
+
+GitApi.pull(remote, branch, );
+
+
+
+});
+
+module.exports = UpdateTC;
