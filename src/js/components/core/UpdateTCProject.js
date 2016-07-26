f547dab62c12fa8c218281eb7f5efc8db7623780
Gfuen <fuentesg@spu.edu>
2016-07-26 10:51:48 -0400

    Ian told me to push so I pushed

--- src/js/components/core/UpdateTCProject.js
+++ src/js/components/core/UpdateTCProject.js
@@ -1,19 +1,20 @@
-var UpdateTC = (function(directory, remote, branch) {
+var UpdateTC = (function(path, remote, branch) {
 
 var remote = window.electron.remote;
 var {dialog} = remote;
 var git = require('simple-git')(directory);
 var GitApi = require('./GitApi.js');
 
-GitApi.pull(remote, branch, function(err) {
-  if (err)
-    dialog.showErrorBox(err);
+GitApi(path).pull(remote, branch, function(err) {
+  if (err) {
+    dialog.showErrorBox('Error', err);
+  }
+  GitApi(path).push(remote, branch, function(err) {
+    if (err)
+      dialog.showErrorBox('Error', err);
+    });
 });
 
-GitApi.push(remote, branch, function(err) {
-  if (err)
-    dialog.showErrorBox(err);
-});
 
 });
 
