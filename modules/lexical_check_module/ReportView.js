019b7fd358c15e4e6e55e235c255a89077e943a3
Evan Wiederspan <evan@wiederspan.org>
2016-07-27 11:19:42 -0400

    Report now uses offline bootstrap

--- modules/lexical_check_module/ReportView.js
+++ modules/lexical_check_module/ReportView.js
@@ -1,18 +1,3 @@
-var React = require('react');
-
-var ReportObject = {};
-
-// var LexicalReport = React.createClass({
-//   getInitialState: function(){
-//     return {
-//     }
-//   },
-//   render: function(){
-//     return (<div></div>);
-//   }
-// });
-
-//module.exports = LexicalReport;
 module.exports = function(chap, verse) {
   return (<div>{`Lexical check for ${chap}-${verse}`}</div>);
 }
