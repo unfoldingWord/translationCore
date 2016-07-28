048edc034b345ac6124d058171ce6edaa0e9782d
loganlebanoff <loganlebanoff@yahoo.com>
2016-07-28 11:52:47 -0400

    Comments and empty phrase report

--- modules/lexical_check_module/ReportView.js
+++ modules/lexical_check_module/ReportView.js
@@ -9,14 +9,19 @@ const Well = ReactBootstrap.Well;
 const NAMESPACE = 'LexicalChecker';
 const extensionRegex = new RegExp('\\.\\w+\\s*$');
 
-function ReportView(chapter, verse) {
+function LexicalReport(chapter, verse) {
   var checks = getChecksByVerse(chapter, verse);
+  // If there are no checks for this verse, then return undefined.
+  // This will make Lexical Check not show at all for this verse.
   if(checks.length == 0) {
     return undefined;
   }
   var checkList = [];
   var numChecked = 0;
   for(let i in checks) {
+    // Only show this specific check if is marked as checked
+    // TODO: Maybe it should still show if there are comments/selectedWords/proposedChanges
+    // even if it is UNCHECKED
     if(checks[i].checkStatus !== "UNCHECKED") {
       checkList.push(
         <ReportItem check={checks[i]} key={i} />
@@ -26,19 +31,22 @@ function ReportView(chapter, verse) {
   }
   return (
     <Well>
-      <h3 style={{marginTop: '-5px', display: 'inline'}}>Lexical Check</h3>
+      <h4 style={{marginTop: '-5px', display: 'inline'}}>Lexical Check</h4>
       <div className='pull-right'>{numChecked}/{checks.length} Completed</div>
       {checkList}
     </Well>
   );
 }
 
+// Given a chapter and verse, returns all of the checks for that reference
 function getChecksByVerse(chapter, verse) {
   var res = [];
   var groups = api.getDataFromCheckStore(NAMESPACE, 'groups');
   for(var group of groups) {
     for(var check of group.checks) {
       if(check.chapter == chapter && check.verse == verse) {
+        // Also appends the gateway language word that we're checking.
+        // This is taken from the group.
         check.wordFile = group.group;
         res.push(check);
       }
@@ -47,25 +55,23 @@ function getChecksByVerse(chapter, verse) {
   return res;
 }
 
+//React component that represents a single check
 class ReportItem extends React.Component {
 	constructor() {
 		super();
   }
-
   headerDiv() {
     if(!this.props.check.wordFile) 
       return undefined;
     return (
-        <h3 style={{marginTop: '-5px'}}>{this.props.check.wordFile.replace(extensionRegex, '')}</h3>
+      <h4 style={{marginTop: '-5px'}}>{this.props.check.wordFile.replace(extensionRegex, '')}</h4>
     );
   }
-
   prettySelectedWords() {
     if(!this.props.check.selectedWords)
       return undefined;
     return this.props.check.selectedWords.join(", ");
   }
-
   selectedWordsDiv() {
     var selectedWords = this.prettySelectedWords();
     if(!selectedWords)
@@ -74,7 +80,6 @@ class ReportItem extends React.Component {
       <div>Selected word: {this.prettySelectedWords()}</div>
     );
   }
-
   checkStatusDiv() {
     if(!this.props.check.checkStatus) 
       return undefined;
@@ -82,7 +87,6 @@ class ReportItem extends React.Component {
       <div>Check status: {this.props.check.checkStatus}</div>
     );
   }
-
   proposedChangesDiv() {
     if(!this.props.check.proposedChanges) 
       return undefined;
@@ -90,7 +94,6 @@ class ReportItem extends React.Component {
       <div>Proposed Changes: {this.props.check.proposedChanges}</div>
     );
   }
-
   commentsDiv() {
     if(!this.props.check.comments) 
       return undefined;
@@ -98,15 +101,11 @@ class ReportItem extends React.Component {
       <div>Comments: {this.props.check.comments}</div>
     );
   }
-
   footerDiv() {
-    if(!this.props.check.user && !this.props.check.timeStamp) 
-      return undefined;
     return (
-      <div style={{fontSize: '75%', color: '#7e7b7b', paddingTop: '10px'}}>{this.props.check.user} - {this.props.check.timeStamp}</div>
+      <div style={{fontSize: '75%', color: '#7e7b7b', paddingTop: '10px'}}>{this.props.check.user || 'USER'} - {this.props.check.timeStamp || 'TIMESTAMP'}</div>
     );
   }
-
   render() {
     return (
       <Well style={{background: 'rgb(255, 255, 255)'}}>
@@ -121,4 +120,4 @@ class ReportItem extends React.Component {
   }
 }
 
-module.exports = ReportView;
+module.exports = LexicalReport;
