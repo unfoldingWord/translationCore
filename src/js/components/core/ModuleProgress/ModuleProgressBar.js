966f4a768758fa36e08a4a01d15cc14239bff36b
Gfuen <fuentesg@spu.edu>
2016-07-29 14:10:30 -0400

    Pulling from develop

--- src/js/components/core/ModuleProgress/ModuleProgressBar.js
+++ src/js/components/core/ModuleProgress/ModuleProgressBar.js
@@ -2,6 +2,7 @@ var React = require('react');
 var remote = window.electron.remote;
 var {dialog} = remote;
 var ProgressBar = require('react-bootstrap/lib/ProgressBar');
+var Collapse = require('react-bootstrap/lib/Collapse');
 var SwitchCheckModuleDropdown = require('../SwitchCheckModuleDropdown.js');
 //var Phrase = require('../');
 //var Lexical = require('../');
@@ -17,17 +18,17 @@ var Progress = React.createClass({
 render: function() {
   return(
       <div data-toggle="collapse" style={style.container}>
-      <div>Phrase Check Module Progress</div>
-        <ProgressBar striped >
-          <ProgressBar bsStyle="success" now={35} label="35%" style={style.bar} />
-          <ProgressBar bsStyle="info" now={60} label="60%" style={style.bar} />
-        </ProgressBar>
-      <div>Lexical Check Module Progress</div>
-        <ProgressBar striped>
-          <ProgressBar bsStyle="success" now={35} label="35%" style={style.bar} />
-          <ProgressBar bsStyle="info" now={20} label="20%" style={style.bar} />
-          <ProgressBar bsStyle="danger" now={20} label="20%" style={style.bar} />
-        </ProgressBar>
+        <div>Phrase Check Module Progress</div>
+          <ProgressBar>
+            <ProgressBar striped key={1} bsStyle="success" now={35} label="35%" style={style.bar} />
+            <ProgressBar striped key={2} bsStyle="info" now={60} label="60%" style={style.bar} />
+          </ProgressBar>
+        <div>Lexical Check Module Progress</div>
+          <ProgressBar>
+            <ProgressBar striped key={1} bsStyle="success" now={35} label="35%" style={style.bar} />
+            <ProgressBar striped key={2} bsStyle="info" now={20} label="20%" style={style.bar} />
+            <ProgressBar striped key={3} bsStyle="danger" now={20} label="20%" style={style.bar} />
+          </ProgressBar>
       </div>
 
   );
--- src/js/components/core/ModuleProgress/ProgressStyle.js
+++ src/js/components/core/ModuleProgress/ProgressStyle.js
@@ -1,12 +1,12 @@
 var style = {
   container:{
-    backgroundColor: "grey",
-    width: "80px",
-    left: "0px"
+    backgroundColor: "pink",
+    width: "150px",
+    position: "absolute",
+    right: "0px"
   },
 
   bar:{
-    width: "60px",
     margin: "auto"
   }
 
