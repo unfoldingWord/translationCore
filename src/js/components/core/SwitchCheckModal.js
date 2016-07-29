1a5c93b2fdd0d3bf40de98c751aba5b9512b3f44
loganlebanoff <loganlebanoff@yahoo.com>
2016-07-29 09:18:45 -0400

    Put fetching back in and it works now.
    Hard-coded stuff that I expect to be there already after a tS project is
    loaded.

--- src/js/components/core/SwitchCheckModal.js
+++ src/js/components/core/SwitchCheckModal.js
@@ -1,7 +1,9 @@
 
 const React = require('react');
+const path = require('path');
 const Button = require('react-bootstrap/lib/Button.js');
 const Modal = require('react-bootstrap/lib/Modal.js');
+const CheckStore = require('../../stores/CheckStore.js');
 const CoreStore = require('../../stores/CoreStore.js');
 const CoreActions = require('../../actions/CoreActions.js');
 const FileModule = require('./FileModule.js');
@@ -35,10 +37,21 @@ class SwitchCheckModal extends React.Component{
   }
 
   moduleClick(folderName) {
-    FileModule.readJsonFile(window.__base + "/modules/" + folderName + "/manifest.json", (jsonObject) => {
+    FileModule.readJsonFile(path.join(window.__base, "modules", folderName, "manifest.json"), (jsonObject) => {
+      // hard coded
+      var params = {originalLanguagePath: "C:/Users/Logan Lebanoff/Desktop/8woc/8woc/data/ulgb", targetLanguagePath: "C:/Users/Logan Lebanoff/Desktop/8woc/test_files/Ephesians", repo: undefined, bookAbbr: "eph"}
+      var array = [];
+      array.push([jsonObject.name, path.join(window.__base, "modules", folderName)]);
+      for(let childFolderName of jsonObject.include) {
+        array.push([childFolderName, path.join(window.__base, "modules", childFolderName)]);
+      }
+      // hard coded
+      if(!CheckStore.storeData.common) {
+        CheckStore.storeData.common = {};
+        CheckStore.storeData.common.saveLocation = "C:/Users/Logan Lebanoff/Desktop/8woc/test_files/Ephesians";
+      }
       debugger;
-      // jsonObject has title, name, and includes (modules that this module needs)
-      // TODO: get the includes and do everyone's FetchDatas
+      CheckDataGrabber.getFetchData(array, params);
     });
   }
 
