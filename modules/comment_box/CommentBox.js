1d1d9fce8f02632fac9c87298d0e3bc1e0269914
mannycolon <colonmanuel7@gmail.com>
2016-08-01 08:45:54 -0400

    styling

--- modules/comment_box/CommentBox.js
+++ modules/comment_box/CommentBox.js
@@ -24,6 +24,7 @@ class CommentBox extends React.Component {
   handleSubmit(e) {
     api.getDataFromCheckStore(NAMESPACE)['currentChanges'] = this.state.comment;
     this.setState({open: false});
+    this.setState({comment: ""});
   }
 
   render() {
@@ -38,7 +39,7 @@ class CommentBox extends React.Component {
             <div style={style.paper}>
               <div style={style.sideline}></div>
                 <div style={style.paperContent}>
-                  <textarea autofocus style={style.textarea}
+                  <textarea autofocus style={style.textarea} value={this.state.comment}
                     onChange={this.handleComment.bind(this)} />
                 </div>
             </div>
