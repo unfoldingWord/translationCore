d562466bb0a2dbcbe6e0cdc2ba7bc57bd5498b7a
Evan Wiederspan <evan@wiederspan.org>
2016-08-03 15:02:47 -0400

    greek parsing v 1

--- modules/t_pane/Verse.js
+++ modules/t_pane/Verse.js
@@ -21,8 +21,22 @@ class Verse extends React.Component {
 	}
 }
 
-function parseGreek(text) {
-	
+var greekRegex = /([^\w\s,.\-?!\(\)]+)\s+(G\d{2,6})\s+(?:G\d{2,6})*\s*([A-Z0-9\-]+)/g;
+
+function parseGreek(text = "") {
+	let result;
+	let words = [];
+	let i = 0;
+	while (result = greekRegex.exec(text)) {
+		try {
+			var [,word,strong,speech] = result;
+		}
+		catch(e) {
+			console.log("parse error");
+		}
+		words.push(<span key={i++} strong={strong} speech={speech} onClick={() => {console.log(word)}}>{word}</span>);
+	}
+	return words;
 }
 
 module.exports = Verse;
