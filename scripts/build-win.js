93895502c3c0237ecba2653b6e3e8779d7b21265
Ian Hoegen <ianhoegen@gmail.com>
2016-08-22 09:09:15 -0700

    Fix suggested changes from @klappy

--- scripts/build-win.js
+++ scripts/build-win.js
@@ -4,11 +4,11 @@ console.log('Starting installer build...');
 
 resultPromise = electronInstaller.createWindowsInstaller({
     appDirectory: './dist/win-unpacked/',
-    exe: 'Translation Core.exe',
+    exe: 'translationCore.exe',
     outputDirectory: './dist/installerWin',
     loadingGif: './images/TC_ANIMATED_Logo.gif',
     setupIcon: './build/icon.ico',
-    setupExe: 'TranslationCoreSetup-0.1.0.exe'
+    setupExe: 'translationCoreSetup.exe'
   });
 
   resultPromise.then(() => console.log("Installer build succesful"), (e) => console.log(`Installer build failed: ${e.message}`));
