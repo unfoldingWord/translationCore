b465d82222f412784f66412f7a82183a3741018b
mannycolon <colonmanuel7@gmail.com>
2016-08-30 10:42:20 -0400

    fixed reportView.js for translationWords and translationNotes so that they fetch commnets data from the store and display them in reports

--- modules/lexical_check_module/ReportView.js
+++ modules/lexical_check_module/ReportView.js
@@ -87,7 +87,7 @@ class ReportItem extends React.Component {
 		super();
   }
   headerDiv() {
-    if(!this.props.check.wordFile) 
+    if(!this.props.check.wordFile)
       return undefined;
     return (
       <h4 style={{marginTop: '-5px'}}>{this.props.check.wordFile.replace(extensionRegex, '')}</h4>
@@ -107,24 +107,24 @@ class ReportItem extends React.Component {
     );
   }
   checkStatusDiv() {
-    if(!this.props.check.checkStatus) 
+    if(!this.props.check.checkStatus)
       return undefined;
     return (
       <div>Check status: {this.props.check.checkStatus}</div>
     );
   }
   proposedChangesDiv() {
-    if(!this.props.check.proposedChanges) 
+    if(!this.props.check.proposedChanges)
       return undefined;
     return (
       <div>Proposed Changes: {this.props.check.proposedChanges}</div>
     );
   }
   commentsDiv() {
-    if(!this.props.check.comments) 
+    if(!this.props.check.comment)
       return undefined;
     return (
-      <div>Comments: {this.props.check.comments}</div>
+      <div>Comments: {this.props.check.comment}</div>
     );
   }
   footerDiv() {
