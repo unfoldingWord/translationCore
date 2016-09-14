dfd46a8e8c50e66b40d338e87dbe6fecca0f6109
Ian Hoegen <ianhoegen@gmail.com>
2016-09-14 13:32:13 -0700

    Display and load recent projects

--- src/js/components/core/ModuleWrapper.js
+++ src/js/components/core/ModuleWrapper.js
@@ -9,6 +9,7 @@ var Button = require('react-bootstrap/lib/Button.js');
 var CoreStore = require('../../stores/CoreStore');
 var NextButton = require('../core/NextButton');
 var PreviousButton = require('../core/PreviousButton');
+var RecentProjects = require('./RecentProjects');
 
 const api = window.ModuleApi;
 
@@ -24,9 +25,18 @@ class ModuleWrapper extends React.Component {
     // TODO: should probably return an empty div if this.state.view doesn't exist
     // but for now it has LexicalCheck as default
     if(!this.state.view) {
+      var projects;
+      if (this.state.showApps) {
+        projects = <p>Click the apps button to start checking</p>;
+      } else if (!api.getDataFromCommon('saveLocation') && !api.getDataFromCommon('tcManifest')) {
+        projects = <RecentProjects.Component onLoad={() => {this.setState({showApps: true})}}/>;
+      } else {
+        this.setState({showApps: true});
+        projects = <p>Click the apps button to start checking</p>;
+      }
       return(
         <div>
-          <p>Click the apps button to start checking</p>
+          {projects}
         </div>
       );
     }
--- src/js/components/core/RecentProjects.js
+++ src/js/components/core/RecentProjects.js
@@ -1,8 +1,63 @@
+const React = require('react');
+const path = require('path');
+const Button = require('react-bootstrap/lib/Button.js');
+const api = window.ModuleApi;
+const CheckStore = require('../../stores/CheckStore');
+
 function addToRecent(path) {
   var previousProjects = localStorage.getItem('previousProjects');
   previousProjects = previousProjects ? JSON.parse(previousProjects) : [];
-  if (!previousProjects.includes(path)) previousProjects.push(path);
+  if (previousProjects.includes(path)) {
+    var indexOfProject = previousProjects.indexOf(path);
+      previousProjects.splice(indexOfProject, 1);
+  }
+  previousProjects.push(path);
   localStorage.setItem('previousProjects', JSON.stringify(previousProjects));
 }
 
+class RecentProjects extends React.Component {
+  getProjects() {
+    var projects = JSON.parse(localStorage.getItem('previousProjects'));
+    if (projects) return projects.reverse();
+    return [];
+  }
+
+  loadProject(filePath) {
+    CheckStore.WIPE_ALL_DATA();
+    api.modules = {};
+    var Access = require('./AccessProject');
+    Access.loadFromFilePath(filePath, this.props.onLoad);
+    api.putDataInCommon('saveLocation', filePath);
+  }
+
+  generateDisplay() {
+    var projectPaths = this.getProjects();
+    var projects = [];
+    var i = 0;
+    for (var project in projectPaths) {
+      var projectPath = projectPaths[project];
+      var projectName = path.basename(projectPath);
+      projects.push(
+        <div key={i++}>
+          <span className={'pull-right'}>
+            <Button onClick={this.loadProject.bind(this, projectPath)}>Load Project</Button>
+          </span>
+          <h3>{projectName}</h3>
+          <p> Location: {projectPaths[project]}</p>
+        </div>
+      );
+    }
+    if (projects.length === 0) return (<p>Click the apps button to start checking</p>);
+    return projects;
+  }
+
+  render() {
+    return (
+      <div>
+      {this.generateDisplay()}
+      </div>
+    );
+  }
+}
 exports.add = addToRecent;
+exports.Component = RecentProjects;
