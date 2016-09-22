b047b7370a47d0c60970b98572d770cda2a72b80
Luke Wilson <lucaswilson@mac.com>
2016-09-22 12:30:46 -0400

    Removed function that was causing selections to change things in click to select

--- modules/translationWords_Check_plugin/TargetVerseDisplay.js
+++ modules/translationWords_Check_plugin/TargetVerseDisplay.js
@@ -254,16 +254,6 @@ const TargetLanguageSelectBox = React.createClass({
     return this.selectedWords;
   },
 
-  textSelected: function(){
-    var text = "";
-    if (window.getSelection) {
-        text = window.getSelection().toString();
-    } else if (document.selection && document.selection.type != "Control") {
-        text = document.selection.createRange().text;
-    }
-    this.addHighlightedString(text);
-  },
-
   render: function() {
     return (
       <div
