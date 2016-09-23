40e0c8fa535dafd404055eb60add3d2bda70417a
Ian Hoegen <ianhoegen@gmail.com>
2016-09-23 11:29:34 -0700

    Download and compile functions for package manager

---
+++ src/js/components/core/package_manager/PackageManager.js
@@ -0,0 +1,67 @@
+const pathex = require('path-extra');
+const fs = require(window.__base + 'node_modules/fs-extra');
+const ncp = require('ncp').ncp;
+const git = require('../GitApi.js');
+const babelCli = pathex.join(window.__base, 'node_modules', '.bin', 'babel');
+const exec = require('child_process').exec;
+
+const PARENT = pathex.datadir('translationCore')
+const PACKAGE_SAVE_LOCATION = pathex.join(PARENT, 'packages');
+const PACKAGE_COMPILE_LOCATION = pathex.join(PARENT, 'packages-compiled')
+const CENTRAL_REPO = ""; //TODO Create central repo to store file
+
+function downloadPackage(packageName) {
+  getPackageList(function(obj){
+    var packageLocation = obj[packageName].location;
+    fs.ensureDirSync(PACKAGE_SAVE_LOCATION);
+    fs.ensureDirSync(PACKAGE_COMPILE_LOCATION);
+    var source = pathex.join(PACKAGE_SAVE_LOCATION, packageName);
+    git(PACKAGE_SAVE_LOCATION).mirror(packageLocation, source, function() {
+      var destination = pathex.join(PACKAGE_COMPILE_LOCATION, packageName);
+      ncp(source, destination, function (err) {
+        compilePackage(destination)
+      });
+    });
+  });
+}
+
+function compilePackage(destination, callback) {
+  var command = babelCli + ' ' + destination + ' --ignore node_modules,.git --out-dir ' + destination;
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    callback('Installation Successful')
+  });
+}
+
+function getJSONFromData(httpRequest) {
+	try {
+		return JSON.parse(httpRequest.response);
+	} catch(error) {
+		console.error(error);
+		return null;
+	}
+}
+
+function getPackageList(callback) {
+  var request = new XMLHttpRequest();
+  request.onload = function() {
+    var obj = getJSONFromData(this);
+    if (obj !== null) {
+      callback(obj);
+     }
+   }
+
+  request.onerror = function() {
+    console.error(this);
+  }
+
+  request.open('GET', CENTRAL_REPO);
+  request.send();
+}
+
+exports.download = downloadPackage;
+exports.list = getPackageList;
+exports.compile = compilePackage;
