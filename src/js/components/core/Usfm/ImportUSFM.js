efd1485a55c045f6f79af72fb026ecf89652e23f
Sterling (Jay) Scott <jay.a.scottjr@biola.edu>
2016-10-13 23:29:09 -0700

    Update ImportUSFM.js
    removed debugger

--- src/js/components/core/Usfm/ImportUSFM.js
+++ src/js/components/core/Usfm/ImportUSFM.js
@@ -136,7 +136,6 @@ function getManifest(folderPath, callback) {
  * @param {String} folderPath - The directory of the project
  ******************************************************************************/
  function loadProject(saveLocation) {
-   debugger;
    getManifest(saveLocation, function(error, tcManifest) {
      if (error) {
        console.error(error);
