b83a50720999a75d922cee41491c3a30c0154f92
mannycolon <colonmanuel7@gmail.com>
2016-10-20 14:39:38 -0400

    fixed appdescription styling, the font used to be black and user was unable to see it, also added margin on top part of the component

--- src/js/components/core/AppDescription.js
+++ src/js/components/core/AppDescription.js
@@ -1,7 +1,5 @@
 const React = require('react');
 
-const RB = require('react-bootstrap');
-const {Button, Col} = RB;
 
 var Styles = {
   appWindow: {
@@ -9,7 +7,7 @@ var Styles = {
     height: "350px",
     borderRadius: '10px',
     padding: '10px',
-    margin: "0px 5px 10px",
+    margin: "5px",
     cursor: "pointer",
     backgroundColor: "#303337",
     border: "3px solid rgba(0, 0, 0, 0.5)",
@@ -28,8 +26,8 @@ class AppDescription extends React.Component{
       <div style={Styles.appWindow} title={"Click to use " + this.props.title + " tool"}
            onClick={this.props.useApp.bind(this, this.props.folderName)}>
         <img style={{width: '60px'}} src={this.props.imagePath} />
-        <h3 style={{display: 'inline-block', marginLeft:'10px'}}>{this.props.title}</h3>
-        <p style={{padding: '20px'}}>{this.props.description}</p>
+        <h3 style={{display: 'inline-block', marginLeft:'10px', color: "#FFFFFF"}}>{this.props.title}</h3>
+        <p style={{padding: '20px', color: "#FFFFFF"}}>{this.props.description}</p>
       </div>
     )
   }
