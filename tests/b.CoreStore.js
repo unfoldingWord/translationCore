c88f6f5c152898968f259516f426b2f36ade246f
Jay Scott <Unknown>
2016-11-23 11:46:44 -0800

    Headers for CoreStore testing

--- tests/b.CoreStore.js
+++ tests/b.CoreStore.js
@@ -3,6 +3,7 @@
   * @author: Ian Hoegen and Jay Scott
   * @todo Write tests for the CoreActions and CoreStore. Probably gotta send actions.
   *****************************************************************************/
-const ModuleApi = require('../src/js/ModuleApi.js');
-
-console.log('tests')
+const CoreActions = require('../src/js/actions/CoreActions.js');
+const CoreStore = require('../src/js/stores/CoreStore.js');
+const chai = require('chai');
+const assert = chai.assert;
