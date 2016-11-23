89d135826f07d1da1cc7e9ed0617cd3bd7e1dcbc
Jay Scott <Unknown>
2016-11-23 05:36:01 -0800

    I Love Tc

--- tests/a.ModuleApi.js
+++ tests/a.ModuleApi.js
@@ -224,7 +224,7 @@ describe('ModuleApi Settings Functions', function () {
   it('setSettings should set the specified setting', function (done) {
     ModuleApi.setSettings(sampleSetting, sampleValue);
     var value = localStorage.getItem('settings');
-    assert.equal([sampleSetting][value], sampleValue);
+    assert.equal([value][sampleSetting], sampleValue);
     done();
   });
   it('getSettings should get the specified setting', function (done) {
