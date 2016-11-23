50da5f604571f6fc6f972ae2f1dee5ef416ee8b0
Jay Scott <Unknown>
2016-11-23 11:58:46 -0800

    Probably not going to need that yet

--- tests/b.CoreStore.js
+++
@@ -1,9 +0,0 @@
-/**
-  * @description Testing for the CoreStore and CoreActions.
-  * @author: Ian Hoegen and Jay Scott
-  * @todo Write tests for the CoreActions and CoreStore. Probably gotta send actions.
-  *****************************************************************************/
-const CoreActions = require('../src/js/actions/CoreActions.js');
-const CoreStore = require('../src/js/stores/CoreStore.js');
-const chai = require('chai');
-const assert = chai.assert;
