cb9a630dd226f96a7914d409e90944a865e3e997
translationCore User <Unknown>
2016-11-23 06:07:33 -0800

    I Love Tc

--- tests/a.ModuleApi.js
+++ tests/a.ModuleApi.js
@@ -174,10 +174,9 @@ describe('ModuleApi Event Listeners', function() {
 
 describe('ModuleApi.getLoggedInUser', function() {
     it('should return an object of the current user', function(done) {
-        this.timeout(10000);
-        const GOGS = require('../../src/js/components/core/login/GogsApi.js');
+        var GOGS = require('../src/js/components/core/login/GogsApi.js');
         var token = ModuleApi.getAuthToken('gogs');
-        const CoreActions = require('../src/js/actions/CoreActions.js');
+        var CoreActions = require('../src/js/actions/CoreActions.js');
         assert.isNotNull(token);
         assert.isNotNull(CoreActions);
         assert.isNotNull(GOGS);
