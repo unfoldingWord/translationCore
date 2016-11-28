76155607277fcda812d3776e56c1bd8c70e42f62
Ian Hoegen <ianhoegen@gmail.com>
2016-11-27 19:12:07 -0800

    Setup load online test

---
+++ tests/b.LoadOnline.js
@@ -0,0 +1,7 @@
+const assert = require('chai').assert;
+const loadOnline = require('./src/components/core/LoadOnline.js');
+
+describe('loadOnline.openManifest', function() {
+  it('loadOnline.openManifest should download a remote repository via git, and return the save location.', function(done){
+  });
+})
