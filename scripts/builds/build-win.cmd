6f1f10fad78c932342e2cb324a200eb4ce50fb15
Ian Hoegen <ianhoegen@gmail.com>
2016-11-28 17:49:38 -0800

    Modify build script to work with travis

--- scripts/builds/build-win.cmd
+++ scripts/builds/build-win.cmd
@@ -1,12 +1,12 @@
-xcopy .\scripts\index.js .\src\js\pages\index.js /Y
+xcopy .\scripts\builds\index.js .\src\js\pages\index.js /Y
 babel src --out-dir src ^
-& node scripts/build-win.js ^
+& node scripts/builds/build-win.js ^
 & echo Extracting app.asar ^
 & asar extract ./dist/translationCore-win32-ia32/resources/app.asar ./temp/app ^
 & echo Copying .bin folder ^
-& xcopy .\node_modules\.bin .\temp\app\node_modules\.bin\ /Y ^
+& xcopy .\scripts\builds\.bin .\temp\app\node_modules\.bin\ /Y ^
 & echo Repacking app.asar ^
 & asar pack ./temp/app ./dist/translationCore-win32-ia32/resources/app.asar ^
-& node scripts/build-win-installer.js ^
+& node scripts/builds/build-win-installer.js ^
 & echo Deleting temp ^
 & rmdir /S /Q temp
