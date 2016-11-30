9f09b959da6edd07aad07d8915946693f431ed9d
translationCore User <Unknown>
2016-11-30 00:14:50 -0800

    testing for Access Project, and CheckDataGrabber done

---
+++ tests/d.AccessProject.js
@@ -0,0 +1,26 @@
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+const should = require('chai').should();
+const Access = require('../src/js/components/core/AccessProject.js');
+const CheckStore = require('../src/js/stores/CheckStore.js')
+const path = require('path-extra');
+const api = window.ModuleApi;
+
+const testProjectPath = window.__base + 'tests/testIO/id_3jn_text_ulb';
+const testUSFMProjectPath = window.__base + 'tests/testIO/60JASOSJNT';
+
+describe('Access.loadFromFilePath', function () {
+    it('should load a project with relevant files from folderpath', function (done) {
+        Access.loadFromFilePath(testProjectPath, done);
+    });
+});
+
+describe('Access.loadCheckData', function () {
+    it('should put the checkdata from filepath into checkstore', function (done) {
+        Access.loadCheckData(testProjectPath + '/checkdata', testProjectPath, function(){
+            expect(CheckStore.storeData).to.not.equal({});
+            expect(localStorage.getItem('lastProject')).to.exist;
+            done();
+        });
+    });
+});
---
+++ tests/e.CheckDataGrabber.js
@@ -0,0 +1,17 @@
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+const should = require('chai').should();
+const CheckDataGrabber = require('../src/js/components/core/create_project/CheckDataGrabber.js');
+const CheckStore = require('../src/js/stores/CheckStore.js')
+const path = require('path-extra');
+const api = window.ModuleApi;
+
+const testTool = window.__base + 'tests/testIO/ExampleChecker';
+
+describe('CheckDataGrabber.loadModuleAndDependencies', function () {
+    it('should load a tool', function (done) {
+        CheckDataGrabber.loadModuleAndDependencies(testTool);
+        expect(api.getDataFromCommon('arrayOfChecks')).to.exist;
+        done();
+    });
+});
