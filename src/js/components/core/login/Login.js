aa7eecbf54c1c2d225d977c32bd1072a05999f9f
Ian Hoegen <ianhoegen@gmail.com>
2016-11-30 17:21:00 -0800

    Change login error message for easier understanding

--- src/js/components/core/login/Login.js
+++ src/js/components/core/login/Login.js
@@ -37,14 +37,13 @@ class Login extends React.Component {
         _this.props.success();
       }
     }).catch(function(reason) {
-      //console.log(reason);
+      console.log(reason);
       if (reason.status === 401) {
-        dialog.showErrorBox('Login Failed', 'Incorrect username or password');
-      } else if (reason.hasOwnProperty('message')) {
+        dialog.showErrorBox('Login Failed', 'Incorrect username or password. This could be caused by using an email address instead of a username.');
+      } else if (reason.message) {
         dialog.showErrorBox('Login Failed', reason.message);
-      } else if (reason.hasOwnProperty('data')) {
-        let errorMessage = reason.data;
-        dialog.showErrorBox('Login Failed', errorMessage);
+      } else if (reason.data) {
+        dialog.showErrorBox('Login Failed', reason.data);
       } else {
         dialog.showErrorBox('Login Failed', 'Unknown Error');
       }
