67ec209a890d2798776fb7d59da11ea4f7279463
EllDoubleYew <lucaswilson@mac.com>
2017-01-19 17:32:58 -0500

    Propgated down the check index to hilight the current submenu item

--- src/js/components/core/SideBar/SideBarContainer.js
+++ src/js/components/core/SideBar/SideBarContainer.js
@@ -55,7 +55,7 @@ class SideBarContainer extends React.Component{
                                 <MenuHeaders ref='menuheaders' subMenuProps={this.props.subMenuProps} currentToolNamespace={this.props.currentToolNamespace} currentGroupObjects={this.props.currentGroupObjects}
                                            isCurrentHeader={this.props.isCurrentHeader} currentCheckIndex={this.props.currentCheckIndex}
                                            currentGroupIndex={this.props.currentGroupIndex} menuClick={this.props.menuClick} currentBookName={this.props.currentBookName}
-                                           currentSubGroupObjects={this.props.currentSubGroupObjects}/>
+                                           currentSubGroupObjects={this.props.currentSubGroupObjects} isCurrentSubMenu={this.props.currentCheckIndex}/>
                           </div>
                         </div>;
     }
