10858ff2382255b6366c73e9f41f3111f061f393
Ian Hoegen <ianhoegen@gmail.com>
2017-01-20 13:16:42 -0800

    Modify build scripts for builds

--- scripts/builds/build-win.cmd
+++ scripts/builds/build-win.cmd
@@ -1,4 +1,5 @@
 xcopy .\scripts\builds\index.js .\src\js\pages\index.js /Y
+babel tC_apps --out-dir tC_apps ^
 babel src --out-dir src ^
 & node scripts/builds/build-win.js ^
 & node scripts/builds/build-win-installer.js ^
