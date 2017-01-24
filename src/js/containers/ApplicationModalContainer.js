66324ed1ea8d9c9428c1ec75ddb54a897aff20a5
manny colon <colonmanuel7@gmail.com>
2017-01-24 14:08:26 -0500

    added sub tabs to application  modal container

--- src/js/containers/ApplicationModalContainer.js
+++ src/js/containers/ApplicationModalContainer.js
@@ -2,12 +2,22 @@ const React = require('react');
 const Modal = require('react-bootstrap/lib/Modal.js');
 const api = window.ModuleApi;
 const CoreActionsRedux = require('../actions/CoreActionsRedux.js');
+const { Tabs, Tab } = require('react-bootstrap/lib');
 
 class ApplicationModalContainer extends React.Component {
     render() {
         return (
             <div>
-            Application container
+              <Tabs defaultActiveKey={1} id="uncontrolled-tab-example"
+                    bsStyle="pills"
+                    style={{borderBottom: "none", backgroundColor: "#5C5C5C", color: '#FFFFFF', width: "100%"}}>
+                <Tab eventKey={1} title="Account" style={{backgroundColor: "#333333"}}>
+
+                </Tab>
+                <Tab eventKey={2} title="Global Settings" style={{backgroundColor: "#333333"}}>
+
+                </Tab>
+              </Tabs>
             </div>
         )
     }
