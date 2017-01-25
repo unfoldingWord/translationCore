da0b6c7ef4b41baade99efa73873fbf95b3b017f
manny colon <colonmanuel7@gmail.com>
2017-01-25 13:32:29 -0500

    added new styling to button and updated buttons text

--- src/js/components/core/login/Login.js
+++ src/js/components/core/login/Login.js
@@ -1,19 +1,12 @@
 const React = require('react');
-const FormGroup = require('react-bootstrap/lib/FormGroup.js');
-const ControlLabel = require('react-bootstrap/lib/ControlLabel.js');
-const FormControl = require('react-bootstrap/lib/FormControl.js');
-const Button = require('react-bootstrap/lib/Button.js');
-const Grid = require('react-bootstrap/lib/Grid.js');
-const Row = require('react-bootstrap/lib/Row.js');
-const Col = require('react-bootstrap/lib/Col.js');
-const style = require('./loginStyle');
 const Registration = require('./Registration.js');
+const { Button, Row, Col, FormGroup, FormControl, utils } = require('react-bootstrap/lib');
+const bootstrapUtils = utils.bootstrapUtils;
+bootstrapUtils.addStyle(Button, 'blue');
+const style = require('./loginStyle');
+
 
 class Login extends React.Component {
-  constructor() {
-    super();
-  }
-
   render() {
     let { displayLogin } = this.props;
     if (!displayLogin) {
@@ -25,10 +18,21 @@ class Login extends React.Component {
     }else{
       return (
         <div>
+          <style type="text/css">
+            {`
+              .btn-blue {
+                background-color: #0277BD;
+                color: white;
+              }
+              .btn-blue:hover {
+                background-color: #C6C4C4;
+              }
+            `}
+          </style>
           <Row className="show-grid">
-            <Col md={12} sm={12} xs={12}>
+            <Col md={12} sm={12} xs={12} style={{marginTop: "50px"}}>
             <center>
-                <h4>Welcome Back!</h4>
+                <h4>Welcome!</h4>
                 <FormGroup controlId="login-form">
                     <FormControl type="text" placeholder="Username"
                                  style={{width: '40%', margin: '15px'}}
@@ -38,7 +42,7 @@ class Login extends React.Component {
                                  style={{width: '40%'}}
                                  onChange={this.props.onHandlePassword}/>
                 </FormGroup>
-                <Button bsStyle="primary"
+                <Button bsStyle="blue"
                         type="submit"
                         onClick={() => {this.props.handleSubmit(this.props.userdata)}}
                         style={{width: '40%', fontWeight: 'bold', marginBottom: '50px'}}>
@@ -46,7 +50,7 @@ class Login extends React.Component {
                 </Button>
                 <h4>{"Don't have an account?"}</h4>
                 <Button onClick={() => this.props.onSwitchToLoginPage(!displayLogin)}
-                        bsStyle="primary"
+                        bsStyle="blue"
                         type="submit"
                         style={{width: '40%', fontWeight: 'bold'}}>
                         Create an Account
