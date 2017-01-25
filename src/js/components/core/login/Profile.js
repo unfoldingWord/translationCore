0b1f9e227bdc4463b6fe0d768b1e57da4d4f9bac
manny colon <colonmanuel7@gmail.com>
2017-01-24 22:21:33 -0500

    added new props to Profile.js to account for new redux implementation

--- src/js/components/core/login/Profile.js
+++ src/js/components/core/login/Profile.js
@@ -11,29 +11,22 @@ const Projects = require('./Projects.js');
 
 class Profile extends React.Component {
   render(){
-    if (this.props.projectVisibility) {
-      return(
-        <Projects {...this.props} back={this.props.hideProjects} />
-      );
-    } else {
-      return(
-        <div>
+    let { userdata } = this.props;
+    console.log(this.props);
+    return(
+      <div>
         <center>
-        <Image style={{height: '100px', width:'100px', marginTop:"50px"}}
-        src={this.props.profilePicture} circle />
-        <span><h3>{this.props.fullName}</h3></span>
-        <span><strong>User Name: </strong></span>
-        <span className="label label-success">{this.props.userName}</span>
-        <span><p>{this.props.emailAccount}</p></span>
-        <Button bsStyle="primary" style={{width: "100%",marginTop:"50px"}} onClick={this.props.showProjects}>
-        Your Door43 Projects
-        </Button>
-        <Button bsStyle="primary" style={{marginBottom:"50px", width: '100%', marginTop: '15px'}}
-        onClick={this.props.handleLogout}>Logout</Button>
+          <Image style={{height: '100px', width:'100px', marginTop:"50px"}}
+                 src={userdata.avatar_url} circle />
+          <h3>{userdata.full_name}</h3>
+          <strong>User Name: </strong>
+          <span className="label label-success">{userdata.username}</span>
+          <span><p>{userdata.email}</p></span>
+          <Button bsStyle="primary" style={{marginBottom:"50px", width: '100%', marginTop: '15px'}}
+                  onClick={this.props.onHandleLogout}>Logout</Button>
         </center>
-        </div>
-      );
-    }
+      </div>
+    );
   }
 }
 
