0cdc2272c1818932388867ced2fec3de74b7abb6
manny colon <colonmanuel7@gmail.com>
2017-01-26 00:31:01 -0500

    implemented redux in ToolsModalContainer.js and added SwitchCheck which receives the props from new toolsReducer

--- src/js/containers/ToolsModalContainer.js
+++ src/js/containers/ToolsModalContainer.js
@@ -1,18 +1,22 @@
 const React = require('react');
-const Modal = require('react-bootstrap/lib/Modal.js');
-const api = window.ModuleApi;
-const CoreActionsRedux = require('../actions/CoreActionsRedux.js');
-const { Tabs, Tab } = require('react-bootstrap/lib');
+const { connect  } = require('react-redux');
+const ToolsActions = require('../actions/ToolsActions.js');
+const { Modal, Tabs, Tab } = require('react-bootstrap/lib');
+const SwitchCheck = require('../components/core/SwitchCheck.js');
 
 class ToolsModalContainer extends React.Component {
+    componentWillMount(){
+      this.props.getToolsMetadatas();
+    }
     render() {
+      console.log(this.props);
         return (
           <div>
             <Tabs defaultActiveKey={1} id="uncontrolled-tab-example"
                   bsStyle="pills"
                   style={{borderBottom: "none", backgroundColor: "#5C5C5C", color: '#FFFFFF', width: "100%"}}>
               <Tab eventKey={1} title="Available Tools" style={{backgroundColor: "#333333"}}>
-
+                <SwitchCheck {...this.props}/>
               </Tab>
               <Tab eventKey={2} title="Online Tools" style={{backgroundColor: "#333333"}}>
 
@@ -23,4 +27,22 @@ class ToolsModalContainer extends React.Component {
     }
 }
 
-module.exports = ToolsModalContainer;
+function mapStateToProps(state) {
+    return Object.assign({}, state.toolsReducer);
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        getToolsMetadatas: () => {
+            dispatch(ToolsActions.getToolsMetadatas());
+        },
+        handleLoadTool: (toolFolderPath) => {
+            dispatch(ToolsActions.loadTool(toolFolderPath));
+        },
+        onHandleUserName: (e) => {
+            dispatch(ToolsActions.setUserName(e.target.value));
+        },
+    }
+}
+
+module.exports = connect(mapStateToProps, mapDispatchToProps)(ToolsModalContainer);
