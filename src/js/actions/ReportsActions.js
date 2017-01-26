f9ba9d555596d29fb2f50469111a8eabe03836ac
manny colon <colonmanuel7@gmail.com>
2017-01-26 17:51:11 -0500

    inluded toast notification to LOAD_REPORTS action method

--- src/js/actions/ReportsActions.js
+++ src/js/actions/ReportsActions.js
@@ -2,7 +2,7 @@ const consts = require('./CoreActionConsts');
 const api = window.ModuleApi;
 
 module.exports.loadReports = function () {
-  //api.Toast.info('Generating reports...', '', 4);
+  api.Toast.info('Generating reports...', '', 4);
   return {
     type: consts.LOAD_REPORTS,
     val: true
