c3795914dd9a4a6d90e6efed7980372440eb81dc
RoyalSix <jay.a.scottjr@biola.edu>
2017-01-26 17:20:08 -0800

    checkng out develop

--- src/js/actions/ImportOnlineActions.js
+++ src/js/actions/ImportOnlineActions.js
@@ -62,3 +62,66 @@ module.exports.loadProjectFromLink = function (link) {
         });
     })
 }
+
+        //  repos: [],
+        //   updateRepos: () => {
+        //     var user = api.getLoggedInUser();
+        //     if (user) {
+        //       var _this = this;
+        //       return Gogs.retrieveRepos(user.userName).then((repos) => {
+        //         this.setState(merge({}, this.state, {
+        //           profileProjectsProps: {
+        //             repos: repos,
+        //           }
+        //         }));
+        //       });
+        //     }
+        //   },
+        //   openSelected: (projectPath) => {
+        //     var dispatch = this.props.dispatch;
+        //     var link = 'https://git.door43.org/' + projectPath + '.git';
+        //     var _this = this;
+        //     loadOnline(link, function (err, savePath, url) {
+        //       if (err) {
+        //         console.error(err);
+        //       } else {
+        //         Upload.sendFilePath(savePath, url, () => {
+        //           dispatch(showCreateProject(false));
+        //         })
+        //       }
+        //     });
+        //   },
+        //   makeList: (repos) => {
+        //     var user = api.getLoggedInUser();
+        //     if (!user) {
+        //       return (
+        //         <div>
+        //           <center>
+        //             <br />
+        //             <h4> Please login first </h4>
+        //             <br />
+        //           </center>
+        //         </div>
+        //       )
+        //     }
+        //     var projectArray = repos;
+        //     var projectList = []
+        //     for (var p in projectArray) {
+        //       var projectName = projectArray[p].project;
+        //       var repoName = projectArray[p].repo;
+        //       projectList.push(
+        //         <div key={p} style={{ width: '100%', marginBottom: '15px' }}>
+        //           {projectName}
+        //           <Button bsStyle='primary' className={'pull-right'} bsSize='sm' onClick={this.state.profileProjectsProps.openSelected.bind(this, repoName)}>Load Project</Button>
+        //         </div>
+        //       );
+        //     }
+        //     if (projectList.length === 0) {
+        //       projectList.push(
+        //         <div key={'None'} style={{ width: '100%', marginBottom: '15px' }}>
+        //           No Projects Found
+        //         </div>
+        //       );
+        //     }
+        //     return projectList;
+        //   }
