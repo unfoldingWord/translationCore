65cc82d07c6dbe904a528974497c3935d524484b
manny colon <colonmanuel7@gmail.com>
2017-01-31 17:54:40 -0500

    created checkStoreReducer.js

--- src/js/reducers/checkStoreReducer.js
+++ src/js/reducers/checkStoreReducer.js
@@ -1,20 +1,32 @@
 const merge = require('lodash.merge');
 const api = window.ModuleApi;
-const CoreStore = require('../stores/CoreStore.js');
 
-//let currentNAMESPACE = CoreStore.getCurrentCheckNamespace();
-let currentNAMESPACE = "TranslationNotesChecker";
 
 const initialState = {
-  currentNAMESPACE: currentNAMESPACE,
-  book: api.getDataFromCheckStore(currentNAMESPACE, 'book'),
-  groups: api.getDataFromCheckStore(currentNAMESPACE, 'groups'),
-  currentGroupIndex: api.getDataFromCheckStore(currentNAMESPACE, 'currentGroupIndex'),
-  currentCheckIndex: api.getDataFromCheckStore(currentNAMESPACE, 'currentCheckIndex'),
+  book: null,
+  groups: null,
+  currentCheck: null,
+  currentGroupIndex: null,
+  currentCheckIndex: null,
 };
 
 module.exports = (state = initialState, action) => {
   switch (action.type) {
+    case "SET_BOOK_NAME":
+      return merge({}, state, {
+        book: action.val,
+      });
+      break;
+    case "SET_GROUPS_OBJECTS":
+      return merge({}, state, {
+        groups: action.val,
+      });
+      break;
+    case "UPDATE_CURRENT_CHECK":
+      return merge({}, state, {
+        currentCheck: action.val,
+      });
+      break;
     case "GO_TO_CHECK":
       return merge({}, state, {
         currentGroupIndex: action.currentGroupIndex,
