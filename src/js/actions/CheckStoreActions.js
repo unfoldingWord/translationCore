cbd339a168bf3b2b6b863f4657b0e7d401240e54
manny colon <colonmanuel7@gmail.com>
2017-01-31 21:17:35 -0500

    Fixed goToCheck, goToNext and goToPrevious

--- src/js/actions/CheckStoreActions.js
+++ src/js/actions/CheckStoreActions.js
@@ -34,66 +34,86 @@ module.exports.updateCurrentCheck = function (NAMESPACE, currentCheck) {
   })
 }
 
-module.exports.goToCheck = function (newGroupIndex, newCheckIndex) {
+module.exports.goToCheck = function (NAMESPACE, newGroupIndex, newCheckIndex) {
   return ((dispatch) => {
     //dispatch({CheckStore.emitEvent('goToCheck');
+    let groups = api.getDataFromCheckStore(NAMESPACE, 'groups');
+    let currentCheck = groups[newGroupIndex]['checks'][newCheckIndex];
     dispatch({
       type: "GO_TO_CHECK",
       currentGroupIndex: newGroupIndex,
       currentCheckIndex: newCheckIndex,
+    });
+    dispatch({
+      type: "UPDATE_CURRENT_CHECK",
+      val: currentCheck,
     });
   })
 }
 
 module.exports.goToNext = function (NAMESPACE) {
   //dispatch({CheckStore.emitEvent('goToNext');
-  let newGroupIndex;
-  let newCheckIndex;
-  let currentGroupIndex = api.getDataFromCheckStore(NAMESPACE, 'currentGroupIndex');
-  let currentCheckIndex = api.getDataFromCheckStore(NAMESPACE, 'currentCheckIndex');
-  let groups = api.getDataFromCheckStore(NAMESPACE, 'groups');
-  if((currentCheckIndex + 1) < groups[currentGroupIndex].checks.length){
-    newGroupIndex = currentGroupIndex;
-    newCheckIndex = currentCheckIndex + 1;
-    api.putDataInCheckStore(NAMESPACE, 'currentGroupIndex', newGroupIndex);
-    api.putDataInCheckStore(NAMESPACE, 'currentCheckIndex', newCheckIndex);
-  }else if((currentCheckIndex + 1) > groups[currentGroupIndex].checks.length){
-    newGroupIndex = currentGroupIndex + 1;
-    newCheckIndex = 0;
-    api.putDataInCheckStore(NAMESPACE, 'currentGroupIndex', newGroupIndex);
-    api.putDataInCheckStore(NAMESPACE, 'currentCheckIndex', newCheckIndex);
-  }
-  return {
-    type: "GO_TO_NEXT",
-    currentGroupIndex: newGroupIndex,
-    currentCheckIndex: newCheckIndex,
-  }
+  return ((dispatch) => {
+    let newGroupIndex = 0;
+    let newCheckIndex = 0;
+    let currentGroupIndex = api.getDataFromCheckStore(NAMESPACE, 'currentGroupIndex');
+    let currentCheckIndex = api.getDataFromCheckStore(NAMESPACE, 'currentCheckIndex');
+    let groups = api.getDataFromCheckStore(NAMESPACE, 'groups');
+    if((currentCheckIndex + 1) < groups[currentGroupIndex].checks.length){
+      newGroupIndex = currentGroupIndex;
+      newCheckIndex = currentCheckIndex + 1;
+      api.putDataInCheckStore(NAMESPACE, 'currentGroupIndex', newGroupIndex);
+      api.putDataInCheckStore(NAMESPACE, 'currentCheckIndex', newCheckIndex);
+    }else if((currentCheckIndex + 1) >= groups[currentGroupIndex].checks.length){
+      newGroupIndex = currentGroupIndex + 1;
+      newCheckIndex = 0;
+      api.putDataInCheckStore(NAMESPACE, 'currentGroupIndex', newGroupIndex);
+      api.putDataInCheckStore(NAMESPACE, 'currentCheckIndex', newCheckIndex);
+    }
+    let currentCheck = groups[newGroupIndex]['checks'][newCheckIndex];
+    dispatch({
+      type: "GO_TO_NEXT",
+      currentGroupIndex: newGroupIndex,
+      currentCheckIndex: newCheckIndex,
+    });
+    dispatch({
+      type: "UPDATE_CURRENT_CHECK",
+      val: currentCheck,
+    });
+  })
 }
 
 module.exports.goToPrevious = function (NAMESPACE) {
   //dispatch({CheckStore.emitEvent('goToPrevious');
-  let newGroupIndex;
-  let newCheckIndex;
-  let currentGroupIndex = api.getDataFromCheckStore(NAMESPACE, 'currentGroupIndex');
-  let currentCheckIndex = api.getDataFromCheckStore(NAMESPACE, 'currentCheckIndex');
-  let groups = api.getDataFromCheckStore(NAMESPACE, 'groups');
-  if(currentCheckIndex >= 1){
-    newGroupIndex = currentGroupIndex;
-    newCheckIndex = currentCheckIndex - 1;
-    api.putDataInCheckStore(NAMESPACE, 'currentGroupIndex', newGroupIndex);
-    api.putDataInCheckStore(NAMESPACE, 'currentCheckIndex', newCheckIndex);
-  }else if (currentCheckIndex == 0 && currentGroupIndex != 0) {
-    newGroupIndex = currentGroupIndex - 1;
-    newCheckIndex = 0;
-    api.putDataInCheckStore(NAMESPACE, 'currentGroupIndex', newGroupIndex);
-    api.putDataInCheckStore(NAMESPACE, 'currentCheckIndex', newCheckIndex);
-  }else if(currentCheckIndex == 0 && currentGroupIndex == 0){
-    newGroupIndex = currentGroupIndex;
-    newCheckIndex = currentCheckIndex;
-  }
-  return {
-    type: "GO_TO_PREVIOUS",
-    currentGroupIndex: newGroupIndex,
-    currentCheckIndex: newCheckIndex,
-  }
+  return ((dispatch) => {
+    let newGroupIndex;
+    let newCheckIndex;
+    let currentGroupIndex = api.getDataFromCheckStore(NAMESPACE, 'currentGroupIndex');
+    let currentCheckIndex = api.getDataFromCheckStore(NAMESPACE, 'currentCheckIndex');
+    let groups = api.getDataFromCheckStore(NAMESPACE, 'groups');
+    if(currentCheckIndex >= 1){
+      newGroupIndex = currentGroupIndex;
+      newCheckIndex = currentCheckIndex - 1;
+      api.putDataInCheckStore(NAMESPACE, 'currentGroupIndex', newGroupIndex);
+      api.putDataInCheckStore(NAMESPACE, 'currentCheckIndex', newCheckIndex);
+    }else if (currentCheckIndex == 0 && currentGroupIndex != 0) {
+      newGroupIndex = currentGroupIndex - 1;
+      newCheckIndex = 0;
+      api.putDataInCheckStore(NAMESPACE, 'currentGroupIndex', newGroupIndex);
+      api.putDataInCheckStore(NAMESPACE, 'currentCheckIndex', newCheckIndex);
+    }else if(currentCheckIndex == 0 && currentGroupIndex == 0){
+      newGroupIndex = currentGroupIndex;
+      newCheckIndex = currentCheckIndex;
+    }
+    let currentCheck = groups[newGroupIndex]['checks'][newCheckIndex];
+    dispatch({
+      type: "GO_TO_PREVIOUS",
+      currentGroupIndex: newGroupIndex,
+      currentCheckIndex: newCheckIndex,
+    });
+    dispatch({
+      type: "UPDATE_CURRENT_CHECK",
+      val: currentCheck,
+    });
+  })
 }
