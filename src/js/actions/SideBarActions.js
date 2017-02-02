5ebc5e483617a6aff0ff388393002d41063fd584
manny colon <colonmanuel7@gmail.com>
2017-02-02 14:54:03 -0500

    split action into for future uses. also fixed bug where wrong parameters where being paased to sidebar action method;2C

--- src/js/actions/SideBarActions.js
+++ src/js/actions/SideBarActions.js
@@ -1,9 +1,16 @@
 const api = window.ModuleApi;
 const CheckStoreActions = require('./CheckStoreActions.js');
 
-module.exports.toggleSubmenu = function (currentToolNamespace, id, currentGroupIndex, bool) {
+module.exports.toggleSubmenu = function () {
+  return {
+    type: "TOGGLE_SUBMENU",
+    val: bool
+  }
+};
+
+module.exports.menuHeaderClicked = function (currentToolNamespace, currentGroupIndex, currentCheckIndex, bool) {
   return ((dispatch) => {
-      dispatch(CheckStoreActions.goToCheck(currentToolNamespace, id, currentGroupIndex));
+      dispatch(CheckStoreActions.goToCheck(currentToolNamespace, currentGroupIndex, currentCheckIndex));
       dispatch({
         type: "TOGGLE_SUBMENU",
         val: bool
