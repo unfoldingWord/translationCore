53ea70a7a1d71b1fecea285e10da4665b8a0d185
Ian Hoegen <ianhoegen@gmail.com>
2017-02-01 22:14:50 -0800

    Now chunks aren't required

--- src/js/components/core/UploadMethods.js
+++ src/js/components/core/UploadMethods.js
@@ -48,8 +48,10 @@ function sendPath(path, link, callback) {
           loadFile(path, 'manifest.json',
             (err, translationStudioManifest) => {
               if (translationStudioManifest) {
+                let tsManifest = verifyChunks(path, translationStudioManifest);
+                console.log(tsManifest);
                 //ts-manifest is present, creating tc-manifest and initiate load
-                saveManifest(path, link, translationStudioManifest, (err, tcManifest) => {
+                saveManifest(path, link, tsManifest, (err, tcManifest) => {
                   loadProjectThatHasManifest(path, callback, tcManifest);
                 });
               }
@@ -198,7 +200,6 @@ function checkIfUSFMFile(savePath, callback) {
   } catch (e) {
     callback(false);
   }
-
 }
 
 function checkIfUSFMProject(savePath, callback) {
@@ -271,6 +272,29 @@ function saveManifest(saveLocation, link, tsManifest, callback) {
     callback(err, null);
   }
 }
+/**
+ * @description - Fixes an issue where manifest chunks are misleading.
+ * @param {Object} tsManifest - A translation studio manifest
+ * @param {String} path - The location of the project
+ */
+ function verifyChunks(path, tsManifest) {
+   let chunkChapters = fs.readdirSync(path);
+   let finishedChunks = [];
+   for (let chapter in chunkChapters) {
+     if (!isNaN(chunkChapters[chapter])) {
+       let chunkVerses = fs.readdirSync(path + '/' + chunkChapters[chapter]);
+       for (let chunk in chunkVerses) {
+         let currentChunk = chunkVerses[chunk].replace(/(?:\(.*\))?\.txt/g, '');
+         let chunkString = chunkChapters[chapter].trim()+ '-' + currentChunk.trim();
+         if (!finishedChunks.includes(chunkString)) {
+           finishedChunks.push(chunkString);
+         }
+       }
+     }
+   }
+   tsManifest.finished_chunks = finishedChunks;
+   return tsManifest;
+ }
 /**
  * @desription - Uses the tc-standard format for projects to make package_version 3 compatible
  * @param oldManifest - The name of an employee.
