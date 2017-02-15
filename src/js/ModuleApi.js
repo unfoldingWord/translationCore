091e61d3cc144a28ed6f650df946cb6b1493d247
Ian Hoegen <ianhoegen@gmail.com>
2017-02-15 14:44:20 -0800

    Setup app to use rollbar instead of HockeyApp

--- src/js/ModuleApi.js
+++ src/js/ModuleApi.js
@@ -15,7 +15,6 @@ const CoreActions = require('./actions/CoreActions.js');
 const Door43DataFetcher = require('./components/core/parsers/Door43DataFetcher.js');
 const BooksOfBible = require('./components/core/BooksOfBible');
 const CheckModule = require('./components/core/CheckModule');
-const HockeyApp = require('./components/core/feedback/HockeyApp.js');
 const MENU_WARN = 'Attempting to save another menu over namespace: ';
 
 class ModuleApi {
@@ -33,7 +32,6 @@ class ModuleApi {
     this.gitDone = true;
     this.currentGroupName = this.initialCurrentGroupName();
     this.currentToolMetaData = null;
-    this.HockeyApp = HockeyApp;
   }
 
   findDOMNode(component) {
