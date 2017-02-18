ac7326a20fd05e7040f74336d6f9b9e4790544b7
manny colon <colonmanuel7@gmail.com>
2017-02-18 02:55:52 -0500

    preparing for 0.4.1

--- scripts/builds/build-win.js
+++ scripts/builds/build-win.js
@@ -21,7 +21,7 @@ packager({
   dir: './',
   platform: 'win32',
   asar: true,
-  'build-version': '0.4.0',
+  'build-version': '0.4.1',
   icon: './build/icon.ico',
   name: 'translationCore',
   overwrite: 'true',
