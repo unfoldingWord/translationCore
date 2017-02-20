3f4ec04197780ddae80ed62dc54f3c3e67e70ae3
manny colon <colonmanuel7@gmail.com>
2017-02-20 18:56:27 -0500

    added setSettings() action without using the api and now saving to a json file in the packages-compiled folder

--- src/js/actions/SettingsActions.js
+++ src/js/actions/SettingsActions.js
@@ -1,12 +1,26 @@
 const consts = require('./CoreActionConsts');
 const api = window.ModuleApi;
+const fs = require('fs-extra');
+const pathex = require('path-extra');
+const PARENT = pathex.datadir('translationCore')
+const PACKAGE_COMPILE_LOCATION = pathex.join(PARENT, 'packages-compiled');
 
-module.exports.onSettingsChange = function (field) {
-  api.setSettings(field.name, field.value);
-  var value = {};
-  value[field.name] = field.value;
-  return {
-    type: consts.CHANGE_SETTINGS,
-    val: value
-  }
+module.exports.setSettings = function(field, value) {
+  return ((dispatch) => {
+    var dir = pathex.join(PACKAGE_COMPILE_LOCATION, 'settings/settings.json');
+    fs.readJson(dir, function (err, settingsObj) {
+      if(err){
+        settingsObj = {};
+      }else {
+        if(field && value != undefined){
+          settingsObj[field] = value;
+        }
+      }
+      fs.outputJson(dir, settingsObj);
+      dispatch({
+        type: consts.CHANGE_SETTINGS,
+        val: settingsObj,
+      });
+    })
+  });
 };
