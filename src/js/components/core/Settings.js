73f4241bc36a3ed9b11d966a7a0d8915903c5725
manny colon <colonmanuel7@gmail.com>
2017-02-20 18:57:52 -0500

    modified the arguments that were being passed down to onSettingsChange() method so that its more consistent and added a method to settings.js to filter names and values of the settings options

--- src/js/components/core/Settings.js
+++ src/js/components/core/Settings.js
@@ -12,23 +12,41 @@ const ControlLabel = require('react-bootstrap/lib/ControlLabel.js');
 const FormControl = require('react-bootstrap/lib/FormControl.js');
 
 class Settings extends React.Component {
+  settingsChange(e){
+    let options = e.target.value;
+    let name = e.target.name;
+    if(options === 'true' || options === 'false'){
+      if(options == "true"){
+        options = true;
+      }else if (options == "false") {
+        options = false;
+      }else{
+        console.error("optionString is not equal to 'true' or 'false'");
+      }
+    }
+    this.props.onSettingsChange(name, options);
+  }
   render() {
+    let { currentSettings } = this.props;
+    let textSelect = currentSettings.textSelect ? currentSettings.textSelect : 'click';
+    let tutorialView = currentSettings.tutorialView ? currentSettings.tutorialView.toString() : 'false';
+    let developerMode = currentSettings.developerMode ? currentSettings.developerMode.toString() : "false";
     return (
       <div style={{paddingTop: '65px', width: '40%', marginLeft: 'auto', marginRight: 'auto'}}>
         <FormGroup controlId="tutorialView">
           <ControlLabel>Tutorial</ControlLabel>
           <FormControl componentClass="select" placeholder="select" name="tutorialView"
             style={{marginBottom: '25px'}}
-            defaultValue={this.props.currentSettings.tutorialView} onChange={this.props.onSettingsChange}>
-            <option value="show">Show</option>
-            <option value="hide">Hide</option>
+            defaultValue={tutorialView} onChange={this.settingsChange.bind(this)}>
+            <option value="true">Show</option>
+            <option value="false">Hide</option>
           </FormControl>
         </FormGroup>
         <FormGroup controlId="textSelect">
           <ControlLabel>Text Select Method</ControlLabel>
           <FormControl componentClass="select" placeholder="select" name="textSelect"
             style={{marginBottom: '25px'}}
-            defaultValue={this.props.currentSettings.textSelect} onChange={this.props.onSettingsChange}>
+            defaultValue={textSelect} onChange={this.settingsChange.bind(this)}>
             <option value="drag">Drag to select</option>
             <option value="click">Click to select</option>
           </FormControl>
@@ -37,9 +55,9 @@ class Settings extends React.Component {
           <ControlLabel>Enable Developer Mode</ControlLabel>
           <FormControl componentClass="select" placeholder="select" name="developerMode"
             style={{marginBottom: '25px'}}
-            defaultValue={this.props.currentSettings.developerMode} onChange={this.props.onSettingsChange}>
-            <option value="disable">Disabled</option>
-            <option value="enable">Enabled</option>
+            defaultValue={developerMode} onChange={this.settingsChange.bind(this)}>
+            <option value="false">Disabled</option>
+            <option value="true">Enabled</option>
           </FormControl>
         </FormGroup>
       </div>
