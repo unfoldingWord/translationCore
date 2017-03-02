cc16fc960b7d9c2a96ede48ce746df03f0661956
manny colon <colonmanuel7@gmail.com>
2017-03-02 11:07:53 -0500

    created NotificationActions.js that includes showNotification() and hideNotification()

---
+++ src/js/actions/NotificationActions.js
@@ -0,0 +1,15 @@
+const consts = require('./CoreActionConsts');
+
+export const showNotification = (message, duration) => {
+  return {
+    type: consts.SHOW_NOTIFICATION,
+    message,
+    duration
+  }
+}
+
+export const hideNotification = () => {
+  return {
+    type: consts.HIDE_NOTIFICATION,
+  }
+}
