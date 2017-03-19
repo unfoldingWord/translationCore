7bdcbaed26224f8f3725ce39ca99e0518fae22ac
manny colon <colonmanuel7@gmail.com>
2017-03-19 03:30:13 -0400

    created method to save resources int he file system following the new data struture

--- src/js/utils/saveMethods.js
+++ src/js/utils/saveMethods.js
@@ -9,3 +9,30 @@ const RESOURCES_DATA_DIR = pathex.join('apps', 'translationCore', 'resources')
 export const saveSettings = (state) => {
   fs.outputJson(SETTINGS_DIRECTORY, state.settingsReducer);
 }
+
+export const saveResources = (state) => {
+  const PROJECT_SAVE_LOCATION = state.projectInfoReducer.projectSaveLocation;
+  let biblesObject = state.resourcesReducer.bibles;
+  let resourcesObject = state.resourcesReducer.resources;
+  for(var keyName in biblesObject){
+    let bibleVersion = keyName + '.json';
+    let savePath = pathex.join(
+      PROJECT_SAVE_LOCATION,
+      RESOURCES_DATA_DIR,
+      'bibles',
+      bibleVersion
+    )
+    fs.outputJson(savePath, biblesObject[keyName])
+  }
+  for(var resources in resourcesObject){
+    for(var file in resourcesObject[resources]){
+      let savePath = pathex.join(
+        PROJECT_SAVE_LOCATION,
+        RESOURCES_DATA_DIR,
+        resources,
+        file
+      )
+      fs.outputJson(savePath, resourcesObject[resources][file])
+    }
+  }
+}
