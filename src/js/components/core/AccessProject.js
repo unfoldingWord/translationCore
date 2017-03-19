d50aa716d378cc13d24a23a2bace7e062da0ca0d
manny colon <colonmanuel7@gmail.com>
2017-03-19 03:14:39 -0400

    dispatching setSaveLocation from AccessProject.js in order to save in the reducer the project save directoy or save location

--- src/js/components/core/AccessProject.js
+++ src/js/components/core/AccessProject.js
@@ -13,6 +13,8 @@ const PACKAGE_SUBMODULE_LOCATION = pathex.join(window.__base, 'tC_apps');
 const CheckDataGrabber = require('./create_project/CheckDataGrabber.js');
 const SettingsActions = require('../../actions/SettingsActions.js');
 const extensionRegex = new RegExp('(\\.\\w+)', 'i');
+import { setSaveLocation } from '../../actions/projectInfoActions'
+import { dispatch } from "../../pages/root"
 var checkList = [];
 
 var Access = {
@@ -24,6 +26,7 @@ var Access = {
    * @param {function} callback - A fucntion that gets called after relevant data is in checkstore
    */
   loadFromFilePath: function (folderpath, callback) {
+    console.log('hello2');
     if (!folderpath) {
       this.loadingProjectError('Must specify location');
       if (callback) {
@@ -38,6 +41,7 @@ var Access = {
     var fileObj = {};
     var manifestLocation = Path.join(folderpath, 'tc-manifest.json');
     try {
+      dispatch(setSaveLocation(folderpath));
       fs.readdir(folderpath, function (err, files) {
         if (err) {
           this.loadingProjectError(err.message);
@@ -76,6 +80,8 @@ var Access = {
    * the checkData folder is loaded
    */
   loadCheckData: function (checkDataFolderPath, folderpath, callback) {
+    console.log('hello3');
+
     if (!checkDataFolderPath || !folderpath) {
       this.loadingProjectError('No checkdata or save path specified');
       if (callback) callback('No checkdata or save path specified');
@@ -86,6 +92,7 @@ var Access = {
       if (!error) {
         _this.getArrayOfChecks(Path.join(checkDataFolderPath, "common.tc"), (arrayOfChecks) => {
           _this.putModulesInCheckstore(arrayOfChecks, checkDataFolderPath, () => {
+            dispatch(setSaveLocation(folderpath));
             api.putDataInCommon('saveLocation', folderpath);
             localStorage.setItem('lastProject', folderpath);
             if (callback) {
@@ -101,6 +108,7 @@ var Access = {
   },
 
   putModulesInCheckstore: function (arrayOfChecks, path, callback) {
+    console.log('hello4');
     try {
       var files = fs.readdirSync(path);
       var index = 0;
@@ -120,6 +128,8 @@ var Access = {
   },
 
   loadingProjectError: function (content, callback = () => { }) {
+    console.log('hello5');
+
     api.createAlert(
       {
         title: 'Error Setting Up Project',
@@ -144,6 +154,7 @@ var Access = {
  * the checkData folder is loaded
  */
   getArrayOfChecks(pathToCommon, callback) {
+    console.log('hello1');
     const _this = this;
     fs.readJson(pathToCommon, (err, common) => {
       for (var element in common) {
