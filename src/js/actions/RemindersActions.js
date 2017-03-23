e2ca747cae6631a630b345386ceaa54af75caf5d
EllDoubleYew <lucaswilson@mac.com>
2017-03-23 14:02:50 -0400

    Doccumenting my action

--- src/js/actions/RemindersActions.js
+++ src/js/actions/RemindersActions.js
@@ -1,6 +1,10 @@
 import consts from '../actions/CoreActionConsts';
 import {generateTimestamp} from '../helpers/index';
 
+/**
+ * @description Toggles the reminder to true or false
+ * @param {String} userName - The username of who toggled the reminder 
+ */
 export const toggleReminder = (userName) => {
     return {
         type: consts.TOGGLE_REMINDER,
