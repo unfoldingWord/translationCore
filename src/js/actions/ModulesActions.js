25ffcf83b5ab6ba9ce50a734e74c32f971a13e14
Christopher Klapp <christopher@klapp.name>
2017-03-23 15:14:14 -0400

    Added action comments

--- src/js/actions/ModulesActions.js
+++ src/js/actions/ModulesActions.js
@@ -2,9 +2,9 @@ import consts from '../actions/CoreActionConsts';
 import {generateTimestamp} from '../helpers/index';
 
 /**
- * @description updates selectionId
- * @param selectionId
- * @return state?
+ * @description updates selectionId so they can be linked later
+ * @param {String} selectionId - the id of the other action
+ * @return {Object} - an action object with the id
  */
 export const updateModuleDataSelectionId = (selectionId) => {
   return {
@@ -12,28 +12,44 @@ export const updateModuleDataSelectionId = (selectionId) => {
     selectionId
   }
 }
-
+/**
+ * @description updates commentId so they can be linked later
+ * @param {String} commentId - the id of the other action
+ * @return {Object} - an action object with the id
+ */
 export const updateModuleDataCommentId = (commentId) => {
   return {
     type: consts.UPDATE_MODULE_DATA_COMMENT_ID,
     commentId
   }
 }
-
+/**
+ * @description updates verseEditId so they can be linked later
+ * @param {String} verseEditId - the id of the other action
+ * @return {Object} - an action object with the id
+ */
 export const updateModuleDataVerseEditId = (verseEditId) => {
   return {
     type: consts.UPDATE_MODULE_DATA_VERSEEDIT_ID,
     verseEditId
   }
 }
-
+/**
+ * @description updates reminderId so they can be linked later
+ * @param {String} reminderId - the id of the other action
+ * @return {Object} - an action object with the id
+ */
 export const updateModuleDataReminderId = (reminderId) => {
   return {
     type: consts.UPDATE_MODULE_DATA_REMINDER_ID,
     reminderId
   }
 }
-
+/**
+ * @description updates username, modifiedTimestamp when all is linked together
+ * @param {String} userName - the userName that links the other ids
+ * @return {Object} - an action object with the id
+ */
 export const linkModuleData = (userName) => {
   return {
     type: consts.LINK_MODULE_DATA,
