ccf5b54f419bb6443197efc06138c1a95a55ef88
Christopher Klapp <christopher@klapp.name>
2017-03-23 15:07:52 -0400

    Splitting actions/reducers/consts for updating separately

--- src/js/actions/ModulesActions.js
+++ src/js/actions/ModulesActions.js
@@ -1,13 +1,42 @@
 import consts from '../actions/CoreActionConsts';
 import {generateTimestamp} from '../helpers/index';
 
-export const linkModuleData = (selectionId, commentId, verseEditId, reminderId, userName) => {
+/**
+ * @description updates selectionId
+ * @param selectionId
+ * @return state?
+ */
+export const updateModuleDataSelectionId = (selectionId) => {
+  return {
+    type: consts.UPDATE_MODULE_DATA_SELECTION_ID,
+    selectionId
+  }
+}
+
+export const updateModuleDataCommentId = (commentId) => {
+  return {
+    type: consts.UPDATE_MODULE_DATA_COMMENT_ID,
+    commentId
+  }
+}
+
+export const updateModuleDataVerseEditId = (verseEditId) => {
+  return {
+    type: consts.UPDATE_MODULE_DATA_VERSEEDIT_ID,
+    verseEditId
+  }
+}
+
+export const updateModuleDataReminderId = (reminderId) => {
+  return {
+    type: consts.UPDATE_MODULE_DATA_REMINDER_ID,
+    reminderId
+  }
+}
+
+export const linkModuleData = (userName) => {
   return {
     type: consts.LINK_MODULE_DATA,
-    selectionId,
-    commentId,
-    verseEditId,
-    reminderId,
     modifiedTimestamp: generateTimestamp(),
     userName
   }
--- src/js/reducers/modulesReducer.js
+++ src/js/reducers/modulesReducer.js
@@ -11,15 +11,32 @@ const initialState = {
 
 const modulesReducer = (state = initialState, action) => {
   switch (action.type) {
+    case consts.UPDATE_MODULE_DATA_SELECTION_ID:
+      return {
+        ...state,
+        selectionId: action.selectionId
+      }
+    case consts.UPDATE_MODULE_DATA_COMMENT_ID:
+      return {
+        ...state,
+        commentId: action.commentId
+      }
+    case consts.UPDATE_MODULE_DATA_VERSEEDIT_ID:
+      return {
+        ...state,
+        verseEditId: action.verseEditId
+      }
+    case consts.UPDATE_MODULE_DATA_REMINDER_ID:
+      return {
+        ...state,
+        reminderId: action.reminderId
+      }
     case consts.LINK_MODULE_DATA:
-      return Object.assign({}, state, {
-        selectionId: action.selectionId,
-        commentId: action.commentId,
-        verseEditId: action.verseEditId,
-        reminderId: action.reminderId,
-        userName: action.userName,
-        modifiedTimestamp: action.modifiedTimestamp
-      })
+      return {
+        ...state,
+        modifiedTimestamp: action.modifiedTimestamp,
+        userName
+      }
     default:
       return state;
   }
