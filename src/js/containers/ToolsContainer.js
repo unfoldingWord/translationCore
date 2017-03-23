7b0ec298a2f9f0af3039bc12955078875a294d5f
mannycolon <colonmanuel7@gmail.com>
2017-03-23 15:11:28 -0400

    added ending Missing semicolon to tools container

--- src/js/containers/ToolsContainer.js
+++ src/js/containers/ToolsContainer.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import CheckStoreActions from '../actions/CheckStoreActions.js'
-import { showNotification } from '../actions/NotificationActions.js'
-import { showPopover } from '../actions/PopoverActions.js'
-import { addNewResource, addNewBible } from '../actions/ResourcesActions.js'
+import React from 'react';
+import { connect } from 'react-redux';
+import CheckStoreActions from '../actions/CheckStoreActions.js';
+import { showNotification } from '../actions/NotificationActions.js';
+import { showPopover } from '../actions/PopoverActions.js';
+import { addNewResource, addNewBible } from '../actions/ResourcesActions.js';
 import { addComment } from '../actions/CommentsActions.js';
 import { addVerseEdit } from '../actions/addVerseEditActions.js';
 
