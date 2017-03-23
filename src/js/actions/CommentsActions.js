c3c99b41e0b9034a6a50f70dfc441176ab4c569b
mannycolon <colonmanuel7@gmail.com>
2017-03-22 22:22:17 -0400

    Added addComment action

--- src/js/actions/CommentsActions.js
+++ src/js/actions/CommentsActions.js
@@ -1,10 +1,10 @@
 import consts from '../actions/CoreActionConsts'
-//import {timestampGenerator} from '../helpers/index'
+import {timestampGenerator} from '../helpers/index'
 
 export const addComment = (text, userName) => {
   return {
     type: consts.ADD_COMMENT,
-    modifiedTimestamp: "2017",//timestampGenerator(),
+    modifiedTimestamp: timestampGenerator(),
     text,
     userName
   }
