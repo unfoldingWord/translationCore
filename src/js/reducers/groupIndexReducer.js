d2382cfcbb6dab512074684afff03a6d9990319b
EllDoubleYew <lucaswilson@mac.com>
2017-03-27 12:35:46 -0400

    Better implementation of the reducer

--- src/js/reducers/groupIndexReducer.js
+++ src/js/reducers/groupIndexReducer.js
@@ -8,7 +8,10 @@ const groupIndexReducer = (state = initialState, action) => {
   switch (action.type) {
     case consts.ADD_GROUP_INDEX: {
       let groupIndex = state.groupIndex;
-      groupIndex[action.groupId] = action.groupName;
+      groupIndex.push({
+          "id": action.groupId,
+          "name": action.groupName
+      });
       return Object.assign({}, state, {
         groupIndex: groupIndex
       });
