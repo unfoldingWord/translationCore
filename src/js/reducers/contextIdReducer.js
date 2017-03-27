511ed58637db9883100eb85fe554c267fe6eec21
mannycolon <colonmanuel7@gmail.com>
2017-03-27 16:23:38 -0400

    added missing semicolons

--- src/js/reducers/contextIdReducer.js
+++ src/js/reducers/contextIdReducer.js
@@ -2,17 +2,17 @@ import consts from '../actions/CoreActionConsts';
 
 const initialState = {
   contextId: null
-}
+};
 
 const contextIdReducer = (state = initialState, action) => {
   switch (action.type) {
     case consts.CHANGE_CURRENT_CONTEXT_ID:
       return Object.assign({}, state, {
         contextId: action.contextId
-      })
+      });
     default:
-      return state
+      return state;
   }
 };
 
-export default contextIdReducer
+export default contextIdReducer;
