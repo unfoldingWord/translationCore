156ea535884eb53ba7cb83581d280516ac6c65b3
EllDoubleYew <lucaswilson@mac.com>
2017-03-28 11:53:29 -0400

    Doh

--- src/js/utils/localStorage.js
+++ src/js/utils/localStorage.js
@@ -1,5 +1,5 @@
 import {loadSettings} from './loadMethods';
-import {saveSettings, saveResources, saveComments, saveVerseEdit, saveSelections, remindersReducer} from './saveMethods';
+import {saveSettings, saveResources, saveComments, saveVerseEdit, saveSelections, saveReminders} from './saveMethods';
 
 /**
  * @description loads state needed to set up reducers with preloaded data
