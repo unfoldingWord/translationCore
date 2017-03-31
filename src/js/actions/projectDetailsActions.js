a32462ddb2a58331f27b3e055303108e15d2f6b5
mannycolon <colonmanuel7@gmail.com>
2017-03-31 15:26:02 -0400

    created new action, setProjectDetail() which sets a property name and a value name in the project detail reducer.

--- src/js/actions/projectDetailsActions.js
+++ src/js/actions/projectDetailsActions.js
@@ -1,6 +1,33 @@
-import consts from './CoreActionConsts'
+import consts from './CoreActionConsts';
 
-export const setSaveLocation = (pathLocation) => ({
-  type: consts.SET_SAVE_PATH_LOCATION,
-  pathLocation
-})
+/**
+ * @description sets the project save location in the projectDetailReducer.
+ * @param {string} pathLocation - project save location and/or directory.
+ * @return {object} action object.
+ */
+export const setSaveLocation = pathLocation => {
+  return {
+    type: consts.SET_SAVE_PATH_LOCATION,
+    pathLocation
+  };
+};
+/**
+ * @description this sets a property name and a value name in the project detail reducer.
+ *  @example detailName = bookName and detailtData = 'Matthew' then
+ *  projectDetailReducer will look as follow:
+ *  {
+ *    ...,
+ *    bookname: Matthew
+ *  }
+ * @param {sting} detailName - projectDetailReducer property name where detailData is saved.
+ * @param {multiple} detailData - this varaiable could be anything from a string, array,
+ * object, boolean ect.
+ * @return {object} action object.
+ */
+export const setProjectDetail = (detailName, detailData) => {
+  return {
+    type: consts.SET_PROJECT_DETAIL,
+    detailName,
+    detailData
+  };
+};
