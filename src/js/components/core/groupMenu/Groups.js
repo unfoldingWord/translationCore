fbe6b88e0dd87f64f0379116edb7c85a55ba50d3
Christopher Klapp <christopher@klapp.name>
2017-04-03 12:32:54 -0400

    abstracted groups function for mapping groupsIndex to Group components

--- src/js/components/core/groupMenu/Groups.js
+++ src/js/components/core/groupMenu/Groups.js
@@ -3,16 +3,27 @@ import Group from './Group'
 import { connect } from 'react-redux'
 
 class Groups extends React.Component {
-  render() {
+/**
+ * @description converts groupsIndex into array of Group components
+ * @param {array} groupsIndex - array of all groupIndex objects
+ * @return {array} groups - array of Group components
+ */
+  groups(groupsIndex) {
     let groups = <div />
-    if (this.props.groupsIndexReducer.groupsIndex !== undefined) {
-      groups = this.props.groupsIndexReducer.groupsIndex.map(groupIndex =>
+    if (groupsIndex !== undefined) {
+      groups = groupsIndex.map(groupIndex =>
         <Group {...this.props} groupIndex={groupIndex} key={groupIndex.id} />
       )
     }
+    return groups
+  }
+
+  render() {
+    let {groupsIndex} = this.props.groupsIndexReducer
+
     return (
-      <div style={{ color: "#FFF", width:"100%"}}>
-        {groups}
+      <div style={{color: "#FFF", width:"100%"}}>
+        {this.groups(groupsIndex)}
       </div>
     )
   }
