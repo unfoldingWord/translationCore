7fbe70c29dd061e3917611d02d4a845b487a2ac9
mannycolon <colonmanuel7@gmail.com>
2017-04-04 11:44:20 -0400

    fixed bug with groupsIndexReducer.js

--- src/js/reducers/groupsIndexReducer.js
+++ src/js/reducers/groupsIndexReducer.js
@@ -6,7 +6,8 @@ const initialState = {
 
 const groupsIndexReducer = (state = initialState, action) => {
   switch (action.type) {
-    case consts.ADD_GROUPS_INDEX: {
+    case consts.SET_GROUPS_INDEX: {
+      console.log(action.groupsIndex);
       return Object.assign({}, state, {
         groupsIndex: action.groupsIndex
       });
