b5b1e391f08e1f247eb86acc8f9f653c7fd2dcc8
Christopher Klapp <christopher@klapp.name>
2017-04-06 18:06:30 -0400

    fixed typo in comments

--- src/js/actions/GroupsIndexActions.js
+++ src/js/actions/GroupsIndexActions.js
@@ -2,7 +2,7 @@ import consts from '../actions/CoreActionConsts';
 
 /**
  * @description This action sends all of the group Ids and
- * group names to the groupIndexReducer
+ * group names to the groupsIndexReducer
  * @param {string} groupIndex - The object of group indecies
  * @return {object} action object.
  */
@@ -17,7 +17,7 @@ export const setGroupsIndex = (groupsIndex) => {
     }
     return 0;
   })
-  
+
   return {
     type: consts.SET_GROUPS_INDEX,
     groupsIndex
