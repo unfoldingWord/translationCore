8dee04379ccf60bb3a20534bbccfae012c8f41ed
RoyalSix <jay.a.scottjr@biola.edu>
2017-04-11 09:33:11 -0700

    fixed function naming bugs and dispathcer

--- src/js/actions/GetDataActions.js
+++ src/js/actions/GetDataActions.js
@@ -267,49 +267,53 @@ export function loadGroupDataFromFileSystem(toolName) {
         let { projectSaveLocation, params } = projectDetailsStore;
         let dataFolder = Path.join(projectSaveLocation, 'apps', 'translationCore', 'index', toolName);
         try {
-            setGroupIndexInStore(dataFolder);
-            setGroupDataInStore(dataFolder);
+            dispatch(setGroupIndexInStore(dataFolder));
+            dispatch(setGroupDataInStore(dataFolder, params));
         } catch (e) {
+            console.warn(e)
         }
     });
 }
 
 export function setGroupDataInStore(dataFolder, params) {
-    let groupDataFolderPath = Path.join(dataFolder, params.bookAbbr);
-    fs.readdir(groupDataFolderPath, (err, groupDataFolderObjs) => {
-        if (!err) {
-            var allGroupsObjects = {};
-            var total = groupDataFolderObjs.length;
-            var i = 1;
-            for (var groupId in groupDataFolderObjs) {
-                if (Path.extname(groupDataFolderObjs[groupId]) != 'json') {
-                    total--;
-                    continue;
-                }
-                let groupName = groupDataFolderObjs[groupId].split('.')[0];
-                const saveGroup = (groupName, groupDataFolderPath) => {
-                    fs.readJson(Path.join(groupDataFolderPath, groupName + '.json'), (err, groupObj) => {
-                        if (!err) {
-                            allGroupsObjects[groupName] = groupObj;
-                            dispatch(LoaderActions.sendProgressForKey(i / total * 100));
-                            i++;
-                            if (i >= total) {
-                                dispatch(GroupsDataActions.loadGroupsDataFromFS(allGroupsObjects));
-                                dispatch(CoreActionsRedux.changeModuleView('main'));
+    return ((dispatch) => {
+        let groupDataFolderPath = Path.join(dataFolder, params.bookAbbr);
+        fs.readdir(groupDataFolderPath, (err, groupDataFolderObjs) => {
+            if (!err) {
+                var allGroupsObjects = {};
+                var total = groupDataFolderObjs.length;
+                var i = 1;
+                for (var groupId in groupDataFolderObjs) {
+                    if (Path.extname(groupDataFolderObjs[groupId]) != '.json') {
+                        total--;
+                        continue;
+                    }
+                    let groupName = groupDataFolderObjs[groupId].split('.')[0];
+                    const saveGroup = (groupName, groupDataFolderPath) => {
+                        fs.readJson(Path.join(groupDataFolderPath, groupName + '.json'), (err, groupObj) => {
+                            if (!err) {
+                                allGroupsObjects[groupName] = groupObj;
+                                dispatch(LoaderActions.sendProgressForKey(i / total * 100));
+                                i++;
+                                if (i >= total) {
+                                    dispatch(GroupsDataActions.loadGroupsDataFromFS(allGroupsObjects));
+                                    dispatch(CoreActionsRedux.changeModuleView('main'));
+                                }
                             }
-                        }
-                    });
+                        });
+                    }
+                    saveGroup(groupName, groupDataFolderPath);
                 }
-                saveGroup(groupName, groupDataFolderPath);
-            }
-        } else throw err
+            } else throw err
+        });
     });
 }
 
 export function setGroupIndexInStore(dataFolder) {
-    let groupIndexObj = fs.readJsonSync(Path.join(dataFolder, 'index.json'))
-    dispatch(GroupsIndexActions.setGroupsIndex(groupIndexObj));
-    dispatch(GroupsIndexActions.loadedGroupsIndexFromFS(true));
+    return ((dispatch) => {
+        let groupIndexObj = fs.readJsonSync(Path.join(dataFolder, 'index.json'))
+        dispatch(GroupsIndexActions.loadGroupsIndexFromFS(groupIndexObj));
+    });
 }
 
 /**
