fedc69592b4bfe1df4c5c24f6f1e51b6efcc086e
Christopher Klapp <christopher@klapp.name>
2017-04-20 14:00:39 -0400

    Initial Plan/Layout

---
+++ src/js/components/home/HomeContainer.js
@@ -0,0 +1,48 @@
+
+
+<HomeContainer>
+  <LicenseModal />
+  <WelcomeSplash />
+  <MainContainer>
+    <StepperContainer>
+      <HomeButton />
+      <Stepper />
+    </StepperContainer>
+    <DisplayContainer>
+      <InstructionsContainer>
+      </InstructionsContainer>
+      <HomeScreenContainer>
+        <User />
+        <Project />
+        <Tool />
+      </HomeScreenContainer>
+      <UserSelectionContainer>
+        <LoginContainer>
+          <LoginD43>
+            <CreateDialog />
+          </LoginD43>
+          <LoginOffline />
+        </LoginContainer>
+      </UserSelectionContainer>
+      <ProjectSelectionContainer>
+        <MyProjects>
+          <SortMenu />
+          <Projects>
+            <Project>
+              <Upload />
+              <Export />
+            </Project>
+          </Projects>
+        </MyProjects>
+        <ProjectsFAB />
+        <OnlineImportModal>
+          <SearchBar />
+          <SearchResults />
+        </OnlineImportModal>
+      </ProjectSelectionContainer>
+      <ToolSelectionContainer>
+        <Cards />
+      </ToolSelectionContainer>
+    </DisplayContainer>
+  </MainContainer>
+</HomeContainer>
