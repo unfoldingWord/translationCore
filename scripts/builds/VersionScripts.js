ae160d49193fd36409009bad3a7337ebc09367fd
Ian Hoegen <ianhoegen@gmail.com>
2017-04-24 11:28:27 -0700

    Update versioning script

--- scripts/builds/VersionScripts.js
+++ scripts/builds/VersionScripts.js
@@ -4,26 +4,18 @@ if (!versionNumber) {
   console.log("No version number specified");
   return;
 }
-var command = 'git tag v' + versionNumber;
-console.log(command);
-exec(command, (err, data) => {
+var script = "git checkout develop &&  \
+  npm version --git-tag-version=false " + versionNumber + '&& \
+  git commit package.json -m"Update version to ' + versionNumber + '" && \
+  git push && \
+  git checkout master && \
+  git merge develop && \
+  git tag v' + versionNumber + '&& \
+  git push origin v' + versionNumber;
+  exec(script, (err, data) => {
   if (err) {
     console.log(err);
     return;
   }
-  console.log('Git commit');
-  exec('npm version --git-tag-version=false ' + versionNumber, (err)=> {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.log('NPM version updated');
-    exec('git push origin v' + versionNumber, (err)=> {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log('Git tag pushed');
-    });
-  });
+  console.log('Version updated succesfully to ' + version);
 });
