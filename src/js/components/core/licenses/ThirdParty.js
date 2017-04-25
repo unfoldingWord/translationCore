9e566cacb67871c5ef4282df293e972dec514f42
EllDoubleYew <lucaswilson@mac.com>
2017-04-25 10:50:52 -0400

    Tying things to the new file

--- src/js/components/core/licenses/ThirdParty.js
+++ src/js/components/core/licenses/ThirdParty.js
@@ -1,15 +1,16 @@
 import React from "react"
-import Licenses from "./licenses.json"
+import Licenses from "./newLicenses.json"
 
 class ThirdParty extends React.Component {
   render() {
-    let libraries = {}
-    for(license in Licenses){
+    let libraries = []
+    for(let license in Licenses){
+      console.log(license);
       libraries.push(
         <div>
           <center>{license}</center>
-          <center>{license.licenses}</center>
-          <center>{license.repository}</center>
+          <center>{Licenses[license].licenses}</center>
+          <center><a href={Licenses[license].repository}>link to license</a></center>
         </div>
       )
     }
