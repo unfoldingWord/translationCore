48a6533926243dc7c3259b4e528142ee694c865f
Ian Hoegen <ianhoegen@gmail.com>
2017-04-27 10:18:16 -0700

    Should fix it this time

--- scripts/builds/windows.sh
+++ scripts/builds/windows.sh
@@ -5,7 +5,6 @@ babel tC_apps --out-dir tC_apps
 node scripts/builds/build-win.js
 unset DISPLAY
 VERSION="0.0.0"
-echo "$VERSION _$TRAVIS_TAG 0"
-if [ -z "$TRAVIS_TAG" ]; then VERSION=$TRAVIS_TAG; else echo "Using default version 0.0.0"; fi
+if [ -z ${TRAVIS_TAG+x} ]; then VERSION=$TRAVIS_TAG; else echo "Using default version 0.0.0"; fi
 echo $VERSION
 wine "C:\inno\ISCC.exe" ".\scripts\inno\windows.iss" "/Qp" "/DVersion=$VERSION"
