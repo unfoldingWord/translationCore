706a65fae3f6916d092b0ecba4954db04c03d36a
Ian Hoegen <ianhoegen@gmail.com>
2017-04-27 10:08:11 -0700

    Debugging Travis_Tag

--- scripts/builds/windows.sh
+++ scripts/builds/windows.sh
@@ -5,6 +5,7 @@ babel tC_apps --out-dir tC_apps
 node scripts/builds/build-win.js
 unset DISPLAY
 VERSION="0.0.0"
-if [ -z "$TRAVIS_TAG" ]; then $VERSION=$TRAVIS_TAG; else echo "Using default version 0.0.0"; fi
+echo "$VERSION _$TRAVIS_TAG 0"
+if [ -z "$TRAVIS_TAG" ]; then VERSION=$TRAVIS_TAG; else echo "Using default version 0.0.0"; fi
 echo $VERSION
 wine "C:\inno\ISCC.exe" ".\scripts\inno\windows.iss" "/Qp" "/DVersion=$VERSION"
