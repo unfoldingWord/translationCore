ea53c96b45a3b6826a239d93c1dd0817e8725b74
Ian Hoegen <ianhoegen@gmail.com>
2017-04-27 02:17:01 -0700

    Move scripts

--- scripts/builds/windows.sh
+++ scripts/builds/windows.sh
@@ -3,4 +3,4 @@ cp ./scripts/builds/index.html ./index.html
 babel src --out-dir src
 babel tC_apps --out-dir tC_apps
 node scripts/builds/build-win.js
-sudo iscc .\scripts\builds\windows.iss /DVersion=${TRAVIS_TAG || TRAVIS_BRANCH}
+sudo iscc .\scripts\builds\windows.iss #/DVersion=${TRAVIS_TAG}
