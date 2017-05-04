f8c963f669322a391d8c9e6b44d454a0bea10a13
Chris Whitfield <cdwhitfield73@gmail.com>
2017-05-04 08:08:06 -0700

    created blank custom menu bar

---
+++ src/js/components/core/BlankMenuBar.js
@@ -0,0 +1,63 @@
+
+var template = [
+  {
+    label: 'Edit',
+    submenu: [
+      {
+        label: 'Cut',
+        accelerator: 'CmdOrCtrl+X',
+        role: 'cut'
+      },
+      {
+        label: 'Copy',
+        accelerator: 'CmdOrCtrl+C',
+        role: 'copy'
+      },
+      {
+        label: 'Paste',
+        accelerator: 'CmdOrCtrl+V',
+        role: 'paste'
+      },
+    ]
+  },
+  {
+    label: 'Window',
+    role: 'window',
+    submenu: [
+      {
+        label: 'Minimize',
+        accelerator: 'CmdOrCtrl+M',
+        role: 'minimize'
+      },
+      {
+        label: 'Reload',
+        accelerator: 'CmdOrCtrl+R',
+        click: function (item, focusedWindow) {
+          if (focusedWindow) focusedWindow.reload();
+        }
+      },
+      {
+        label: 'Toggle Developer Tools',
+        accelerator:
+            process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+        click: function (item, focusedWindow) {
+          if (focusedWindow)
+            focusedWindow.webContents.toggleDevTools();
+        }
+      },
+    ]
+  },
+];
+
+if (process.platform === 'darwin') {
+  template.unshift({
+    label: 'translationCore',
+    submenu: [
+      {
+        accelerator: 'CmdOrCtrl+Q',
+        role: 'quit'
+      }
+    ]
+  });
+}
+module.exports = { template };
