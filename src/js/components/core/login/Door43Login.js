a1512807c4ebeabd57620d850284fb471cedc0a7
mannycolon <colonmanuel7@gmail.com>
2017-05-05 16:46:23 -0400

    started working with new login implemnetation

---
+++ src/js/components/core/login/Door43Login.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { shell } from 'electron';
+import Registration from './Registration.js';
+import { Row, Col, FormGroup, FormControl } from 'react-bootstrap';
+
+class Door43Login extends React.Component {
+
+  render() {
+    let { displayLogin } = this.props;
+    if (!displayLogin) {
+      return (
+        <Col md={12} sm={12} xs={12} style={{ marginTop: "50px" }}>
+          <center>
+            <Registration back={() => this.props.onSwitchToLoginPage(!displayLogin)} />
+          </center>
+        </Col>
+      );
+    } else {
+      return (
+        <div>
+          <Row className="show-grid">
+            <Col md={12} sm={12} xs={12} style={{ marginTop: "50px" }}>
+              <center>
+                <h4>Welcome!</h4>
+                <FormGroup controlId="login-form">
+                  <FormControl type="text" placeholder="Username"
+                    style={{ width: '40%', margin: '15px' }}
+                    onChange={this.props.onHandleUserName} />
+                  <FormControl type="password"
+                    placeholder="Password"
+                    style={{ width: '40%' }}
+                    onChange={this.props.onHandlePassword} />
+                </FormGroup>
+                <button className="btn-prime" onClick={() => this.props.handleSubmit(this.props.userdata)}>
+                  Sign In
+                </button>
+                <div>
+                  <h4>Don't have an account?</h4>
+                  <button className="btn-prime" onClick={() => shell.openExternal('https://git.door43.org/user/sign_up')}>
+                    Create an Account
+                </button><br /><br />
+                </div>
+              </center>
+            </Col>
+          </Row>
+        </div>
+      );
+    }
+  }
+}
+
+export default Door43Login;
--- src/js/components/core/login/Login.js
+++ src/js/components/core/login/Login.js
@@ -1,52 +1,26 @@
-import React from 'react';
-import { shell } from 'electron';
-const Registration = require('./Registration.js');
-const { Row, Col, FormGroup, FormControl } = require('react-bootstrap/lib');
+import React, { Component } from 'react';
+import Door43Login from './Door43Login'
 
-class Login extends React.Component {
+class Login extends Component {
 
   render() {
-    let { displayLogin } = this.props;
-    if (!displayLogin) {
-      return (
-        <Col md={12} sm={12} xs={12} style={{ marginTop: "50px" }}>
-          <center>
-            <Registration back={() => this.props.onSwitchToLoginPage(!displayLogin)} />
-          </center>
-        </Col>
-      );
-    } else {
-      return (
-        <div>
-          <Row className="show-grid">
-            <Col md={12} sm={12} xs={12} style={{ marginTop: "50px" }}>
-              <center>
-                <h4>Welcome!</h4>
-                <FormGroup controlId="login-form">
-                  <FormControl type="text" placeholder="Username"
-                    style={{ width: '40%', margin: '15px' }}
-                    onChange={this.props.onHandleUserName} />
-                  <FormControl type="password"
-                    placeholder="Password"
-                    style={{ width: '40%' }}
-                    onChange={this.props.onHandlePassword} />
-                </FormGroup>
-                <button className="btn-prime" onClick={() => this.props.handleSubmit(this.props.userdata)}>
-                  Sign In
-                </button>
-                <div>
-                  <h4>Don't have an account?</h4>
-                  <button className="btn-prime" onClick={() => shell.openExternal('https://git.door43.org/user/sign_up')}>
-                    Create an Account
-                </button><br /><br />
-                </div>
-              </center>
-            </Col>
-          </Row>
-        </div>
-      );
-    }
+    return (
+      <div>
+        <table>
+          <tbody>
+            <tr>
+              <td style={{backgroundColor: "#000"}}>
+
+              </td>
+              <td>
+                <Door43Login {...this.props} />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
   }
 }
 
-module.exports = Login;
+export default Login;
