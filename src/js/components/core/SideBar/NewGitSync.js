abdc615aa2519b8c4021063ba923f035d8e87e5b
translationCore User <Unknown>
2017-05-15 16:53:02 -0400

    Uploading to Door43

--- src/js/components/core/SideBar/NewGitSync.js
+++ src/js/components/core/SideBar/NewGitSync.js
@@ -6,9 +6,12 @@ function uploadProject(user, projectPath){
   git(projectPath).save(user, 'Uploading to Door43', projectPath, () =>{
     gogs(user.token).createRepo(user, projectPath).then(repo => {
       var newRemote = 'https://' + user.username + ":" + user.password + '@git.door43.org/' + repo.full_name + '.git';
+      console.log(newRemote);
       git(projectPath).push(newRemote, "master", err => {
         throw(err);
       })
+    }).catch(err => {
+        throw(err);
     })
   })
 }
