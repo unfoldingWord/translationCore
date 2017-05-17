1eeb5259c1b43f2f5ca0f69a97883d8b310d7d1b
mannycolon <colonmanuel7@gmail.com>
2017-05-17 15:44:42 -0400

    added a TODO to loadMethods.js in the loadGroupsDataToExport to change all fs to sync actions

--- src/js/utils/loadMethods.js
+++ src/js/utils/loadMethods.js
@@ -48,7 +48,7 @@ export function loadModulesSettings() {
     return {};
   }
 }
-
+// TODO: change to readJson to readJsonSync
 export function loadGroupsDataToExport(tool, dataFolder, params) {
   return new Promise((resolve, reject) => {
     let groupDataFolderPath = path.join(dataFolder, 'index', tool, params.bookAbbr);
