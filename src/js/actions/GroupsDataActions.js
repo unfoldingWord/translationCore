c2d0856ef9b532e79d5c76cb94e2f3070165dc8a
mannycolon <colonmanuel7@gmail.com>
2017-05-25 06:24:05 -0400

    fixed bug where verifyGroupDataMatchesWithFs() action creator was not accounting for two groupIDs with the same chapter and verse.

--- src/js/actions/GroupsDataActions.js
+++ src/js/actions/GroupsDataActions.js
@@ -53,17 +53,25 @@ export function verifyGroupDataMatchesWithFs() {
           verses = filterAndSort(verses);
           verses.forEach(verseFolder => {
             let filePath = path.join(dataPath, chapterFolder, verseFolder);
-            let latestFile = loadFile(filePath);
-            if (latestFile.contextId.tool === state.currentToolReducer.toolName) {
-              toggleGroupDataItems(folderName, latestFile, dispatch);
-            }
+            let latestObjects = getUniqueObjectsFromFolder(filePath);
+            latestObjects.forEach(object => {
+              if (object.contextId.tool === state.currentToolReducer.toolName) {
+                toggleGroupDataItems(folderName, object, dispatch);
+              }
+            });
           });
         });
       });
     }
   });
 }
-
+/**
+ * @description generates a path to a check data item.
+ * @param {object} state - redux store state.
+ * @param {string} PROJECT_SAVE_LOCATION - project path/directory.
+ * @param {string} checkDataName - comments, reminders, selections and verseEdits folders.
+ * @return {string} path/directory to be use to load a file.
+ */
 function generatePathToDataItems(state, PROJECT_SAVE_LOCATION, checkDataName) {
   if (PROJECT_SAVE_LOCATION && state) {
     let bookAbbreviation = state.projectDetailsReducer.params.bookAbbr;
@@ -76,7 +84,11 @@ function generatePathToDataItems(state, PROJECT_SAVE_LOCATION, checkDataName) {
     return loadPath;
   }
 }
-
+/**
+ * @description filters and sorts an array.
+ * @param {array} array - array to be filtered and sorted.
+ * @return {array} filtered and sorted array.
+ */
 function filterAndSort(array) {
   let filteredArray = array.filter(folder => {
     return folder !== ".DS_Store";
@@ -87,19 +99,68 @@ function filterAndSort(array) {
   });
   return filteredArray;
 }
-
-function loadFile(loadPath) {
+/**
+ * @description gets the objects with the latest timestamp and a unique groupID.
+ * @param {string} loadPath - path or directory where check data is saved.
+ * @return {array} array of check data objects with latest timestamp and a unique groupID.
+ */
+function getUniqueObjectsFromFolder(loadPath) {
   let files = fs.readdirSync(loadPath);
+  let uniqueCheckDataObjects = [];
 
   files = files.filter(file => { // filter the filenames to only use .json
     return path.extname(file) === '.json';
   });
 
   let sorted = files.sort().reverse(); // sort the files to use latest
-  let readPath = path.join(loadPath, sorted[0]);
-  return fs.readJsonSync(readPath);
+  let checkDataObjects = sorted.map(file => {
+    // get the json of all files
+    try {
+      let readPath = path.join(loadPath, file)
+      let _checkDataObject = fs.readJsonSync(readPath)
+      return _checkDataObject;
+    } catch (err) {
+      console.warn('File exists but could not be loaded \n', err);
+      return undefined;
+    }
+  });
+
+  checkDataObjects.forEach(element => {
+    let checkDataObjectsWithSameGroupId = checkDataObjects.filter(_checkDataObject => {
+      // filter the checkDataObjects to unique grouId array
+      let keep = _checkDataObject.contextId.groupId === element.contextId.groupId && !contains(_checkDataObject, uniqueCheckDataObjects);
+      return keep;
+    });
+    if (checkDataObjectsWithSameGroupId[0]) {
+      // return the first one since it is the latest modified one
+      uniqueCheckDataObjects.push(checkDataObjectsWithSameGroupId[0]);
+    }
+    // filter out all checkDataObjects that are already in checkDataObjectsWithSameGroupId.
+    checkDataObjects = checkDataObjects.filter(_checkDataObject => {
+      return _checkDataObject.contextId.groupId !== element.contextId.groupId;
+    });
+    // clearing checkDataObjectsWithSameGroupId in order to reuse it for next checkdata object
+    checkDataObjectsWithSameGroupId = [];
+  });
+  return uniqueCheckDataObjects;
+}
+/**
+ * @description returns boolean indicating if object was found in the array (arrayToBeChecked).
+ * @param {object} object - obect to check if is included in array.
+ * @param {array} arrayToBeChecked - array compare against if object is included.
+ * @return {boolean} - true/false: is it included or not.
+ */
+function contains(object, arrayToBeChecked) {
+  let included = arrayToBeChecked.indexOf(object);
+  return included >= 0 ? true : false;
 }
 
+/**
+ * @description dispatches appropiate action based on label string.
+ * @param {string} label - string to be use to determine which action to dispatch.
+ * @param {object} fileObject - checkdata object.
+ * @param {function} dispatch - redux action dispatcher.
+ */
 function toggleGroupDataItems(label, fileObject, dispatch) {
   switch (label) {
     case "comments":
