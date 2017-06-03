8d134bb7c4ca52e48dc626acc6553edddaa5b123
mannycolon <colonmanuel7@gmail.com>
2017-06-02 20:20:25 -0400

    created tests for alertModalReducer.js

--- src/js/reducers/alertModalReducer.js
+++ src/js/reducers/alertModalReducer.js
@@ -1,38 +1,39 @@
 import consts from '../actions/CoreActionConsts';
 
 const initialState = {
-  visibility: false,
-  title: null,
-  content: null,
-  leftButtonText: null,
-  rightButtonText: null,
-  moreInfo: null,
-  response: null,
-  callback: null,
-  moreInfoOpen: false,
+  // visibility: false,
+  // title: null,
+  // content: null,
+  // leftButtonText: null,
+  // rightButtonText: null,
+  // moreInfo: null,
+  // moreInfoOpen: false,
   alertDialogVisibility: false,
   alertDialogLoading: false,
-  alertText: null
+  alertText: null,
+  button1: null,
+  button2: null,
+  callback: null
 };
 
 const alertModalReducer = (state = initialState, action) => {
   switch (action.type) {
-    case consts.SHOW_ALERT_MODAL:
-      return {
-        ...state,
-        title: action.title,
-        content: action.content,
-        leftButtonText: action.leftButtonText,
-        rightButtonText: action.rightButtonText,
-        moreInfo: action.moreInfo,
-        visibility: action.visibility,
-        callback: action.callback
-      }
-    case consts.TOGGLE_MORE_INFO:
-      return {
-        ...state,
-        moreInfoOpen: !state.moreInfoOpen
-      }
+    // case consts.SHOW_ALERT_MODAL:
+    //   return {
+    //     ...state,
+    //     title: action.title,
+    //     content: action.content,
+    //     leftButtonText: action.leftButtonText,
+    //     rightButtonText: action.rightButtonText,
+    //     moreInfo: action.moreInfo,
+    //     visibility: action.visibility,
+    //     callback: action.callback
+    //   }
+    // case consts.TOGGLE_MORE_INFO:
+    //   return {
+    //     ...state,
+    //     moreInfoOpen: !state.moreInfoOpen
+    //   }
     case consts.OPEN_ALERT_DIALOG:
       return {
         ...state,
@@ -42,12 +43,6 @@ const alertModalReducer = (state = initialState, action) => {
         button1: null,
         button2: null,
         callback: null
-      }
-    case consts.CLOSE_ALERT_DIALOG:
-      return {
-        ...state,
-        alertDialogVisibility: false,
-        alertDialogLoading: false
       }
     case consts.OPEN_OPTION_DIALOG:
       return {
@@ -59,9 +54,11 @@ const alertModalReducer = (state = initialState, action) => {
         button2: action.button2Text,
         callback: action.callback
       }
+    case consts.CLOSE_ALERT_DIALOG:
+      return initialState;
     default:
       return state;
   }
-}
+};
 
 export default alertModalReducer;
