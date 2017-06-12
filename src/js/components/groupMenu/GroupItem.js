990312f688fa98d1e934f12b7d5018918faa363e
RoyalSix <jay.a.scottjr@biola.edu>
2017-06-12 10:42:54 -0700

    added docs

--- src/js/components/groupMenu/GroupItem.js
+++ src/js/components/groupMenu/GroupItem.js
@@ -19,9 +19,12 @@ class GroupItem extends React.Component {
   componentDidMount() {
     if (this.props.active) {
       if (this.inView(this.props.groupMenuHeader, this)) {
+        //If the menu and current check are able to be rendered in the 
+        //same window scroll to the group menu item
         this.props.scrollIntoView(this.props.groupMenuHeader);
       }
       else {
+        //Scroll to the current check item
         this.props.scrollIntoView(this);
       }
     }
@@ -30,19 +33,28 @@ class GroupItem extends React.Component {
   componentWillReceiveProps(nextProps, context) {
     if (nextProps.active) {
       if (this.inView(nextProps.groupMenuHeader, this)) {
+        //If the menu and current check are able to be rendered in the 
+        //same window scroll to the group menu item
         nextProps.scrollIntoView(nextProps.groupMenuHeader);
       }
       else {
+         //Scroll to the current check item
         nextProps.scrollIntoView(this);
       }
     }
   }
 
+/**
+ * @description - Tests if the the two elements are in the scope of the window (scroll bar)
+ * The consts MENU_BAR_HEIGHT & MENU_ITEM_HEIGHT are set to account for the static window avialablity 
+ * @param {object} groupMenu - The current group menu header that is extended/actived (i.e. Metaphors)
+ * @param {object} currentItem - The current group check item that is active (i.e. Luke 1:1)
+ */
   inView(groupMenu, currentItem) {
     var rectGroup = ReactDOM.findDOMNode(groupMenu).getBoundingClientRect();
     var rectItem = ReactDOM.findDOMNode(currentItem).getBoundingClientRect();
     var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
-    return Math.abs(rectGroup.top - rectItem.top) + MENU_BAR_HEIGHT + MENU_BAR_HEIGHT <= viewHeight;
+    return Math.abs(rectGroup.top - rectItem.top) + MENU_BAR_HEIGHT + MENU_ITEM_HEIGHT <= viewHeight;
   }
 
   statusGlyph() {
