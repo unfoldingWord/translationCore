4d44a7f133c32baf52cbb337211e24b589756b03
mannycolon <colonmanuel7@gmail.com>
2017-06-20 12:12:58 -0400

    now saving only the target kanguage from the bibles in the resources reducer

--- src/js/utils/localStorage.js
+++ src/js/utils/localStorage.js
@@ -49,7 +49,7 @@ export const saveState = (prevState, newState) => {
     saveSettings(newState);
     saveLocalUserdata(newState);
     saveModuleSettings(newState);
-    if (Object.keys(newState.resourcesReducer.bibles).length > 0) {
+    if (Object.keys(newState.resourcesReducer.bibles.targetLanguage).length > 0) {
       saveTargetLanguage(newState);
     }
     // only save checkData reducers if contextId hasn't changed
