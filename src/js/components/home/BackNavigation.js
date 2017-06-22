b8240b1209dd9477169a4281e5264901c1079957
Christopher Klapp <christopher@klapp.name>
2017-06-22 08:55:42 -0400

    HomeScreen Added disabled states to back/next buttons

--- src/js/components/home/BackNavigation.js
+++ src/js/components/home/BackNavigation.js
@@ -2,11 +2,9 @@ import React, { Component } from 'react'
 
 class BackNavigation extends Component {
 
-  button(text, onclick) {
-    let disabled = false;
-    let className = 'btn-second'
+  button(text, onclick, disabled) {
     return (
-      <button className={className} disabled={disabled} onClick={onclick}>
+      <button className='btn-second' disabled={disabled} onClick={onclick}>
         {text}
       </button>
     )
@@ -14,11 +12,22 @@ class BackNavigation extends Component {
 
   render() {
     let { goToNextStep, goToPrevStep} = this.props.actions
-
+    let {stepIndex} = this.props.reducers.BodyUIReducer.stepper;
+    let backDisabled = false, nextDisabled = false;
+    switch (stepIndex) {
+      case 0:
+        backDisabled = true;
+        break;
+      case 3:
+        nextDisabled = true;
+        break;
+      default:
+        break;
+    }
     return (
       <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
-        {this.button('Go back', goToPrevStep)}
-        {this.button('Next', goToNextStep)}
+        {this.button('Go back', goToPrevStep, backDisabled)}
+        {this.button('Next', goToNextStep, nextDisabled)}
       </div>
     )
   }
