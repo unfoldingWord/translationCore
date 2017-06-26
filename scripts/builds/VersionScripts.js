a2724e1f0ee9c44a294cae96b9434537133f81cf
Christopher Klapp <christopher@klapp.name>
2017-06-26 19:53:58 -0400

    removed comments from travis.yml and go back to develop in update-version script

--- scripts/builds/VersionScripts.js
+++ scripts/builds/VersionScripts.js
@@ -5,14 +5,15 @@ if (!versionNumber) {
   return;
 }
 var script = "git pull && \
-  npm version --git-tag-version=false " + versionNumber + '&& sleep 1s && \
+  npm version --git-tag-version=false " + versionNumber + ' && sleep 1s && \
   git commit package.json package-lock.json -m "Update version to ' + versionNumber + '" && sleep 1s && \
   git push && sleep 1s && \
   git checkout master && sleep 1s && \
   git merge develop --ff && sleep 1s && \
   git push && sleep 1s && \
-  git tag v' + versionNumber + '&& sleep 1s && \
-  git push origin v' + versionNumber;
+  git tag v' + versionNumber + ' && sleep 1s && \
+  git push origin v' + versionNumber + ' && sleep 1s && \
+  git checkout develop';
 
   exec(script, (err, data) => {
   if (err) {
