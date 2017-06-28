d17006950a967fc7e1c59a585a71fa3d3fec0d1f
mannycolon <colonmanuel7@gmail.com>
2017-06-28 14:54:08 -0400

    refactor/organized code from getDataActions and openProjectActions to ProjectSelectionActions.js ToolSelectionActions.js ProjectLoadingActions.js and helpers

---
+++ src/js/actions/ProjectLoadingActions.js
@@ -0,0 +1,34 @@
+import path from 'path-extra';
+import consts from './ActionTypes';
+// actions
+import * as TargetLanguageActions from './TargetLanguageActions';
+import * as AlertModalActions from './AlertModalActions';
+// helpers
+import * as ProjectLoadingHelpers from '../helpers/ProjectLoadingHelpers';
+
+/**
+ * @description function that handles both getGroupsIndex and
+ * getGroupData with promises.
+ * @param {string} toolName - name of the tool being loaded.
+ * @return {object} object action.
+ */
+export function loadProjectData(toolName) {
+  return ((dispatch, getState) => {
+    return new Promise((resolve, reject) => {
+      let { projectDetailsReducer } = getState();
+      let { projectSaveLocation, params } = projectDetailsReducer;
+      const dataDirectory = path.join(projectSaveLocation, '.apps', 'translationCore', 'index', toolName);
+
+      dispatch(TargetLanguageActions.generateTargetBible(projectSaveLocation));
+      ProjectLoadingHelpers.getGroupsIndex(dispatch, toolName, dataDirectory)
+        .then(() => {
+          ProjectLoadingHelpers.getGroupData(dispatch, dataDirectory, toolName, params)
+          .then(dispatch({ type: consts.TOGGLE_LOADER_MODAL, show:false }))
+        });
+    })
+    .catch(err => {
+      console.warn(err);
+      AlertModalActions.openAlertDialog("Oops! We have encountered a problem loading your project. Please contact Help Desk (help@door43.org) for assistance.");
+    });
+  })
+}
---
+++ src/js/actions/ToolSelectionActions.js
@@ -0,0 +1,62 @@
+import fs from 'fs-extra';
+import path from 'path-extra';
+import consts from './ActionTypes';
+// actions
+import * as AlertModalActions from './AlertModalActions';
+// helpers
+import * as LoadHelpers from '../helpers/LoadHelpers';
+
+/**
+ * @description Loads the tool into the main app view, and initates the tool Container component.
+ * @param {string} moduleFolderName - Folder path of the tool being loaded.
+ * @param {string} toolName - name of the current tool being loaded.
+ * @return {object} action object.
+ */
+export function selectTool(moduleFolderName, currentToolName) {
+  return ((dispatch) => {
+    const modulePath = path.join(moduleFolderName, 'package.json');
+    const dataObject = fs.readJsonSync(modulePath);
+    const checkArray = LoadHelpers.createCheckArray(dataObject, moduleFolderName);
+    try {
+      dispatch({ type: consts.START_LOADING });
+      dispatch({ type: consts.CLEAR_TOOL_DATA });
+      dispatch({ type: consts.CLEAR_PREVIOUS_GROUPS_DATA });
+      dispatch({ type: consts.CLEAR_PREVIOUS_GROUPS_INDEX });
+      dispatch({ type: consts.CLEAR_CONTEXT_ID });
+      dispatch( {
+        type: consts.SET_TOOL_NAME,
+        currentToolName
+      });
+      dispatch({
+        type: consts.SET_TOOL_TITLE,
+        currentToolTitle: dataObject.title
+      });
+      dispatch(saveToolViews(checkArray));
+    } catch (e) {
+      console.warn(e);
+      AlertModalActions.openAlertDialog("Oops! We have encountered a problem setting up your project to be loaded. Please contact Help Desk (help@door43.org) for assistance.");
+    }
+  });
+}
+
+/**
+ * @description Saves tools included module Containers in the store
+ * @param {Array} checkArray - Array of the checks that the views should be loaded.
+ * @return {object} action object.
+ */
+export function saveToolViews(checkArray) {
+  return (dispatch => {
+    for (let module of checkArray) {
+      try {
+        const viewObj = require(path.join(module.location, 'Container'));
+        dispatch({
+          type: consts.SAVE_TOOL_VIEW,
+          identifier: module.name,
+          module: viewObj.view || viewObj.container
+        })
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  });
+}
