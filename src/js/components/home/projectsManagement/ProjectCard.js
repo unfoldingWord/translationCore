761c1f431b7a5d29100a220f411c0f5744231f19
Christopher Klapp <christopher@klapp.name>
2017-06-29 15:05:43 -0400

    Basic Layout and design of ProjectsManagement page.

---
+++ src/js/components/home/projectsManagement/ProjectCard.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Glyphicon } from 'react-bootstrap';
+import path from 'path-extra';
+import moment from 'moment';
+import fs from 'fs-extra';
+// components
+import TemplateCard from '../TemplateCard';
+
+let Project = (props) => {
+  const {projectSaveLocation, bookName, params, manifest} = props.projectDetails;
+  let content = <div />;
+
+  if (projectSaveLocation && bookName && params && manifest) {
+    const projectName = path.basename(projectSaveLocation);
+    const projectDataLocation = path.join(projectSaveLocation, '.apps', 'translationCore');
+    const accessTime = fs.statSync(projectDataLocation).atime;
+    const accessTimeAgo = moment().to(accessTime);
+    const { bookAbbr } = params;
+    const { target_language } = manifest;
+
+    /**
+    * @description generates a detail for the contentDetails
+    * @param {string} glyph - name of the glyph to be used
+    * @param {string} text - text used for the detail
+    * @return {component} - component returned
+    */
+    const detail = (glyph, text) => {
+      return (
+        <div>
+          <Glyphicon glyph={glyph} style={{ marginRight: '5px', top: '2px' }} />
+          <span>{text}</span>
+        </div>
+      );
+    }
+
+    // content
+    content = (
+      <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '-10px' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+          <strong style={{ fontSize: 'x-large' }}>{projectName}</strong>
+          <div style={{ display: 'flex', justifyContent: 'space-between', width: '410px', marginBottom: '6px' }}>
+            {detail('time', accessTimeAgo)}
+            {detail('book', bookName + ' (' + bookAbbr + ')')}
+            {detail('globe', target_language.name + ' (' + target_language.id + ')')}
+          </div>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', textAlign: 'right', marginRight: '-6px' }}>
+          <div>
+            <Glyphicon glyph="option-vertical" style={{ fontSize: "large" }} />
+          </div>
+          <div>
+            <button className='btn-prime' disabled={true} onClick={()=>{}} style={{ width: '90px', marginBottom: '0' }}>
+              Select
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+
+  }
+
+  // heading
+  const heading = (
+    <span></span>
+  );
+
+  return (
+    <TemplateCard
+      heading={heading}
+      content={content}
+    />
+  );
+}
+
+Project.propTypes = {
+  projectDetails: PropTypes.object.isRequired,
+  actions: PropTypes.object.isRequired
+};
+
+export default Project;
---
+++ src/js/components/home/projectsManagement/ProjectsFAB.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Glyphicon} from 'react-bootstrap';
+
+let ProjectFAB = (props) => {
+  return (
+    <div>
+      ProjectFAB
+    </div>
+  );
+}
+
+export default ProjectFAB;
