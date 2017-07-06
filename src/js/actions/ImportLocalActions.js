c21c91ef754fd5914a00191225c7737b3769e691
mannycolon <colonmanuel7@gmail.com>
2017-07-06 01:51:16 -0400

    added error checking for selectLocalProjectToLoad()

--- src/js/actions/ImportLocalActions.js
+++ src/js/actions/ImportLocalActions.js
@@ -1,11 +1,43 @@
+import React from 'react';
+import path from 'path-extra';
+import fs from 'fs-extra';
 import { remote } from 'electron';
-
+// actions
+import * as AlertModalActions from './AlertModalActions';
+import * as BodyUIActions from './BodyUIActions';
+// contstants
 const { dialog } = remote;
 
 export function selectLocalProjectToLoad() {
   return ((dispatch, getState)=> {
+    const alertMessage = (
+      <div>
+        No file was selected. Please click on the
+        <span style={{ color: 'var(--accent-color-dark)', fontWeight: "bold"}}>
+          &nbsp;Import Local Project&nbsp;
+        </span>
+        button again and select the project you want to load.
+      </div>
+    );
+
     dialog.showOpenDialog((filePath) => {
-      console.log(filePath);
+      if(filePath === undefined){
+        dispatch(BodyUIActions.toggleProjectsFAB());
+        dispatch(AlertModalActions.openAlertDialog(alertMessage));
+      } else if(verifyIsValidProject(filePath[0])) {
+        console.log(filePath);
+      } else {
+        console.log(filePath);
+      }
     });
   });
+}
+
+
+function verifyIsValidProject(projectPath) {
+  const projectManifestPath = path.join(projectPath, "manifest.json");
+  if (fs.existSync(projectManifestPath)) {
+    return true;
+  }
+  return false;
 }
