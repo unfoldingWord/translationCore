750f4e28150b175971228c1e33432e71a4e3294f
RoyalSix <jay.a.scottjr@biola.edu>
2017-07-10 10:54:52 -0700

    added tempororay fix for peresisting, extracted components

--- src/js/actions/OnlineModeActions.js
+++ src/js/actions/OnlineModeActions.js
@@ -1,15 +1,13 @@
-import React from 'react';
 const { BrowserWindow } = require('electron').remote
 import consts from './ActionTypes';
-import * as AlertModalActions from './AlertModalActions';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import { Checkbox } from 'material-ui';
+import * as AlertModalActions from './AlertModalActions';``
+import OnlineDialog from '../components/dialogComponents/OnlineDialog';
 
 export function confirmOnlineAction(callback) {
   return ((dispatch, getState) => {
     var onlineMode = getState().settingsReducer.onlineMode;
     if (!onlineMode) {
-      dispatch(AlertModalActions.openOptionDialog(onlineModeWarning((val) => dispatch(checkBox(val))),
+      dispatch(AlertModalActions.openOptionDialog(OnlineDialog((val) => dispatch(checkBox(val))),
         (result) => {
           if (result != 'Cancel') {
             callback();
@@ -18,30 +16,6 @@ export function confirmOnlineAction(callback) {
         }, 'Access Internet', 'Cancel'))
     } else callback()
   })
-}
-
-export function agreeCheckBox(checkBoxAction) {
-  return (
-    <Checkbox
-      style={{ width: "0px", marginRight: -10 }}
-      iconStyle={{ fill: 'black' }}
-      labelStyle={{ color: "var(--reverse-color)", opacity: "0.7", fontWeight: "500" }}
-      onCheck={(e) => {
-        checkBoxAction(e.target.checked)
-      }}
-    />
-  )
-}
-
-export function onlineModeWarning(checkBoxAction) {
-  return (
-    <MuiThemeProvider>
-      <div>
-        <p style={{ fontSize: 15 }}>You are about to transmit data over the internet, you sure you want to do that?</p>
-        <div style={{ display: 'flex' }}>{agreeCheckBox(checkBoxAction)} Do not show this warning again</div>
-      </div>
-    </MuiThemeProvider>
-  )
 }
 
 export function checkBox(val) {
