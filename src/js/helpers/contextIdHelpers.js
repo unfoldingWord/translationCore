1cbf822a774db85a78a24fddf23a20192c69d2c2
Christopher Klapp <christopher@klapp.name>
2017-08-02 18:07:11 -0400

    validates context id and next if invalid

---
+++ src/js/helpers/contextIdHelpers.js
@@ -0,0 +1,21 @@
+// helpers
+import * as selectionHelpers from './selectionHelpers';
+
+/**
+ * @description this action validates if the contextId is valid by checking to see if the quote occurrence exists
+ * @param {object} state - current state to get the verse out of
+ * @param {object} contextId - the contextId object.
+ * @param {string} bibleId - the id/name of the bible to get the chapter/verse text to check for the string occurrences
+ * @return {object} New state for contextId reducer.
+ */
+export const validateContextIdQuote = (state, contextId, bibleId) => {
+  let valid = false
+  if (contextId && bibleId) {
+    const { chapter, verse } = contextId.reference;
+    const { quote, occurrence } = contextId;
+    const verseText = state.resourcesReducer.bibles[bibleId][chapter][verse];
+    const occurrences = selectionHelpers.occurrences(verseText, quote);
+    valid = occurrence <= occurrences;
+  }
+  return valid;
+}
