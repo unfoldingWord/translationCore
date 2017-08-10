6253313fa4f4e06ebcee58b3116723a86ef64d82
RoyalSix <jay.a.scottjr@biola.edu>
2017-08-10 07:39:29 -0700

    checking out

--- src/js/components/projectValidation/MissingVersesCheck/index.js
+++ src/js/components/projectValidation/MissingVersesCheck/index.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 class MissingVersesCheck extends Component {
+    componentDidMount() {
+        this.props.actions.changeProjectValidationInstructions(
+            <div>
+                <div>Some verses are missing from your project</div>
+                <div>You can fix these in translationStudio or Autographa</div>
+            </div>
+        )
+    }
+    generateVerseCards(missingVersesObject) {
+        return missingVersesObject.map((verseObject) => {
+            return (
+                <div>
+                {verseObject.chapter} + verseObject.verse
+                </div>
+            )
+        })
+    }
     render() {
         return (
             <div>
-                MissingVersesCheck
+                {this.generateVerseCards(this.props.missingVersesObject)}
             </div>
         );
     }
