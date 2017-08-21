b72717402e3f359a59dfce5fcec7f487f7604c27
Christopher Klapp <klappy@users.noreply.github.com>
2017-08-21 17:16:18 -0400

    Added collapsible submenu to Group Menu (#2490)
    * Added collapsible submenu to Group Menu
    
    * renamed expandMenu action

---
+++ src/js/actions/GroupMenuActions.js
@@ -0,0 +1,13 @@
+import consts from './ActionTypes';
+
+/**
+ * @description This action expands/collapses the submenu in the group menu
+ * @param {bool} isSubMenuExpanded - true or false
+ */
+
+export const expandSubMenu = (isSubMenuExpanded) => {
+  return {
+    type: consts.GROUP_MENU_EXPAND_SUBMENU,
+    isSubMenuExpanded
+  }
+}
--- src/js/reducers/groupMenuReducer.js
+++ src/js/reducers/groupMenuReducer.js
@@ -2,17 +2,15 @@ import consts from '../actions/ActionTypes';
 
 const initialState = {
   menuVisibility: true,
-  subMenuOpen: true,
-  openCheck:0
+  isSubMenuExpanded: true
 };
 
 const groupMenuReducer = (state = initialState, action) => {
   switch (action.type) {
-    case consts.TOGGLE_SUBMENU:
+    case consts.GROUP_MENU_EXPAND_SUBMENU:
       return {
         ...state,
-        subMenuOpen: (action.newGroup || state.openCheck != action.openCheck) ? true: !state.subMenuOpen,
-        openCheck: action.openCheck
+        isSubMenuExpanded: action.isSubMenuExpanded
       }
     case consts.TOGGLE_MENU_DRAWER:
       return { ...state, menuVisibility: !state.menuVisibility }
