dd60b78819d175cded92e49358af141dcf02c543
RoyalSix <jay.a.scottjr@biola.edu>
2017-08-21 18:58:29 -0700

    removed access internet dialog from local user login

--- src/js/actions/ProjectUploadActions.js
+++ src/js/actions/ProjectUploadActions.js
@@ -15,63 +15,65 @@ import open from 'open';
  */
 export function uploadProject(projectPath, user) {
   return (dispatch => {
-    dispatch(OnlineModeActions.confirmOnlineAction(() => {
-      var projectName = projectPath.split(path.sep).pop();
-      const message = "Uploading " + projectName + " to Door43. Please wait...";
-      dispatch(AlertModalActions.openAlertDialog(message, true));
-      if (!user.token && !user.localUser) {
-        const message = "Your login has become invalid. Please log out and log back in.";
-        return dispatch(AlertModalActions.openAlertDialog(message, false));
-      } else if (user.localUser) {
-        const message = "You need to be logged in with a Door43 account to upload a project. Please log in to continue.";
-        return dispatch(AlertModalActions.openAlertDialog(message, false));
-      }
+    if (!user.localUser) {
+      dispatch(OnlineModeActions.confirmOnlineAction(() => {
+        var projectName = projectPath.split(path.sep).pop();
+        const message = "Uploading " + projectName + " to Door43. Please wait...";
+        dispatch(AlertModalActions.openAlertDialog(message, true));
+        if (!user.token) {
+          const message = "Your login has become invalid. Please log out and log back in.";
+          return dispatch(AlertModalActions.openAlertDialog(message, false));
+        }
 
-      gogs(user.token).createRepo(user, projectName).then(repo => {
-        var newRemote = 'https://' + user.token + '@git.door43.org/' + repo.full_name + '.git';
+        gogs(user.token).createRepo(user, projectName).then(repo => {
+          var newRemote = 'https://' + user.token + '@git.door43.org/' + repo.full_name + '.git';
 
-        git(projectPath).save(user, 'Commit before upload', projectPath, err => {
-          if (err) {
-            dispatch(AlertModalActions.openAlertDialog("Error saving project: " + err));
-          } else {
-            git(projectPath).push(newRemote, "master", err => {
-              if (err) {
-                if (err.status === 401 || err.code === "ENOTFOUND" || err.toString().includes("connect ETIMEDOUT") || err.toString().includes("INTERNET_DISCONNECTED") || err.toString().includes("unable to access") || err.toString().includes("The remote end hung up")) {
-                  const message = "Unable to connect to the server. Please check your Internet connection.";
-                  dispatch(AlertModalActions.openAlertDialog(message));
-                } else if (err.toString().includes("rejected because the remote contains work")) {
-                  const message = projectName + ' cannot be uploaded because there have been changes to the translation of that project on your Door43 account.';
-                  dispatch(AlertModalActions.openAlertDialog(message));
-                } else if (err.hasOwnProperty('message')) {
-                  dispatch(AlertModalActions.openAlertDialog('Error Uploading: ' + err.message));
-                } else if (err.hasOwnProperty('data') && err.data) {
-                  dispatch(AlertModalActions.openAlertDialog('Error Uploading: ' + err.data));
+          git(projectPath).save(user, 'Commit before upload', projectPath, err => {
+            if (err) {
+              dispatch(AlertModalActions.openAlertDialog("Error saving project: " + err));
+            } else {
+              git(projectPath).push(newRemote, "master", err => {
+                if (err) {
+                  if (err.status === 401 || err.code === "ENOTFOUND" || err.toString().includes("connect ETIMEDOUT") || err.toString().includes("INTERNET_DISCONNECTED") || err.toString().includes("unable to access") || err.toString().includes("The remote end hung up")) {
+                    const message = "Unable to connect to the server. Please check your Internet connection.";
+                    dispatch(AlertModalActions.openAlertDialog(message));
+                  } else if (err.toString().includes("rejected because the remote contains work")) {
+                    const message = projectName + ' cannot be uploaded because there have been changes to the translation of that project on your Door43 account.';
+                    dispatch(AlertModalActions.openAlertDialog(message));
+                  } else if (err.hasOwnProperty('message')) {
+                    dispatch(AlertModalActions.openAlertDialog('Error Uploading: ' + err.message));
+                  } else if (err.hasOwnProperty('data') && err.data) {
+                    dispatch(AlertModalActions.openAlertDialog('Error Uploading: ' + err.data));
+                  } else {
+                    dispatch(AlertModalActions.openAlertDialog('Error Uploading: Unknown error'));
+                  }
                 } else {
-                  dispatch(AlertModalActions.openAlertDialog('Error Uploading: Unknown error'));
-                }
-              } else {
-                dispatch(
-                  AlertModalActions.openAlertDialog(
-                    <div>
-                      <span>
-                        <span style={{ fontWeight: 'bold' }}>{user.username + ", "}</span>
-                        {"your project was uploaded successfully to: "}
-                        <a style={{cursor: 'pointer'}} onClick={() => {
-                          dispatch(OnlineModeActions.confirmOnlineAction(() => {
-                            open('https://git.door43.org/' + user.username + '/' + projectName)
-                          }))
-                        }}>
-                          {"https://git.door43.org/" + user.username + '/' + projectName}
-                        </a>
-                      </span>
-                    </div>
+                  dispatch(
+                    AlertModalActions.openAlertDialog(
+                      <div>
+                        <span>
+                          <span style={{ fontWeight: 'bold' }}>{user.username + ", "}</span>
+                          {"your project was uploaded successfully to: "}
+                          <a style={{ cursor: 'pointer' }} onClick={() => {
+                            dispatch(OnlineModeActions.confirmOnlineAction(() => {
+                              open('https://git.door43.org/' + user.username + '/' + projectName)
+                            }))
+                          }}>
+                            {"https://git.door43.org/" + user.username + '/' + projectName}
+                          </a>
+                        </span>
+                      </div>
+                    )
                   )
-                )
-              }
-            })
-          }
-        });
-      })
-    }))
+                }
+              })
+            }
+          });
+        })
+      }))
+    } else {
+      const message = "You need to be logged in with a Door43 account to upload a project. Please log in to continue.";
+      return dispatch(AlertModalActions.openAlertDialog(message, false));
+    }
   })
 }
