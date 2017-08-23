e8c34e0a54947afc971a6f2e9d454b8604e1ebe5
mannycolon <colonmanuel7@gmail.com>
2017-08-23 15:05:48 -0400

    fixed Empty project card

--- src/js/components/home/projectsManagement/ProjectCard.js
+++ src/js/components/home/projectsManagement/ProjectCard.js
@@ -7,47 +7,49 @@ import ProjectCardMenu from './ProjectCardMenu'
 
 let ProjectCard = (props) => {
   const { projectName, projectSaveLocation, accessTimeAgo, bookAbbr, bookName, target_language, isSelected } = props.projectDetails;
-  let content = <div />;
-
-  if (projectName && projectSaveLocation && accessTimeAgo && bookAbbr && bookName && target_language) {
-    /**
-    * @description generates a detail for the contentDetails
-    * @param {string} glyph - name of the glyph to be used
-    * @param {string} text - text used for the detail
-    * @return {component} - component returned
-    */
-    const detail = (glyph, text) => {
-      return (
-        <div>
-          <Glyphicon glyph={glyph} style={{ marginRight: '5px', top: '2px' }} />
-          <span>{text}</span>
-        </div>
-      );
+  let cardDetails = [
+    {
+      glyph: 'time',
+      text: accessTimeAgo
+    },
+    {
+      glyph: 'book',
+      text: bookName && bookAbbr ? bookName + ' (' + bookAbbr + ')' : 'No book information found'
+    },
+    {
+      glyph: 'globe',
+      text: target_language.name && target_language.id ? target_language.name + ' (' + target_language.id + ')' : 'No language information found'
     }
+  ]
 
-    // content
-    content = (
-      <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '-10px' }}>
-        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
-          <strong style={{ fontSize: 'x-large' }}>{projectName}</strong>
-          <div style={{ display: 'flex', justifyContent: 'space-between', width: '410px', marginBottom: '6px' }}>
-            {detail('time', accessTimeAgo)}
-            {detail('book', bookName + ' (' + bookAbbr + ')')}
-            {detail('globe', target_language.name + ' (' + target_language.id + ')')}
-          </div>
-        </div>
-        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', textAlign: 'right', marginRight: '-6px' }}>
-          <ProjectCardMenu {...props} />
-          <div>
-            <button className='btn-prime' disabled={isSelected} onClick={() => {props.actions.selectProject(projectSaveLocation)}} style={{ width: '90px', marginBottom: '0' }}>
-              Select
-            </button>
-          </div>
+  // content
+  let content = (
+    <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '-10px' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+        <strong style={{ fontSize: 'x-large' }}>{projectName}</strong>
+        <div style={{ display: 'flex', justifyContent: 'space-between', width: '410px', marginBottom: '6px' }}>
+          {
+            cardDetails.map((cardDetail) => {
+              return (
+                <div key={cardDetail.glyph}>
+                  <Glyphicon glyph={cardDetail.glyph} style={{ marginRight: '5px', top: '2px' }} />
+                  <span>{cardDetail.text}</span>
+                </div>
+              );
+            })
+          }
         </div>
       </div>
-    );
-
-  }
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', textAlign: 'right', marginRight: '-6px' }}>
+        <ProjectCardMenu {...props} />
+        <div>
+          <button className='btn-prime' disabled={isSelected} onClick={() => {props.actions.selectProject(projectSaveLocation)}} style={{ width: '90px', marginBottom: '0' }}>
+            Select
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 
   // heading
   const heading = (
