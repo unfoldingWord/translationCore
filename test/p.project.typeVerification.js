029a1d43822f339e077e699e8b1c57cd65cba79d
RoyalSix <jay.a.scottjr@biola.edu>
2017-09-19 11:13:48 -0700

    added back load online testing

--- test/p.project.typeVerification.js
+++ test/p.project.typeVerification.js
@@ -4,8 +4,8 @@ import fs from 'fs-extra';
 //helpers
 import * as ProjectSelectionHelpers from '../src/js/helpers/ProjectSelectionHelpers';
 //projects
-const OBS_project_1 = window.__base + 'test/fixtures/project/projectVerification/obs_project_1';
-const OBS_project_2 = window.__base + 'test/fixtures/project/projectVerification/obs_project_2';
+const obs_project_1 = window.__base + 'test/fixtures/project/projectVerification/obs_project_1';
+const obs_project_2 = window.__base + 'test/fixtures/project/projectVerification/obs_project_2';
 const multibook_project_1 = window.__base + 'test/fixtures/project/projectVerification/multibook_project_1';
 const multibook_project_2 = window.__base + 'test/fixtures/project/projectVerification/multibook_project_2';
 const en_ta_project = window.__base + 'test/fixtures/project/projectVerification/en_ta';
@@ -13,6 +13,7 @@ const en_tw_project = window.__base + 'test/fixtures/project/projectVerification
 const en_tn_project = window.__base + 'test/fixtures/project/projectVerification/en_tn';
 
 describe('ProjectSelectionHelpers.testResourceByType', () => {
+  //Testing false negatives for resources (tN, tW, tA) and Open Bible Scriptures
   it('should detect project as translationNotes', function (done) {
     let errMessage = ProjectSelectionHelpers.testResourceByType(en_tn_project, 'tn');
     expect(errMessage).to.be.true;
@@ -28,7 +29,18 @@ describe('ProjectSelectionHelpers.testResourceByType', () => {
     expect(errMessage).to.be.true;
     done();
   });
+  it('should detect project as translationWords', function (done) {
+    let errMessage = ProjectSelectionHelpers.testResourceByType(obs_project_1, 'obs');
+    expect(errMessage).to.be.true;
+    done();
+  });
+  it('should detect project as Open Bible Stories', function (done) {
+    let errMessage = ProjectSelectionHelpers.testResourceByType(obs_project_2, 'obs');
+    expect(errMessage).to.be.true;
+    done();
+  });
 
+  //Testing false positives for resources (tN, tW, tA) and Open Bible Scriptures
   it('should not detect project as translationNotes if it isnt', function (done) {
     let errMessage = ProjectSelectionHelpers.testResourceByType(en_ta_project, 'tn');
     expect(errMessage).to.be.false;
@@ -44,9 +56,15 @@ describe('ProjectSelectionHelpers.testResourceByType', () => {
     expect(errMessage).to.be.false;
     done();
   });
+  it('should not detect project as Open Bible Stories if it isnt', function (done) {
+    let errMessage = ProjectSelectionHelpers.testResourceByType(en_tw_project, 'obs');
+    expect(errMessage).to.be.false;
+    done();
+  });
 });
 
 describe('ProjectSelectionHelpers.generalMultiBookProjectSearch', () => {
+  //Testing false negatives for multiple book projects
   it('should detect project as having multiple books', function (done) {
     let numberOfUSFMBooksInProjects = ProjectSelectionHelpers.generalMultiBookProjectSearch(multibook_project_1);
     expect(numberOfUSFMBooksInProjects).to.be.greaterThan(1);
@@ -55,6 +73,18 @@ describe('ProjectSelectionHelpers.generalMultiBookProjectSearch', () => {
   it('should detect project as having multiple books', function (done) {
     let numberOfUSFMBooksInProjects = ProjectSelectionHelpers.generalMultiBookProjectSearch(multibook_project_2);
     expect(numberOfUSFMBooksInProjects).to.be.greaterThan(1);
+    done();
+  });
+
+  //Testing false negatives for multiple book projects
+  it('should not detect project as having multiple books if it isnt', function (done) {
+    let numberOfUSFMBooksInProjects = ProjectSelectionHelpers.generalMultiBookProjectSearch(obs_project_1);
+    expect(numberOfUSFMBooksInProjects).to.not.be.greaterThan(1);
+    done();
+  });
+  it('should not detect project as having multiple books if it isnt', function (done) {
+    let numberOfUSFMBooksInProjects = ProjectSelectionHelpers.generalMultiBookProjectSearch(en_tw_project);
+    expect(numberOfUSFMBooksInProjects).to.not.be.greaterThan(1);
     done();
   });
 });
