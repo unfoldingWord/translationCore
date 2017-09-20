69c43c8ef050718a4b7b9172eec4107e41177821
Joel Lonbeck <joel@neutrinographics.com>
2017-09-19 22:38:38 -0700

    fixed git tests

--- __tests__/GitApi.test.js
+++ __tests__/GitApi.test.js
@@ -93,7 +93,9 @@ describe('GitApi.mirror', () => {
   });
 
   test('mirror should clone a git repo into a directory', () => {
+      jest.setTimeout(5000);
       return new Promise((resolve) => {
+          fs.removeSync('../testRepo');
           GitApi('../testDir').mirror('https://git.door43.org/klappy/blank.git', '../testRepo', function(err) {
               expect(err).toBeNull();
               resolve();
