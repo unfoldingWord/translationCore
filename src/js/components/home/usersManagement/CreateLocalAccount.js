09ff99321f5d53ba178a63ecd93c6ba6d43ae581
mannycolon <colonmanuel7@gmail.com>
2017-09-22 11:08:37 -0400

    rebranded 'local user' as 'guest'

--- src/js/components/home/usersManagement/CreateLocalAccount.js
+++ src/js/components/home/usersManagement/CreateLocalAccount.js
@@ -52,14 +52,13 @@ class CreateLocalAccount extends Component {
       <div>
         <p style={{ fontSize: 20, fontWeight: 'bold' }}>Attention</p>
         <p>You have chosen to be known as "
-                    <span style={{ fontWeight: 'bold', color: 'var(--accent-color-dark)' }}>{this.state.localUsername}</span>
-          ". This username will be publicly viewable.
-                    <br /><br />
-          If you are not comfortable with being known as "
-                    <span style={{ fontWeight: 'bold', color: 'var(--accent-color-dark)' }}>{this.state.localUsername}</span>
-          ", you may <span style={{ fontWeight: 'bold', color: 'var(--accent-color-dark)' }}>Cancel </span>
-          and enter a new name.
-                </p>
+          <span style={{ fontWeight: 'bold', color: 'var(--accent-color-dark)' }}>{this.state.localUsername}</span>
+            ". This username will be publicly viewable.<br /><br />
+            If you are not comfortable with being known as "
+          <span style={{ fontWeight: 'bold', color: 'var(--accent-color-dark)' }}>{this.state.localUsername}</span>
+            ", you may <span style={{ fontWeight: 'bold', color: 'var(--accent-color-dark)' }}>Cancel </span>
+            and enter a new name.
+        </p>
       </div>
     )
   }
@@ -67,7 +66,7 @@ class CreateLocalAccount extends Component {
   loginButtons() {
     const loginEnabled = this.state.localUsername && this.state.checkBoxChecked ? true : false;
     const callback = (result) => {
-      if (result == "Create Account") this.props.loginUser({username:this.state.localUsername}, true);
+      if (result == "Continue") this.props.loginUser({username:this.state.localUsername}, true);
       this.props.actions.closeAlert();
     }
     return (
@@ -77,14 +76,14 @@ class CreateLocalAccount extends Component {
           style={{ width: 150, margin: "40px 10px 0px 0px" }}
           onClick={() => this.props.setView('main')}>
           Go Back
-                </button>
+        </button>
         <button
           className={loginEnabled ? "btn-prime" : "btn-prime-reverse"}
           disabled={!loginEnabled}
           style={{ width: 200, margin: "40px 0px 0px 10px" }}
-          onClick={() => this.props.actions.openOptionDialog(this.localUserWarning(), callback, "Create Account", "Cancel")}>
-          Create
-                </button>
+          onClick={() => this.props.actions.openOptionDialog(this.localUserWarning(), callback, "Continue", "Cancel")}>
+          Continue
+        </button>
       </div>
     )
   }
@@ -93,17 +92,14 @@ class CreateLocalAccount extends Component {
     return (
       <div style={{ display: 'flex', justifyContent: "center", alignItems: 'center', width: '100%' }}>
         {this.agreeCheckBox()}
-        <span>
-          I have read and agree to the
-                    </span>
-        &nbsp;
-                    <a
+        <span>I have read and agree to the</span>&nbsp;
+        <a
           style={{ cursor: "pointer", textDecoration: "none" }}
           onClick={() =>
             this.infoPopup("Terms and Conditions")
           }>
           terms and conditions
-                    </a>
+        </a>
       </div>
     )
   }
@@ -132,7 +128,7 @@ class CreateLocalAccount extends Component {
     return (
       <MuiThemeProvider>
       <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', width: '100%' }}>
-        <div style={{ fontSize: 25, fontWeight: 100, padding: '0px' }}>New Local User</div>
+        <div style={{ fontSize: 25, fontWeight: 100, padding: '0px' }}>New Guest</div>
         <span style={{ color: 'grey' }}>This is publicly visible</span>
         {this.localUsernameInput()}
         {this.termsAndConditionsAgreement()}
