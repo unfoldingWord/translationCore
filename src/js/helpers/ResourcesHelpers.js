40b9b196bf6dd522ec17dd152dfa22bccd0b59b8
Christopher Klapp <christopher@klapp.name>
2017-09-29 12:56:55 -0400

    Generate default Book Chapter Verse Index when none present

--- src/js/helpers/ResourcesHelpers.js
+++ src/js/helpers/ResourcesHelpers.js
@@ -11,8 +11,8 @@ const STATIC_RESOURCES_PATH = path.join(__dirname, '../../../tC_resources/resour
 export function getBibleFromStaticPackage(force = false) {
   let languagesIds = ['en', 'grc', 'he']; // english, greek, hebrew.
   languagesIds.forEach((languagesId) => {
-    const STATIC_RESOURCES_BIBLES_PATH = path.join(__dirname, '../../../tC_resources/resources', languagesId, 'bibles');
-    const BIBLE_RESOURCES_PATH = path.join(path.homedir(), 'translationCore/resources', languagesId, 'bibles');
+    const STATIC_RESOURCES_BIBLES_PATH = path.join(STATIC_RESOURCES_PATH, languagesId, 'bibles');
+    const BIBLE_RESOURCES_PATH = path.join(USER_RESOURCES_PATH, languagesId, 'bibles');
     let bibleNames = fs.readdirSync(STATIC_RESOURCES_BIBLES_PATH);
     bibleNames.forEach((bibleName) => {
       let bibleSourcePath = path.join(STATIC_RESOURCES_BIBLES_PATH, bibleName);
@@ -60,7 +60,7 @@ export function getLexiconsFromStaticPackage(force = false) {
 
 export function copyGroupsIndexToProjectResources(currentToolName, projectGroupsIndexPath) {
   const languageId = 'en';
-  const version = currentToolName === 'translationWords' ? 'v6' : 'V0';
+  const version = currentToolName === 'translationWords' ? 'v6' : 'v0';
   const groupsIndexPath = currentToolName === 'translationWords' ? path.join('kt', 'index.json') : 'index.json';
   const groupsIndexSourcePath = path.join(USER_RESOURCES_PATH, languageId, 'translationHelps', currentToolName, version, groupsIndexPath);
   const groupsIndexDestinationPath = path.join(projectGroupsIndexPath,'index.json');
@@ -68,9 +68,24 @@ export function copyGroupsIndexToProjectResources(currentToolName, projectGroups
   if(fs.existsSync(groupsIndexSourcePath)) {
     fs.copySync(groupsIndexSourcePath, groupsIndexDestinationPath);
   } else {
-    console.log("translationHelps resources path was not found, " + groupsIndexSourcePath);
+    copyChapterGroupsIndexToProjectResources(groupsIndexDestinationPath);
+    console.log("Chapter Groups Index generated. translationHelps resources path was not found, " + groupsIndexSourcePath);
   }
 }
+/**
+ * @description - Auto generate the chapter index since more projects will use it
+ * @param {String} groupsIndexDestinationPath - path to store the index
+ */
+export const copyChapterGroupsIndexToProjectResources = (groupsIndexDestinationPath) => {
+  const groupsIndex = Array(150).fill().map((_, i) => {
+    let chapter = i + 1;
+    return {
+      id: 'chapter_' + chapter,
+      name: 'Chapter ' + chapter
+    };
+  });
+  fs.outputJsonSync(groupsIndexDestinationPath, groupsIndex);
+};
 
 export function copyGroupsDataToProjectResources(currentToolName, groupsDataDirectory, bookAbbreviation) {
   const languageId = 'en';
@@ -81,9 +96,43 @@ export function copyGroupsDataToProjectResources(currentToolName, groupsDataDire
   if(fs.existsSync(groupsDataSourcePath)) {
     fs.copySync(groupsDataSourcePath, groupsDataDirectory);
   } else {
-    console.log("translationHelps resources path was not found, " + groupsDataSourcePath);
+    copyChapterGroupsDataToProjectResources(bookAbbreviation, currentToolName, groupsDataDirectory);
+    console.log("Chapter Groups Data generated. translationHelps resources path was not found, " + groupsDataSourcePath);
   }
 }
+/**
+ * @description - Auto generate the chapter index since more projects will use it
+ * @param {String} bookId - id of the current book
+ * @param {String} toolId - id of the current tool
+ * @param {String} groupsDataDirectory - path to store the index
+ */
+export const copyChapterGroupsDataToProjectResources = (bookId, currentToolName, groupsDataDirectory) => {
+  const ulbIndexPath = path.join(STATIC_RESOURCES_PATH, 'en', 'bibles', 'ulb', 'v10', 'index.json');
+  if (fs.existsSync(ulbIndexPath)) { // make sure it doens't crash if the path doesn't exist
+    const ulbIndex = fs.readJsonSync(ulbIndexPath); // the index of book/chapter/verses
+    const bookData = ulbIndex[bookId]; // get the data in the index for the current book
+    Array(bookData.chapters).fill().map((_, i) => { // create array from number of chapters
+      const chapter = i + 1; // index is 0 based, so add one for chapter number
+      const verses = bookData[chapter]; // get the number of verses in the chapter
+      const groupData = Array(verses).fill().map((_, i) => { // turn number of verses into array
+        const verse = i + 1; // index is 0 based, so add one for verse number
+        return {
+          "contextId": {
+            "reference": {
+              "bookId": bookId,
+              "chapter": chapter,
+              "verse": verse
+            },
+            "tool": currentToolName,
+            "groupId": "chapter_" + chapter
+          }
+        };
+      });
+      const chapterIndexPath = path.join(groupsDataDirectory, 'chapter_' + chapter + '.json');
+      fs.outputFileSync(chapterIndexPath, JSON.stringify(groupData, null, 2));
+    });
+  }
+};
 
 /**
  * @description Helper function to get a bibles manifest file from the bible resources folder.
