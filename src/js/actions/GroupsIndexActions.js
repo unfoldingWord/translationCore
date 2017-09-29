ffa357c86ef42e182703176d43a3980382086099
Christopher Klapp <christopher@klapp.name>
2017-09-29 12:57:19 -0400

    Add support for sorting Chapter numbers in index

--- src/js/actions/GroupsIndexActions.js
+++ src/js/actions/GroupsIndexActions.js
@@ -10,6 +10,12 @@ export const loadGroupsIndex = (groupsIndex) => {
   return ((dispatch) => {
     // Alphabetize the groups order
     groupsIndex = groupsIndex.sort((a, b) => {
+      // try to sort on the number
+      if (a.id.match(/\d+/) && b.id.match(/\d+/)) { // if both contain digits
+        if (a.id.replace(/\d+/,'') === b.id.replace(/\d+/,'')) { // if string is the same other than digits
+          return parseInt(a.id.replace(/[^\d]+/,'')) - parseInt(b.id.replace(/[^\d]+/,'')); // sort on the number
+        }
+      }
       if (a.id.toUpperCase() < b.id.toUpperCase()) {
         return -1;
       }
