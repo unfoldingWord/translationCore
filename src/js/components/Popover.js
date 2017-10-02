2e8f8f7e8b1113fe692b67412e1e7c96741cfbbe
RoyalSix <jay.a.scottjr@biola.edu>
2017-10-02 12:13:38 -0700

    added popover close icon

--- src/js/components/Popover.js
+++ src/js/components/Popover.js
@@ -1,27 +1,31 @@
-import React, {Component} from 'react';
-import {Popover, Divider} from 'material-ui';
+import React, { Component } from 'react';
+import { Popover, Divider } from 'material-ui';
+import CloseIcon from 'material-ui/svg-icons/navigation/close';
 
 export default class PopoverComponent extends Component {
   render() {
-    let {popoverVisibility, title, bodyText, positionCoord, onClosePopover} = this.props;
+    let { popoverVisibility, title, bodyText, positionCoord, onClosePopover } = this.props;
     if (!popoverVisibility) {
       return (<div></div>);
     } else {
       return (
         <div>
           <Popover
-            style={{padding: '10px 0', backgroundColor: "var(--background-color-light)"}}
+            style={{ padding: '10px 0', backgroundColor: "var(--background-color-light)" }}
             open={popoverVisibility}
             anchorEl={positionCoord}
-            anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
-            targetOrigin={{horizontal: 'left', vertical: 'top'}}
+            anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }}
+            targetOrigin={{ horizontal: 'left', vertical: 'top' }}
             onRequestClose={onClosePopover}
           >
-            <span style={{padding: '0 10px'}}>
-              {title}
-            </span>
+            <div style={{display:'flex'}}>
+              <span style={{ padding: 0 }}>
+                {title}
+              </span>
+              <CloseIcon style={{ color: 'red', alignItems:'center', marginLeft: 'auto', marginBottom: 5, marginRight: 5 }} />
+            </div>
             <Divider />
-            <span style={{padding: '0 20px'}}>
+            <span style={{ padding: '0 20px' }}>
               {bodyText}
             </span>
           </Popover>
