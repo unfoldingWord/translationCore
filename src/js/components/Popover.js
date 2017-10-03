4e6cccbec764691ba48638a7d15853546b97f1d5
RoyalSix <jay.a.scottjr@biola.edu>
2017-10-03 16:01:02 -0700

    fixed styling on popover

--- src/js/components/Popover.js
+++ src/js/components/Popover.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Popover, Divider } from 'material-ui';
-import CloseIcon from 'material-ui/svg-icons/navigation/close';
+import { Glyphicon } from 'react-bootstrap';
 
 export default class PopoverComponent extends Component {
   render() {
@@ -11,18 +11,24 @@ export default class PopoverComponent extends Component {
       return (
         <div>
           <Popover
-            style={{ padding: '10px 0', backgroundColor: "var(--background-color-light)" }}
+            style={{ padding:0 , backgroundColor: "var(--background-color-light)" }}
             open={popoverVisibility}
             anchorEl={positionCoord}
             anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }}
             targetOrigin={{ horizontal: 'left', vertical: 'top' }}
             onRequestClose={onClosePopover}
           >
-            <div style={{ display: 'flex' }}>
-              <span style={{ padding: 0 }}>
+            <div style={{ display: 'flex', alignItems:'center', padding:'5px 0px' }}>
+              <span style={{ marginLeft: 5 }}>
                 {title}
               </span>
-              <CloseIcon onClick={onClosePopover} style={{ cursor: 'pointer', color: 'red', alignItems: 'center', marginLeft: 'auto', marginBottom: 5, marginRight: 5 }} />
+              <Glyphicon glyph={"remove"}
+                style={{
+                  color: "var(--text-color-light)",
+                  cursor: 'pointer',alignItems: 'center',
+                  marginLeft: 'auto', marginRight: 5
+                }}
+                onClick={onClosePopover} />
             </div>
             <Divider />
             <span style={{ padding: '0 20px' }}>
