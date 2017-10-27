340a3dfc3164fd14b2e56e7712f177729b87ed65
mannycolon <colonmanuel7@gmail.com>
2017-10-27 15:51:15 -0400

    added tests appropiately

--- __tests__/RemindersActions.test.js
+++ __tests__/RemindersActions.test.js
@@ -1,27 +1,25 @@
 import consts from '../src/js/actions/ActionTypes';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
+import * as actions from '../src/js/actions/RemindersActions';
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
 describe('RemindersActions.toggleReminder', () => {
   test('Toggle reminder', () => {
-      const expectedAction = {
-        type: consts.TOGGLE_REMINDER,
-        modifiedTimestamp: "2017-10-27T18:13:41.455Z",
-        userName: 'mannycolon'
-      };
-      const store = mockStore({
-        enabled: false,
-        userName: null,
-        modifiedTimestamp: null
-      });
-      
-      expect(store.dispatch({
-        type: consts.TOGGLE_REMINDER,
-        modifiedTimestamp: "2017-10-27T18:13:41.455Z",
-        userName: 'mannycolon'
-      })).toEqual(expectedAction);
+    const expectedAction = {
+      type: consts.TOGGLE_REMINDER,
+      modifiedTimestamp: "2017-10-27T18:13:41.455Z",
+      userName: 'mannycolon'
+    };
+    const store = mockStore({
+      enabled: false,
+      userName: null,
+      modifiedTimestamp: null
+    });
+    expect(store.dispatch(
+      actions.toggle('mannycolon', "2017-10-27T18:13:41.455Z")
+    )).toEqual(expectedAction);
   });
 });
--- src/js/actions/CommentsActions.js
+++ src/js/actions/CommentsActions.js
@@ -2,22 +2,26 @@ import consts from './ActionTypes';
 // helpers
 import {generateTimestamp} from '../helpers/index';
 
+export function comment(text, username, timestamp) {
+  return {
+    type: consts.ADD_COMMENT,
+    modifiedTimestamp: timestamp,
+    text,
+    userName: username
+  };
+}
+
 /**
  * @description this action adds a comment to the current check.
  * @param {string} text - comment text.
- * @param {string} userName - Alias name.
+ * @param {string} username - Alias name.
  * @return {object} New state for comment reducer.
  */
-export const addComment = (text, userName) => {
+export const addComment = (text, username) => {
   return ((dispatch, getState) => {
     let state = getState();
     let contextId = state.contextIdReducer.contextId;
-    dispatch({
-      type: consts.ADD_COMMENT,
-      modifiedTimestamp: generateTimestamp(),
-      text,
-      userName
-    });
+    dispatch(comment(text, username, generateTimestamp()));
     dispatch({
       type: consts.TOGGLE_COMMENTS_IN_GROUPDATA,
       contextId,
