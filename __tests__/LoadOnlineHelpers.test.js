cb347c52ddb8d9cce6f9a98cbdc888910d6e347c
Bruce McLean <mcleanb@photo-nomad.net>
2017-10-30 16:49:57 -0400

    LoadOnlineHelpers - added git mocking importOnlineActions - fixed error messages and moved methods to LoadOnlineHelpers for testing.

--- __tests__/LoadOnlineHelpers.test.js
+++ __tests__/LoadOnlineHelpers.test.js
@@ -4,75 +4,149 @@ import path from 'path-extra';
 import fs from 'fs-extra';
 //helpers
 import * as loadOnline from '../src/js/helpers/LoadOnlineHelpers';
-
-// TODO: this should be re-written to mock disk and network I/O.
+require('jest');
 jest.unmock('simple-git');
 
-describe('loadOnline.openManifest', function () {
+describe('LoadOnlineHelpers.importOnlineProjectFromUrl', function () {
 
-    test('loadOnline.openManifest should return an error if no link is specified', () => {
-        return new Promise((resolve) => {
-            loadOnline.openManifest(null, function (err, savePath, url) {
-                expect(typeof err.text).toEqual('string');
-                expect(savePath).toBeNull();
-                expect(url).toBeNull();
-                expect(err.text).toEqual('No link specified');
-                resolve();
-            });
+    let returnedErrorMessage;
+    let returnedUrl;
+    let returnedSavePath;
+
+    beforeEach(() => {
+        // clear out test projects
+        ['bhadrawahi_tit', 'noprojecthere'].forEach((file) => {
+            const savePath = path.join(path.homedir(), 'translationCore', 'projects', file);
+            fs.removeSync(savePath); // clear out
         });
+
+        returnedErrorMessage = "INVALID";
+        returnedUrl = "INVALID";
+        returnedSavePath = "INVALID";
     });
 
-    test('loadOnline.openManifest should fail on an invalid link.', () => {
-        jest.setTimeout(5000);
+    test('LoadOnlineHelpers.importOnlineProjectFromUrl should succeed on valid URL', () => {
         return new Promise((resolve) => {
-            loadOnline.openManifest('https://git.door43.org/klappy/noprojecthere.git', function (err, savePath, url) {
-                expect(typeof err.text).toEqual('string');
-                resolve();
-            });
-        });
-    });
-
-    test('loadOnline.openManifest should not deny a non .git link.', () => {
-        return new Promise((resolve) => {
-            const expectedSavePath = path.join(path.homedir(), 'translationCore', 'projects', 'bhadrawahi_tit');
-            const expectedURL = 'https://git.door43.org/klappy/bhadrawahi_tit';
-            loadOnline.openManifest(expectedURL, function (err, savePath, url) {
-                expect(savePath).toEqual(expectedSavePath);
-                expect(url).toEqual(expectedURL + '.git');
-                fs.removeSync(savePath);
-                resolve();
-            });
-        });
-    });
-
-    test('loadOnline.openManifest should return the home directory and url', () => {
-        return new Promise((resolve) => {
-            const expectedSavePath = path.join(path.homedir(), 'translationCore', 'projects', 'bhadrawahi_tit');
+            const URL = 'https://git.door43.org/klappy/bhadrawahi_tit';
             const expectedURL = 'https://git.door43.org/klappy/bhadrawahi_tit.git';
-            loadOnline.openManifest(expectedURL, function (err, savePath, url) {
-                expect(err).toBeNull();
-                expect(savePath).toEqual(expectedSavePath);
-                expect(url).toEqual(expectedURL);
-                fs.removeSync(savePath);
-                resolve();
-            });
+            const expectedErrorStr = null;
+            const gitErrorMessage = null;
+            const {mock_git, mock_dispatch, mock_handleImportResults} = mockImport(gitErrorMessage);
+            loadOnline.importOnlineProjectFromUrl(URL, mock_dispatch, mock_handleImportResults, mock_git);
+
+            fs.removeSync(returnedSavePath);
+            expect(returnedErrorMessage).toEqual(expectedErrorStr);
+            expect(returnedUrl).toEqual(expectedURL);
+            expect(returnedSavePath).not.toBeNull();
+            resolve();
         });
     });
 
-    test('loadOnline.processGitMirrorResponse with missing source should throw error', () => {
+    test('LoadOnlineHelpers.importOnlineProjectFromUrl null link should show error', () => {
         return new Promise((resolve) => {
-            const expectedSavePath = path.join(path.homedir(), 'translationCore', 'projects', 'sw_tit_text_ulb');
-            const expectedURL = 'https://git.door43.org/Danjuma_Alfred_H/sw_tit_text_ulb.git';
-            const err = "Cloning into '" + expectedSavePath + "'...\nfatal: repository '" + expectedURL + "' not found\n";
-            const expectedErrorStr = "Project not found";
+            const expectedURL = null;
+            const expectedErrorStr = "No link specified";
+            const gitErrorMessage = null;
+            const {mock_git, mock_dispatch, mock_handleImportResults} = mockImport(gitErrorMessage);
+            loadOnline.importOnlineProjectFromUrl(expectedURL, mock_dispatch, mock_handleImportResults, mock_git);
 
-            loadOnline.processGitMirrorResponse(err, expectedSavePath, expectedURL, function (err, savePath, url) {
-                const containsErrorString = expectedErrorStr.indexOf(expectedErrorStr) >= 0;
-                expect(containsErrorString).toEqual(true);
-                expect(savePath).toBeNull();
-                expect(url).toBeNull();
-                resolve();
-            });
+            expect(returnedErrorMessage).toEqual(expectedErrorStr);
+            expect(returnedUrl).toBeNull();
+            expect(returnedSavePath).toBeNull();
+            resolve();
         });
     });
+
+    test('LoadOnlineHelpers.importOnlineProjectFromUrl should handle missing .git', () => {
+        return new Promise((resolve) => {
+            const URL = 'https://git.door43.org/klappy/bhadrawahi_tit';
+            const expectedURL = 'https://git.door43.org/klappy/bhadrawahi_tit.git';
+            const expectedErrorStr = null;
+            const gitErrorMessage = null;
+            const {mock_git, mock_dispatch, mock_handleImportResults} = mockImport(gitErrorMessage);
+            loadOnline.importOnlineProjectFromUrl(URL, mock_dispatch, mock_handleImportResults, mock_git);
+
+            fs.removeSync(returnedSavePath);
+            expect(returnedErrorMessage).toEqual(expectedErrorStr);
+            expect(returnedUrl).toEqual(expectedURL);
+            expect(returnedSavePath).not.toBeNull();
+            resolve();
+        });
+    });
+
+    test('LoadOnlineHelpers.importOnlineProjectFromUrl with access error should show error', () => {
+        return new Promise((resolve) => {
+            const expectedURL = 'https://git.door43.org/Danjuma_Alfred_H/sw_tit_text_ulb.git';
+            const expectedErrorStr = "Unable to connect to the server. Please check your Internet connection.";
+            const gitErrorMessage = "Cloning into 'xxx'...\nfatal: unable to access\n";
+            const {mock_git, mock_dispatch, mock_handleImportResults} = mockImport(gitErrorMessage);
+            loadOnline.importOnlineProjectFromUrl(expectedURL, mock_dispatch, mock_handleImportResults, mock_git);
+
+            expect(returnedErrorMessage.toString().includes(expectedErrorStr)).toBe(true);
+            resolve();
+        });
+    });
+
+    test('LoadOnlineHelpers.importOnlineProjectFromUrl with disconnect should show error', () => {
+        return new Promise((resolve) => {
+            const expectedURL = 'https://git.door43.org/Danjuma_Alfred_H/sw_tit_text_ulb.git';
+            const expectedErrorStr = "Unable to connect to the server. Please check your Internet connection.";
+            const gitErrorMessage = "Cloning into 'xxx'...\nfatal: The remote end hung up\n";
+            const {mock_git, mock_dispatch, mock_handleImportResults} = mockImport(gitErrorMessage);
+            loadOnline.importOnlineProjectFromUrl(expectedURL, mock_dispatch, mock_handleImportResults, mock_git);
+
+            expect(returnedErrorMessage.toString().includes(expectedErrorStr)).toBe(true);
+            resolve();
+        });
+    });
+
+    test('LoadOnlineHelpers.importOnlineProjectFromUrl failed to load should show error', () => {
+        return new Promise((resolve) => {
+            const expectedURL = 'https://git.door43.org/Danjuma_Alfred_H/sw_tit_text_ulb.git';
+            const expectedErrorStr = "Unable to connect to the server. Please check your Internet connection.";
+            const gitErrorMessage = "Cloning into 'xxx'...\nfatal: Failed to load\n";
+            const {mock_git, mock_dispatch, mock_handleImportResults} = mockImport(gitErrorMessage);
+            loadOnline.importOnlineProjectFromUrl(expectedURL, mock_dispatch, mock_handleImportResults, mock_git);
+
+            expect(returnedErrorMessage.toString().includes(expectedErrorStr)).toBe(true);
+            resolve();
+        });
+    });
+
+    test('LoadOnlineHelpers.importOnlineProjectFromUrl with missing source should show error', () => {
+        return new Promise((resolve) => {
+            const expectedURL = 'https://git.door43.org/Danjuma_Alfred_H/sw_tit_text_ulb.git';
+            const expectedErrorStr = "Project not found";
+            const gitErrorMessage = "Cloning into 'xxx'...\nfatal: repository '" + expectedURL + "' not found\n";
+            const {mock_git, mock_dispatch, mock_handleImportResults} = mockImport(gitErrorMessage);
+            loadOnline.importOnlineProjectFromUrl(expectedURL, mock_dispatch, mock_handleImportResults, mock_git);
+
+            expect(returnedErrorMessage.toString().includes(expectedErrorStr)).toBe(true);
+            resolve();
+        });
+    });
+
+    //
+    // helpers
+    //
+
+    function mockImport(gitErrorMessage) {
+        const mock_git = jest.fn();
+        mock_git.mockReturnValue({
+            mirror: function (url, path, callback) {
+                if (callback) {
+                    callback(gitErrorMessage);
+                }
+            }
+        });
+        const mock_dispatch = jest.fn();
+        mock_dispatch.mockReturnValue(true);
+        const mock_handleImportResults = jest.fn((dispatch, url, savePath, errMessage) => {
+            returnedErrorMessage = errMessage;
+            returnedUrl = url;
+            returnedSavePath = savePath;
+        });
+        return {mock_git, mock_dispatch, mock_handleImportResults};
+    }
+
 });
