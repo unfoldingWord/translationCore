1b1e3ac08c8e10b45453999518802c69522fe732
Bruce McLean <mclean.b@icloud.com>
2017-11-01 11:19:26 -0400

    LoadOnlineHelpers - change clone warning when repo is not found (#3014)
    * LoadOnlineHelpers - change clone warning when repo is not found
    
    * LoadOnlineHelpers - updated load failed message to show url
    
    * LoadOnlineHelpers - added git mocking
    importOnlineActions - fixed error messages and moved methods to LoadOnlineHelpers for testing.
    
    * cleanup
    
    * cleanup

--- src/js/actions/ImportOnlineActions.js
+++ src/js/actions/ImportOnlineActions.js
@@ -38,6 +38,31 @@ export function updateRepos() {
     });
 }
 
+/**
+ * @description handles the import results.  If errMessage is not null then displays the error message.  Otherwise
+ *                  it verifies and selects the project.
+ * @param {module} dispatch
+ * @param {string} url - url of project imported
+ * @param {string} savePath - destination folder for project
+ * @param {string} errMessage - if not null, then error message returned from load
+ */
+function handleImportResults(dispatch, url, savePath, errMessage) {
+  if (errMessage) {
+    dispatch(AlertModalActions.openAlertDialog(errMessage));
+    dispatch({type: "LOADED_ONLINE_FAILED"});
+    dispatch({type: consts.RESET_IMPORT_ONLINE_REDUCER});
+  } else {
+    dispatch({type: consts.RESET_IMPORT_ONLINE_REDUCER});
+    dispatch(clearLink());
+    dispatch(AlertModalActions.closeAlertDialog());
+    dispatch(ImportLocalActions.verifyAndSelectProject(savePath, url));
+  }
+}
+
+/**
+ * @description import online project
+ * @return {function(*=, *)}
+ */
 export function importOnlineProject() {
   return ((dispatch, getState) => {
     const link = getState().importOnlineReducer.importLink;
@@ -45,33 +70,7 @@ export function importOnlineProject() {
       dispatch(
         AlertModalActions.openAlertDialog("Importing " + link + " Please wait...", true)
       );
-
-      loadOnline.openManifest(link, function (err, savePath, url) {
-        if (err) {
-          let errmessage = "An unknown problem occurred during import";
-
-          if (err.toString().includes("fatal: unable to access")) {
-              errmessage = "Unable to connect to the server. Please check your Internet connection.";
-          } else if (err.toString().includes("fatal: The remote end hung up")) {
-              errmessage = "Unable to connect to the server. Please check your Internet connection.";
-          } else if (err.toString().includes("Failed to load")) {
-              errmessage = "Unable to connect to the server. Please check your Internet connection.";
-          } else if (err.toString().includes("fatal: repository")) {
-              errmessage = "The URL does not reference a valid project";
-          } else if (err.type && err.type === "custom") {
-              errmessage = err.text;
-          }
-
-          dispatch(AlertModalActions.openAlertDialog(errmessage));
-          dispatch({ type: "LOADED_ONLINE_FAILED" });
-          dispatch({ type: consts.RESET_IMPORT_ONLINE_REDUCER });
-        } else {
-          dispatch({ type: consts.RESET_IMPORT_ONLINE_REDUCER });
-          dispatch(clearLink());
-          dispatch(AlertModalActions.closeAlertDialog());
-          dispatch(ImportLocalActions.verifyAndSelectProject(savePath, url));
-        }
-      });
+      loadOnline.importOnlineProjectFromUrl(link, dispatch, handleImportResults);
     }));
   });
 }
