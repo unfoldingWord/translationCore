2a47ad8ffd607b6402e9791f4badef4db715b737
richmahn <rich.mahn@unfoldingword.org>
2017-11-02 14:57:51 -0600

    Adds test for ProjectFAB

---
+++ __tests__/ProjectFAB.test.js
@@ -0,0 +1,13 @@
+/* eslint-env jest */
+
+import React from 'react';
+import ProjectFAB from '../src/js/components/home/projectsManagement/ProjectsFAB';
+import renderer from 'react-test-renderer';
+
+// Tests for ProjectFAB React Component
+describe('Test ProjectFAB component',()=>{
+  test('Comparing ProjectFAB component render with snapshot taken 11/02/2017 in __snapshots__ should match', () => {
+      const renderedValue =  renderer.create(<ProjectFAB homeScreenReducer={{showFABOptions: true}} />).toJSON();
+      expect(renderedValue).toMatchSnapshot();
+  });
+});
