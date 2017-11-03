e32d4bd23e0bb159b1f5762c3124f9dcaa571588
Bruce McLean <mcleanb@photo-nomad.net>
2017-11-03 16:04:58 -0400

    SearchOptions.test.js - fixed test to check the floating labels for search options.

--- __tests__/SearchOptions.test.js
+++ __tests__/SearchOptions.test.js
@@ -8,26 +8,56 @@ require('jest');
 
 // Tests for ProjectFAB React Component
 describe('Test SearchOptions component',()=>{
-  test('Comparing SearchOptions Component render with snapshot', () => {
-    // const renderedValue =  renderer.create(<ProjectFAB homeScreenReducer={{showFABOptions: true}} />).toJSON();
+  test('Comparing SearchOptions Component should render with snapshot and have "Language Code" on floating label', () => {
     const userName = "dummy";
     const mock_searchReposByUser = jest.fn();
     mock_searchReposByUser.mockReturnValue(true);
     const mock_actions = { searchReposByUser: mock_searchReposByUser };
-
     const importLink = "link";
+    const expectedSearchLabels = ['User','Language Code','Book'];
 
     const renderedValue =  renderer.create(
       <MuiThemeProvider>
         <SearchOptions actions={mock_actions} importLink={importLink} username={userName} />
       </MuiThemeProvider>
     ).toJSON();
-    expect(renderedValue).toMatchSnapshot();
+    
+    const labels = searchForChildren(renderedValue, 'label');
+    const labelsText = getDisplayedText(labels);
+    expect(labelsText).toEqual(expectedSearchLabels);
   });
+
+  //
+  // Helper
+  //
+
+  function getDisplayedText(labels) {
+    const labelsText = [];
+    labels.forEach((labels) => {
+      labels.forEach((item) => {
+        if (typeof item === 'string') {
+          labelsText.push(item);
+        }
+      });
+    });
+    return labelsText;
+  }
+
+  function searchForChildren(search, find) {
+    let found = [];
+    if (search.children) {
+      search.children.forEach((child) => {
+        if (child.type === find) {
+          found.push(child.children);
+        } else if(child.children) {
+          const found_below = searchForChildren(child, find);
+          if(found_below.length) {
+            found = found.concat(found_below);
+          }
+        }
+      });
+    }
+    return found;
+  }
 });
 
-{/*<SearchOptions*/}
-  {/*actions={this.props.actions}*/}
-  {/*importLink={importLink}*/}
-  {/*username={userdata.username}*/}
-{/*/>*/}
