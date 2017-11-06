9908a04487d5db9a2f69a3eea9c8faa38bb05395
richmahn <rich.mahn@unfoldingword.org>
2017-11-06 07:12:02 -0700

    Adds unit test for ProjectCard

---
+++ __tests__/ProjectCard.test.js
@@ -0,0 +1,37 @@
+/* eslint-env jest */
+
+import React from 'react';
+import ProjectCard from '../src/js/components/home/projectsManagement/ProjectCard';
+import renderer from 'react-test-renderer';
+import * as ProjectSelectionActions from '../src/js/actions/ProjectSelectionActions';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+// Tests for ProjectFAB React Component
+describe('Test ProjectFAB component',()=>{
+  test('Comparing ProjectCard Component render with snapshot taken 11/06/2017 in __snapshots__ should match', () => {
+    const projectDetails = {
+      projectName: 'en_1co_ulb', 
+      projectSaveLocation: '/tmp/en_1co_ulb', 
+      accessTimeAgo: '5 days ago', 
+      bookAbbr: '1co', 
+      bookName: '1 Corinthians', 
+      target_language: 'en', 
+      isSelected: false
+    };
+    const actions = {
+      selectProject: (projectPath) => {
+        dispatch(ProjectSelectionActions.selectProject(projectPath));
+      }
+    };
+    const renderedValue =  renderer.create(
+      <MuiThemeProvider>
+        <ProjectCard 
+          user={"johndoe"} 
+          key={"en_1co"} 
+          projectDetails={projectDetails} 
+          actions={actions} />
+      </MuiThemeProvider>
+    ).toJSON();
+    expect(renderedValue).toMatchSnapshot();
+  });
+});
