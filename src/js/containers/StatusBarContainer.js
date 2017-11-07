e97888e2f1552c684d0d28e92dde1f475111daa1
Bruce McLean <mcleanb@photo-nomad.net>
2017-11-07 07:29:34 -0500

    StatusBarContainer.js - tweak to path.

--- src/js/containers/StatusBarContainer.js
+++ src/js/containers/StatusBarContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-const path = require('path');
+import path from 'path-extra';
 // Components
 import StatusBar from '../components/StatusBar';
 // Actions
