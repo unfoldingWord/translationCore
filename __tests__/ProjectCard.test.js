8d76bfdfaffaa544005b0662de6dd7cba7b20b09
Bruce McLean <mcleanb@photo-nomad.net>
2017-11-10 12:13:12 -0500

    Merge branch 'develop' of https://github.com/unfoldingWord-dev/translationCore into bugFix/brucem_2955_Windows_menu_bar_shows_full_path_of_the_current_project
    # Conflicts:
    #	package-lock.json

--- __tests__/ProjectCard.test.js
+++ __tests__/ProjectCard.test.js
@@ -3,36 +3,33 @@
 import React from 'react';
 import ProjectCard from '../src/js/components/home/projectsManagement/ProjectCard';
 import renderer from 'react-test-renderer';
-import * as ProjectSelectionActions from '../src/js/actions/ProjectSelectionActions';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 // Tests for ProjectCard React Component
 describe('Test ProjectCard component',()=>{
   test('Comparing ProjectCard Component render with snapshot taken 11/06/2017 in __snapshots__ should match', () => {
-    const projectDetails = {
-      projectName: 'en_1co_ulb', 
-      projectSaveLocation: '/tmp/en_1co_ulb', 
-      accessTimeAgo: '5 days ago', 
-      bookAbbr: '1co', 
-      bookName: '1 Corinthians', 
-      target_language: {
-        id: 'en',
-        name: 'English'
-      }, 
-      isSelected: false
-    };
-    const actions = {
-      selectProject: (projectPath) => {
-        dispatch(ProjectSelectionActions.selectProject(projectPath));
+    const props = {
+      user: "johndoe", 
+      key: "en_1co", 
+      projectDetails: {
+        projectName: 'en_1co_ulb', 
+        projectSaveLocation: '/tmp/en_1co_ulb', 
+        accessTimeAgo: '5 days ago', 
+        bookAbbr: '1co', 
+        bookName: '1 Corinthians', 
+        target_language: {
+          id: 'en',
+          name: 'English'
+        }, 
+        isSelected: false
+      },
+      actions: {
+        selectProject: () => jest.fn()
       }
     };
     const renderedValue =  renderer.create(
       <MuiThemeProvider>
-        <ProjectCard 
-          user={"johndoe"} 
-          key={"en_1co"} 
-          projectDetails={projectDetails} 
-          actions={actions} />
+        <ProjectCard {...props} />
       </MuiThemeProvider>
     ).toJSON();
     expect(renderedValue).toMatchSnapshot();
