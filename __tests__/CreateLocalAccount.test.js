30ec896314c55a0a16d7c8419a35df028fbe999f
richmahn <rich.mahn@unfoldingword.org>
2017-11-16 12:08:04 -0700

    Update to submodules and tests

---
+++ __tests__/CreateLocalAccount.test.js
@@ -0,0 +1,21 @@
+/* eslint-env jest */
+
+import React from 'react';
+import CreateLocalAccount from '../src/js/components/home/usersManagement/LoginDoor43Account';
+import {shallow} from 'enzyme';
+
+// Tests for CreateLocalAccount React Component
+describe('Test CreateLocalAccount component',()=>{
+  test('Test username field of CreateLocalAccount has the focus function', () => {
+    const props = {
+      actions: {
+        showPopover: jest.fn()
+      },
+      setView: jest.fn(),
+      loginUser: jest.fn()
+    };
+    const enzymeWrapper = shallow(<CreateLocalAccount {...props} />);
+    const usernameField = enzymeWrapper.find('TextField.Username');
+    expect(usernameField.node.ref.name).toEqual('focusUsernameInputField');
+  });
+});
