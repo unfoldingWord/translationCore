7c8c703edd867a71778fc06ffadc69d77e885fc1
mannycolon <colonmanuel7@gmail.com>
2017-11-17 11:12:24 -0500

    extracted code that unzips a .studio project to its own function. Now unzipping project to 'imports/temp/{project_name}', then moving it to tc projects folder and deleting temp folder from imports folder. Now renamed .studio projects load on tC

--- src/js/actions/ImportLocalActions.js
+++ src/js/actions/ImportLocalActions.js
@@ -15,6 +15,8 @@ import * as LoadHelpers from '../helpers/LoadHelpers';
 import * as ProjectSelectionHelpers from '../helpers/ProjectSelectionHelpers';
 // contstants
 const DEFAULT_SAVE = path.join(path.homedir(), 'translationCore', 'projects');
+const TEMP_IMPORT_path = path.join(path.homedir(), 'translationCore', 'imports', 'temp');
+
 export const ALERT_MESSAGE = (
   <div>
     No file was selected. Please click on the
@@ -109,13 +111,15 @@ function verifyProject(sourcePath, url) {
       let oldPath = sourcePath;
       sourcePath = path.join(DEFAULT_SAVE, fileName);
       if (!LoadHelpers.projectAlreadyExists(sourcePath, oldPath)) {
-        zip.extractAllTo(DEFAULT_SAVE, /*overwrite*/true);
+        importProjectAndMoveToMyProjects(zip, fileName);
         tSProject = true;
       } else {
         return reject(
-          <div>The project you selected ({sourcePath}) already exists.<br />
+          <div>
+            The project you selected ({sourcePath}) already exists.<br />
             Reimporting existing projects is not currently supported.
-      </div>);
+          </div>
+        );
       }
     }
 
@@ -135,7 +139,8 @@ function verifyProject(sourcePath, url) {
           <div>
             The project you selected ({sourcePath}) already exists.<br />
             Reimporting existing projects is not currently supported.
-      </div>);
+          </div>
+        );
       }
     }
     /** Projects here should be tC fromatted */
@@ -148,7 +153,7 @@ function verifyProject(sourcePath, url) {
           <div>
             The project you selected ({sourcePath}) already exists.<br />
             Reimporting existing projects is not currently supported.
-      </div>
+          </div>
         );
       }
       return resolve({ newProjectPath, type: 'tC' });
@@ -180,9 +185,10 @@ function detectInvalidProjectStructure(sourcePath) {
           return resolve();
         } else {
           return reject(
-            <div>The project you selected has an invalid manifest ({sourcePath})
-            <br />Please select a new project.
-        </div>
+            <div>
+              The project you selected has an invalid manifest ({sourcePath})<br />
+              Please select a new project.
+            </div>
           );
         }
       } else {
@@ -195,3 +201,26 @@ function detectInvalidProjectStructure(sourcePath) {
     }
   });
 }
+
+
+/**
+ * Imports a .tStudio project to a temporary folder path in tC imports folder
+ * and then moves it into my projects folder.
+ * @param {Object} zip
+ * @param {String} fileName
+ */
+export const importProjectAndMoveToMyProjects = (zip, projectName) => {
+  // extract .tstudio project to temp folder in tC imports folder
+  zip.extractAllTo(TEMP_IMPORT_path, /*overwrite*/true);
+  // the folder name of the project inside the .studio zip file may be different from
+  // the actual name of the .studio zip file therefore get actual name from folder
+  let currentProjectName = fs.readdirSync(TEMP_IMPORT_path)
+    .filter((element) => element !== 'manifest.json' && element !== '.DS_Store')[0];
+  const srcPath = path.join(TEMP_IMPORT_path, currentProjectName);
+  // projectName is the file name of the .studio file which now allows renaming .tstudio projects
+  const destinationPath = path.join(DEFAULT_SAVE, projectName);
+  // copy project from imports folder to projects folder
+  fs.copySync(srcPath, destinationPath);
+  // remove/delete project folder from temp folder
+  fs.removeSync(TEMP_IMPORT_path);
+};
