f82db31deb912a28a1883c8b1f9de49f947e53f1
RoyalSix <jay.a.scottjr@biola.edu>
2017-11-27 10:32:09 -0800

    added basic template for psudeo code

--- __tests__/OnlineImportWorkflowHelpers.test.js
+++ __tests__/OnlineImportWorkflowHelpers.test.js
@@ -1,8 +1,9 @@
-import path from 'path-extra';
+import path from 'path';
+import pathex from 'path-extra';
 import * as fs from 'fs-extra';
 import * as OnlineImportWorkflowHelpers from '../src/js/helpers/Import/OnlineImportWorkflowHelpers';
 const STANDARD_PROJECT = 'https://git.door43.org/royalsix/es-419_tit_text_ulb';
-const PROJECTS_PATH = path.join(path.homedir(), 'translationCore', 'imports');
+const PROJECTS_PATH = path.join(pathex.homedir(), 'translationCore', 'imports');
 
 
 describe('OnlineImportWorkflowHelpers.cloneRepo', () => {
--- src/js/helpers/Import/OnlineImportWorkflowHelpers.js
+++ src/js/helpers/Import/OnlineImportWorkflowHelpers.js
@@ -1,3 +1,7 @@
+import git from '../GitApi';
+import pathex from 'path-extra';
+import * as fs from 'fs-extra';
+
 /**
  * @Description:
  * Helpers for the business logic and conditions that wrapping together
@@ -5,7 +9,38 @@
  * Note: This avoids problematic chaining that is troublesome to debug
  */
 
-
 export const cloneRepo = (link) => {
-  
+  return new Promise((resolve, reject) => {
+    /** Get project name */
+    var expression = new RegExp(/^https?:\/\/(git.door43.org|door43.org\/u)\/[^\/]+\/([^\/.]+).git$/);
+      if (expression.test(link)) {
+        var projectName = expression.exec(link)[2];
+        var savePath = path.join(pathex.homedir(), 'translationCore', 'imports', projectName);
+      } else {
+          return reject('The URL does not reference a valid project');
+      }
+    /** Clone repo to imports folder using git mirror */
+    /** Return when repo is cloned */
+    resolve();
+  });
 };
+
+/**
+* @description Runs the git command to clone a repo.
+* @param {string} savePath - The location of the git repo
+* @param {string} url - The url of the git repo
+* @param {function} callback - The function to be run on complete
+* @param {module} gitHandler - optional for testing.  If not given will use git module
+******************************************************************************/
+export function runGitCommand(savePath, url, callback, gitHandler) {
+  gitHandler = gitHandler || git;
+  gitHandler(savePath).mirror(url, savePath, function (err) {
+    if (err) {
+      fs.removeSync(savePath);
+      if (callback)
+        callback({ type: "custom", text: err }, savePath, url);
+    } else {
+      callback(null, savePath, url);
+    }
+  });
+}
