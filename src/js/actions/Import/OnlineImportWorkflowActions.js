27d03309a1aea21f3b9339e4bcd4817e69ecb885
RoyalSix <jay.a.scottjr@biola.edu>
2017-11-30 11:25:38 -0800

    added clone repo function

--- src/js/actions/Import/OnlineImportWorkflowActions.js
+++ src/js/actions/Import/OnlineImportWorkflowActions.js
@@ -1,16 +1,15 @@
-// import * as OnlineImportWorkflowHelpers from '../../helpers/Import/OnlineImportWorkflowHelpers';
-
 /**
  * @Description:
  * Actions that dispatch other actions to wrap up online importing
 **/
+import { cloneRepo } from '../../helpers/Import/OnlineImportWorkflowHelpers';
 import { migrate } from './ProjectMigrationActions';
 import { validate } from './ProjectValidationActions';
 import { move } from './ProjectImportFilesystemActions';
 
 export const onlineImport = (projectPath, projectLink) => {
-  return(async (dispatch) => {
-    //dispatch(OnlineImportWorkflowHelpers.cloneRepo());
+  return (async (dispatch) => {
+    //dispatch(cloneRepo());
     migrate(projectPath);
     await dispatch(validate(projectPath, projectLink));
     dispatch(move());
