933c778b4dc4865cc99a73fd190d8160ba39b7e9
mannycolon <colonmanuel7@gmail.com>
2017-12-13 11:50:51 -0500

    added  tstudio and tcore to file checking

--- src/js/helpers/FileConversionHelpers/index.js
+++ src/js/helpers/FileConversionHelpers/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import path from 'path-extra';
 // helpers
 import * as UsfmFileConversionHelpers from './UsfmFileConversionHelpers';
@@ -8,9 +9,16 @@ export const convert = (sourceProjectPath, selectedProjectFilename) => {
     try {
       if(projectHasUsfmFileExtension(sourceProjectPath)) {
         await UsfmFileConversionHelpers.convertToProjectFormat(sourceProjectPath, selectedProjectFilename);
-      } else {
+      } else if (projectHasTstudioOrTcoreFileExtension(sourceProjectPath)) {
         // project's extension name is either .tstudio or .tcore
         await ZipFileConversionHelpers.convertToProjectFormat(sourceProjectPath, selectedProjectFilename);
+      } else {
+        reject(
+          <div>
+            The project you selected ({sourceProjectPath}) is an invalid tstudio or tcore project. <br />
+            Please verify the project you selected is a valid  tstudio or tcore file.
+          </div>
+        );
       }
       resolve();
     } catch (error) {
@@ -24,4 +32,11 @@ export const projectHasUsfmFileExtension = (sourceProjectPath) => {
   const usfm = ['.usfm', '.sfm', '.txt'];
 
   return usfm.includes(projectExtensionName);
+};
+
+export const projectHasTstudioOrTcoreFileExtension = (sourceProjectPath) => {
+  const projectExtensionName = path.extname(sourceProjectPath).toLowerCase();
+  const validFileExtensions = ['.tstudio', '.tcore'];
+
+  return validFileExtensions.includes(projectExtensionName);
 };
