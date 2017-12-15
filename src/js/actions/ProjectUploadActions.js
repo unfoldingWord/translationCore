530cee8e6c682f6c1e188324bc29c0d6af89fbc9
Sterling (Jay) Scott <jay.a.scottjr@biola.edu>
2017-12-15 08:41:03 -0800

    Fix For Errors When Opening Non-Compliant Projects (#3384)
    * init
    
    * added tests fro gogs
    
    * added more testing for gogs helpers
    
    * fixed misspelled comment and added tools commits
    
    * removed unnesesary import
    
    * ran npm install

--- src/js/actions/ProjectUploadActions.js
+++ src/js/actions/ProjectUploadActions.js
@@ -6,7 +6,7 @@ import git from '../helpers/GitApi.js';
 import * as AlertModalActions from './AlertModalActions';
 import * as OnlineModeConfirmActions from './OnlineModeConfirmActions';
 // helpers
-import * as GogsApiHelper from '../helpers/GogsApiHelper';
+import * as GogsApiHelpers from '../helpers/GogsApiHelpers';
 
 /**
  * Upload project to door 43, based on currently logged in user.
@@ -25,8 +25,7 @@ export function uploadProject(projectPath, user) {
           const message = "Your login has become invalid. Please log out and log back in.";
           return dispatch(AlertModalActions.openAlertDialog(message, false));
         }
-
-        GogsApiHelper.createRepo(user, projectName).then(repo => {
+        GogsApiHelpers.createRepo(user, projectName).then(repo => {
           var newRemote = 'https://' + user.token + '@git.door43.org/' + repo.full_name + '.git';
 
           git(projectPath).save(user, 'Commit before upload', projectPath, err => {
