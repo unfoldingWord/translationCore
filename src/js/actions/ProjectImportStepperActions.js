6cc0699c17e52351ea670f5924acf5a63aa4c1f2
mannycolon <colonmanuel7@gmail.com>
2017-12-19 12:01:35 -0500

    Revert "Revert "Auto Assign CC-BY-SA License to door 43 Projects (#3423)""
    This reverts commit dc4c2c26cf012da513d83a54bf95951cbc89e34e.

--- src/js/actions/ProjectImportStepperActions.js
+++ src/js/actions/ProjectImportStepperActions.js
@@ -56,6 +56,7 @@ export function initiateProjectValidationStepper() {
       //If there are no invalid checks
       TargetLanguageActions.generateTargetBibleFromProjectPath(projectSaveLocation, manifest);
       dispatch(ProjectLoadingActions.displayTools());
+      importStepperDone();
     } else {
       //Show the checks that didn't pass
       dispatch(updateStepperIndex());
