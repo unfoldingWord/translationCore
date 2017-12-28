778fc427e5f382e3e9711ba04e868f3e8825724a
richmahn <rich.mahn@unfoldingword.org>
2017-12-28 16:25:12 -0700

    Fix - #3236 - Corrected error message if manifest doesn't exist

--- __tests__/ManifestValidationHelpers.test.js
+++ __tests__/ManifestValidationHelpers.test.js
@@ -4,19 +4,21 @@
 
 jest.mock('fs-extra');
 import fs from 'fs-extra';
+import path from 'path';
 //helpers
 import * as ManifestValidationHelpers from '../src/js/helpers/ProjectValidation/ManifestValidationHelpers';
-const MANIFEST_EXISTS_PATH = 'path/to/project/where/manifest/exists';
-const MANIFEST_NOT_EXISTS_PATH = 'path/to/project/where/manifest/doesnt/exists';
+const MANIFEST_EXISTS_PATH = path.join('path', 'to', 'project', 'where', 'manifest', 'exists');
+const MANIFEST_NOT_EXISTS_PATH = path.join('path', 'to', 'project', 'where', 'manifest', 'doesnt', 'exists');
 beforeEach(() => {
   fs.__setMockFS({
     [MANIFEST_EXISTS_PATH]: [],
-    [MANIFEST_EXISTS_PATH + '/manifest.json']: []
+    [path.join(MANIFEST_EXISTS_PATH, 'manifest.json')]: []
   });
 });
 
 describe('ManifestValidationHelpers.manifestExists', () => {
   test('should return that the manifest exists', async () => {
+    console.log(MANIFEST_EXISTS_PATH);
     let result = await ManifestValidationHelpers.manifestExists(MANIFEST_EXISTS_PATH);
     expect(result).toBeTruthy();
   });
@@ -25,7 +27,7 @@ describe('ManifestValidationHelpers.manifestExists', () => {
       await ManifestValidationHelpers.manifestExists(MANIFEST_NOT_EXISTS_PATH);
     }
     catch (e) {
-      expect(e).toBe('Manifest does not exist.');
+      expect(e).toBe('Unable to find the manifest for project '+path.basename(MANIFEST_NOT_EXISTS_PATH)+'. It will not be loaded.');
     }
   });
 });
