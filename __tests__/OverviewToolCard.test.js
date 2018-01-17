880fb2a33e48fc965a1d74727a63ba57c5b0ecca
richmahn <rich.mahn@unfoldingword.org>
2018-01-17 13:59:11 -0700

    Updates to the OverviewToolCard tests

--- __tests__/OverviewToolCard.test.js
+++ __tests__/OverviewToolCard.test.js
@@ -2,15 +2,15 @@
 jest.unmock('fs-extra');
 jest.unmock('adm-zip');
 import React from 'react';
-import consts from '../src/js/actions/ActionTypes';
-import { applyMiddleware, createStore } from 'redux';
+import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import reducers from '../src/js/reducers';
 import ToolCard from '../src/js/components/home/overview/ToolCard';
 import renderer from 'react-test-renderer';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import * as LocalImportWorkflowActions from '../src/js/actions/Import/LocalImportWorkflowActions';
 const project = '__tests__/fixtures/project/tstudio_project/abu_tit_text_reg.tstudio';
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 jest.mock('../src/js/components/home/toolsManagement/ToolCardProgress', () => 'ToolCardProgress');
 
@@ -18,33 +18,51 @@ describe('Tool Card component Tests', () => {
   let store;
   beforeEach(() => {
     // create a new store instance for each test
-    store = createStore(
-      reducers,
-      applyMiddleware(thunk)
-    );
+    store = mockStore({
+      localImportReducer: {
+        selectedProjectFilename: "abu_tit_text_reg",
+        sourceProjectPath: project
+      },
+      settingsReducer: {
+        developerMode: ''
+      },
+      projectDetailsReducer: {
+        manifest: {
+          project: {
+            id: 'tit'
+          },
+          target_language: {
+            id: 'abu'
+          },
+          'resource': {
+            id: 'reg'
+          }
+        }
+      }
+    });
   });
+
   it('ToolCard Before Project Loaded', () => {
-    let state = {
-      reducers: store.getState(),
+    const props = {
+      reducers: {
+        toolsReducer: {
+          currentToolTitle: '',
+          currentToolName: ''
+        },
+        projectDetailsReducer: {
+          currentProjectToolsProgress: '',
+          projectSaveLocation: ''
+        }
+      },
       actions: {}
     };
     const component = renderer.create(
       <MuiThemeProvider>
-        <ToolCard {...state} />
+        <ToolCard {...props} />
       </MuiThemeProvider>,
     );
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
-
-
-    store.dispatch({
-      type: consts.UPDATE_SOURCE_PROJECT_PATH,
-      sourceProjectPath: project
-    });
-    store.dispatch({
-      type: consts.UPDATE_SELECTED_PROJECT_FILENAME,
-      selectedProjectFilename: "abu_tit_text_reg"
-    });
 
     store.dispatch(LocalImportWorkflowActions.localImport());
 
@@ -56,26 +74,23 @@ describe('Tool Card component Tests', () => {
     const title = 'Current Tool Title';
     const name = 'ToolName';
     const progress = 0.5;
-    store.dispatch({
-      type: consts.SET_CURRENT_TOOL_TITLE,
-       currentToolTitle: title
-    });
-    store.dispatch({
-      type: consts.SET_CURRENT_TOOL_NAME,
-       currentToolName: name
-    });
-    store.dispatch({
-      type: consts.SET_PROJECT_PROGRESS_FOR_TOOL,
-      toolName: name,
-      progress: progress
-    });
-    let state = {
-      reducers: store.getState(),
+    const props = {
+      reducers: {
+        toolsReducer: {
+          currentToolTitle: title,
+          currentToolName: name
+        },
+        projectDetailsReducer: {
+          currentProjectToolsProgress: {
+            [name]: progress
+          }
+        }
+      },
       actions: {}
     };
     const component = renderer.create(
       <MuiThemeProvider>
-        <ToolCard {...state} />
+        <ToolCard {...props} />
       </MuiThemeProvider>,
     );
     let tree = component.toJSON();
