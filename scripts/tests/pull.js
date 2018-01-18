01d0d7117122c653124da428183f4ef4314faf6f
RoyalSix <jay.a.scottjr@biola.edu>
2018-01-18 00:41:16 -0500

    added script to pull/install moduels

---
+++ scripts/tests/pull.js
@@ -0,0 +1,12 @@
+const exec = require('child_process').exec;
+const branchName = process.argv[2];
+if (!branchName) {
+  console.log('No branch name specified');
+} else {
+  var script = `git checkout ${branchName}; git pull;`;
+  script += `git submodule foreach --recursive '(git checkout ${branchName}; git pull;)'`;
+  exec(script, (err) => {
+    const message = err ? err : 'Pulled all submodule branches ' + branchName;
+    console.log(message);
+  });
+}
