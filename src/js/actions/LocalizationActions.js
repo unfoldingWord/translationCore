ef3c0d64f66034cb28aeab4093c8383680c90727
Joel Lonbeck <joel@neutrinographics.com>
2018-01-22 13:16:35 -0800

    hooked up loading locale files

--- src/js/actions/LocalizationActions.js
+++ src/js/actions/LocalizationActions.js
@@ -1,7 +1,8 @@
 /**
  * These actions are used for controlling localization within the application.
  */
-
+import fs from 'fs-extra';
+import path from 'path';
 import {initialize, addTranslation, setActiveLanguage} from 'react-localize-redux';
 
 /**
@@ -12,13 +13,37 @@ import {initialize, addTranslation, setActiveLanguage} from 'react-localize-redu
  */
 export const loadLocalization = () => {
   return (dispatch) => {
-    // TODO: load the localization files
-    const languages = [
-      { name: 'English', code: 'en' }
-    ];
-    const translations = {};
-    dispatch(initialize(languages, { defaultLanguage: 'en'}));
-    dispatch(addTranslation(translations));
+    const localeDir = path.join(__dirname, '../../locale');
+    return fs.readdir(localeDir).then((items) => {
+      let languages = [];
+      let translations = [];
+      if(!items) {
+        return Promise.reject(`No localization files found in ${localeDir}`);
+      }
+      for(let file of items) {
+        if(!file.endsWith('.json')) {
+          console.warn(`Skipping invalid localization file ${file}`);
+          continue;
+        }
+        const localeFile = path.join(localeDir, file);
+        try {
+          let translation = JSON.parse(fs.readFileSync(localeFile));
+          languages.push(file.replace(/\.json/, ''));
+          translations.push(translation);
+        } catch(e) {
+          console.error(`Failed to load localization ${localeFile}`, e);
+        }
+      }
+      return Promise.resolve({
+        languages: [],
+        translations: []
+      });
+    }).then(({languages, translations}) => {
+      dispatch(initialize(languages, { defaultLanguage: 'en'}));
+      dispatch(addTranslation(translations));
+    }).catch(err => {
+      console.log('Failed to initialize localization', err);
+    });
   };
 };
 
--- src/js/redux/configureStore.js
+++ src/js/redux/configureStore.js
@@ -1,5 +1,6 @@
 import {createStore, applyMiddleware} from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import promise from 'redux-promise';
 import rootReducers from '../reducers/index.js';
 
 //  preloadedState will be used for Data persistence
@@ -9,7 +10,8 @@ export default function configureStore(persistedState) {
     rootReducers,
     persistedState,
     applyMiddleware(
-      thunkMiddleware
+      thunkMiddleware,
+      promise
     )
   );
 }
