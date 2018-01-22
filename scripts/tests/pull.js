a5a78274f3f5fbc593bc6047725bb4103c0e3835
RoyalSix <jay.a.scottjr@biola.edu>
2018-01-22 12:25:43 -0800

    fix for pull script#2

--- scripts/tests/pull.js
+++ scripts/tests/pull.js
@@ -4,7 +4,7 @@ if (!branchName) {
   throw Error('No branch name specified');
 } else {
   var script = `git checkout ${branchName}; git pull;`;
-  script += `git submodule foreach --recursive '(git_res=$(git checkout ${branchName} 2>&1); if [[ $git_res != *"error"* ]]; then git reset --hard HEAD; git pull; fi;)'`;
+  script += `git submodule foreach --recursive '(git add .; git stash; git_res=$(git checkout ${branchName} 2>&1); if [[ $git_res != *"error"* ]]; then git reset --hard HEAD; git pull; fi;)'`;
   exec(script, (err, stdout) => {
     console.log(stdout);
   });
