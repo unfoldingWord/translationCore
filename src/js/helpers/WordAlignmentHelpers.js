ea494b9a3ced31decce11cd429b665f435963684
Christopher Klapp <christopher@klapp.name>
2018-01-31 18:30:25 -0500

    Rewrite of occurrence(s)InString with tests

--- src/js/helpers/WordAlignmentHelpers.js
+++ src/js/helpers/WordAlignmentHelpers.js
@@ -28,54 +28,11 @@ export const getWordText = (wordObject) => {
 export const populateOccurrencesInWordObjects = (wordObjects) => {
   const string = combineGreekVerse(wordObjects);
   return wordObjects.map((wordObject, index) => {
-    wordObject.occurrence = getOccurrenceInString(string, index, getWordText(wordObject));
-    wordObject.occurrences = occurrencesInString(string, getWordText(wordObject));
+    wordObject.occurrence = stringHelpers.occurrenceInString(string, index, getWordText(wordObject));
+    wordObject.occurrences = stringHelpers.occurrencesInString(string, getWordText(wordObject));
     return wordObject;
   });
 };
-
-/**
- * gets the occurrence of a subString in a string by using the subString index in the string.
- * @param {String} string
- * @param {Number} currentWordIndex
- * @param {String} subString
- * TODO: Replace with the tokenizer version of this to prevent puctuation issues
- * Cannot replace with tokenizer until tokenizer handles all greek use cases that broke tokenizer
- */
-export const getOccurrenceInString = (string, currentWordIndex, subString) => {
-  let arrayOfStrings = string.split(' ');
-  let occurrence = 1;
-  let slicedStrings = arrayOfStrings.slice(0, currentWordIndex);
-
-  slicedStrings.forEach((slicedString) => {
-    if (slicedStrings.includes(subString)) {
-      slicedString === subString ? occurrence += 1 : null;
-    } else {
-      occurrence = 1;
-    }
-  });
-  return occurrence;
-};
-/**
- * @description Function that count occurrences of a substring in a string
- * @param {String} string - The string to search in
- * @param {String} subString - The sub string to search for
- * @returns {Integer} - the count of the occurrences
- * @see http://stackoverflow.com/questions/4009756/how-to-count-string-occurrence-in-string/7924240#7924240
- * modified to fit our use cases, return zero for '' substring, and no use case for overlapping.
- */
-export const occurrencesInString = (string, subString) => {
-  if (subString.length <= 0) return 0;
-  let occurrences = 0, position = 0, step = subString.length;
-  while (position < string.length) {
-    position = string.indexOf(subString, position);
-    if (position === -1) break;
-    ++occurrences;
-    position += step;
-  }
-  return occurrences;
-};
-
 /**
  * @description wordObjectArray via string
  * @param {String} string - The string to search in
@@ -83,7 +40,7 @@ export const occurrencesInString = (string, subString) => {
  */
 export const wordObjectArrayFromString = (string) => {
   const wordObjectArray = stringHelpers.tokenize(string).map((word, index) => {
-    const occurrence = stringHelpers.getOccurrenceInString(string, index, word);
+    const occurrence = stringHelpers.occurrenceInString(string, index, word);
     const occurrences = stringHelpers.occurrencesInString(string, word);
     return {
       word,
@@ -134,7 +91,7 @@ export const sortWordObjectsByString = (wordObjectArray, stringData) => {
 };
 
 /**
- * 
+ *
  * @param {string} projectSaveLocation - Full path to the users project to be exported
  */
 export const getAlignmentPathsFromProject = (projectSaveLocation) => {
@@ -154,14 +111,14 @@ export const getAlignmentPathsFromProject = (projectSaveLocation) => {
 };
 
 /**
- * @description - Method to fetch a target language chapter JSON and source/target language 
+ * @description - Method to fetch a target language chapter JSON and source/target language
  * alignment data JSON for the corresponding chapter. This is essientially the data
  * needed to in order to produce a USFM 3 from the aligned data.
- * @param {string} wordAlignmentDataPath - path to where the source/target language 
+ * @param {string} wordAlignmentDataPath - path to where the source/target language
  * alignment data JSON is located
  * @param {string} projectTargetLanguagePath path to where the target language chapter JSON is
  * located
- * @param {string} chapterFile 
+ * @param {string} chapterFile
  */
 export const getAlignmentDataFromPath = (wordAlignmentDataPath, projectTargetLanguagePath, chapterFile) => {
   try {
@@ -190,7 +147,7 @@ export const convertAlignmentDataToUSFM = (wordAlignmentDataPath, projectTargetL
     const chapterNumber = path.parse(chapterFile).name;
     let { chapterAlignmentJSON, targetLanguageChapterJSON } = getAlignmentDataFromPath(wordAlignmentDataPath, projectTargetLanguagePath, chapterFile);
     for (var verseNumber in chapterAlignmentJSON) {
-      //Iterate through verses of chapter alignment data, 
+      //Iterate through verses of chapter alignment data,
       //and retieve relevant information for conversion
       const verseAlignments = chapterAlignmentJSON[verseNumber];
       const verseString = targetLanguageChapterJSON[verseNumber];
@@ -212,7 +169,7 @@ export const setVerseObjectsInAlignmentJSON = (usfmToJSONObject, chapterNumber, 
 };
 
 /**
- * 
+ *
  * @param {string} usfm - Usfm data to be written to FS
  * @param {string} projectSaveLocation - Location of usfm to be written
  */
