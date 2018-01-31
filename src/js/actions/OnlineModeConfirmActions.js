9f22e1e4babfdf969a5dda1268c150ff946975cd
Joel Lonbeck <joel@neutrinographics.com>
2018-01-30 17:01:24 -0800

    localized online mode confirm actions

--- src/js/actions/OnlineModeConfirmActions.js
+++ src/js/actions/OnlineModeConfirmActions.js
@@ -1,28 +1,31 @@
-import consts from './ActionTypes';
+import types from './ActionTypes';
 // components
 import OnlineDialog from '../components/dialogComponents/OnlineDialog';
+import {getTranslate} from '../selectors';
 // actions
 import * as AlertModalActions from './AlertModalActions';
 // consts
 
 export function confirmOnlineAction(callback) {
   return ((dispatch, getState) => {
+    const translate = getTranslate(getState());
+    const cancelText = translate('cancel');
     var onlineMode = getState().settingsReducer.onlineMode;
     if (!onlineMode) {
       dispatch(AlertModalActions.openOptionDialog(OnlineDialog((val) => dispatch(checkBox(val))),
         (result) => {
-          if (result != 'Cancel') {
+          if (result !== cancelText) {
             dispatch(AlertModalActions.closeAlertDialog());
             callback();
           } else dispatch(AlertModalActions.closeAlertDialog());
-        }, 'Access Internet', 'Cancel'));
+        }, translate('access_internet'), cancelText));
     } else callback();
   });
 }
 
 export function checkBox(val) {
   return {
-    type: consts.UPDATE_ONLINE_MODE,
+    type: types.UPDATE_ONLINE_MODE,
     val
   };
 }
