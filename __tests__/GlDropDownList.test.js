23422f5585a056d28184907b89899a0bf06045fb
bspidel <bruce.spidel@gmail.com>
2018-02-02 14:05:52 -0500

    Remove selectionChange for test

--- __tests__/GlDropDownList.test.js
+++ __tests__/GlDropDownList.test.js
@@ -4,19 +4,19 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 
 // Components
-import GlDropDownList from '../src/js/components/home/toolsManagement/GlDropDownList';
-import ToolCard from       '../src/js/components/home/toolsManagement/ToolCard';
-import ToolsCards from     '../src/js/components/home/toolsManagement/ToolsCards';
+import GLDropDownList from '../src/js/components/home/toolsManagement/GLDropDownList';
+//import ToolCard from       '../src/js/components/home/toolsManagement/ToolCard';
+//import ToolsCards from     '../src/js/components/home/toolsManagement/ToolsCards';
 
 describe('Test Gateway Language Drop Down List',() => {
   test('Comparing dropdownlist Component render with snapshot', () => {
     const props = {
-      currentGLSelection: 1,
-      selectionChange: {ToolCard.selectionChange()}
-      manifest: {ToolsCards.manifest}
+      currentGLSelection: '1'
+      //selectionChange: {ToolCard.selectionChange(0)}
+      //manifest: {ToolsCards.manifest}
     };
     const renderedValue = renderer.create(
-      <GlDropdownList{...props} />
+      <GLDropDownList{...props} />
     ).toJSON();
     expect(renderedValue).toMatchSnapshot();
   });
