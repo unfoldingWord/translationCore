963f24c9c524dac655f7119833189bfebc2f366f
bspidel <bruce.spidel@gmail.com>
2018-02-02 14:13:53 -0500

    try another reference to selectionChange

--- __tests__/GlDropDownList.test.js
+++ __tests__/GlDropDownList.test.js
@@ -5,14 +5,14 @@ import renderer from 'react-test-renderer';
 
 // Components
 import GLDropDownList from '../src/js/components/home/toolsManagement/GLDropDownList';
-//import ToolCard from       '../src/js/components/home/toolsManagement/ToolCard';
+import ToolCard from       '../src/js/components/home/toolsManagement/ToolCard';
 //import ToolsCards from     '../src/js/components/home/toolsManagement/ToolsCards';
 
 describe('Test Gateway Language Drop Down List',() => {
   test('Comparing dropdownlist Component render with snapshot', () => {
     const props = {
-      currentGLSelection: '1'
-      //selectionChange: {ToolCard.selectionChange(0)}
+      currentGLSelection: 1,
+      selectionChange: {ToolCard.selectionChange(0)}
       //manifest: {ToolsCards.manifest}
     };
     const renderedValue = renderer.create(
