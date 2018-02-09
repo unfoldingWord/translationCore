8207cd49d5a07cf81a89f7b0327f5eda8f5af594
Bruce McLean <mcleanb@photo-nomad.net>
2018-02-09 10:35:32 -0500

    Changed unit test for splitting. Added bottom words and multi-merges.

--- __tests__/WordAlignmentActions.test.js
+++ __tests__/WordAlignmentActions.test.js
@@ -10,12 +10,13 @@ describe('WordAlignmentActions.unmergeAlignments', () => {
   it('should reorder unordered greek', () => {
     const testPath = '__tests__/fixtures/splitWords/heb1:1_outOfOrder.json';
     const testData = fs.readJSONSync(testPath);
-    const fromAlignmentIndex = 8;
+    const fromAlignmentIndex = 7;
     const alignmentsInitial = JSON.parse(JSON.stringify(testData.alignmentsInitial));
 
     const { alignments, wordBank } = WordAlignmentActions.unmergeAlignments(
       testData.removeItem, alignmentsInitial, testData.wordBank, fromAlignmentIndex, testData.greek, testData.verseString
     );
     expect(alignments).toEqual(testData.alignmentsFinal);
+    expect(wordBank.length).toEqual(2);
   });
 });
