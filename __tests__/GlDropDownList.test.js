d55fbf08aa3c5824af4d1e6e2bb872da17b1f1fe
mannycolon <colonmanuel7@gmail.com>
2018-02-13 12:16:02 -0500

    fixed failing test and updated snapshots

--- __tests__/GlDropDownList.test.js
+++ __tests__/GlDropDownList.test.js
@@ -1,21 +1,20 @@
 /* eslint-env jest */
-
 import React from 'react';
 import renderer from 'react-test-renderer';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
 // Components
 import GlDropDownList from '../src/js/components/home/toolsManagement/GlDropDownList';
-function dummy() {}
+
 describe('Test Gateway Language Drop Down List',() => {
   test('Comparing dropdownlist Component render with snapshot', () => {
     const props = {
       currentGLSelection: 1,
-      selectionChange: dummy
+      selectionChange: () => {},
+      translate: key => key
     };
     const renderedValue = renderer.create(
       <MuiThemeProvider>
-        <GlDropDownList{...props} />
+        <GlDropDownList {...props} />
       </MuiThemeProvider>
     ).toJSON();
     expect(renderedValue).toMatchSnapshot();
