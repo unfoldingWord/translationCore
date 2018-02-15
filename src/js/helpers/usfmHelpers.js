8902961510804fc75be8433ebfacf59ab6146d04
Bruce McLean <mcleanb@photo-nomad.net>
2018-02-15 12:47:48 -0500

    USFM export - fix for language names with spaces.

--- src/js/helpers/usfmHelpers.js
+++ src/js/helpers/usfmHelpers.js
@@ -111,7 +111,7 @@ export function getUSFMDetails(usfmObject) {
         let languageCodeArray = headerIDArray[index].trim().split('_');
         if (languageCodeArray.length === 3) {
           details.language.id = languageCodeArray[1].toLowerCase();
-          details.language.name = languageCodeArray[0].split('-').join(' ');
+          details.language.name = languageCodeArray[0].split('⋅').join(' '); // replace spaces
           details.language.direction = languageCodeArray[2].toLowerCase();
         }
       }
