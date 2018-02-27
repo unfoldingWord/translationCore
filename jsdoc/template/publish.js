c316a16cc7ebb35653f093d38962a8c06104b1b2
Joel Lonbeck <joel@neutrinographics.com>
2018-02-26 16:12:07 -0800

    added documentation publishing

--- jsdoc/template/publish.js
+++ jsdoc/template/publish.js
@@ -468,7 +468,9 @@ exports.publish = function (taffyData, opts, tutorials) {
   var packageInfo = (find({kind: 'package'}) || []) [0];
 
   if (packageInfo && packageInfo.name) {
-    outdir = path.join(outdir, packageInfo.name, packageInfo.version || '');
+    // later if we want to version the docs we could add the package version here.
+    // outdir = path.join(outdir, packageInfo.name, packageInfo.version || '');
+    outdir = path.join(outdir, packageInfo.name);
   }
   fs.mkPath(outdir);
 
