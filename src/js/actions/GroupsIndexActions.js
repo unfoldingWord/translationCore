5e8c04937f82e9368bd75d9210d161827d5b647e
Richard Mahn <rmahn@getmealticket.com>
2018-03-02 17:13:15 -0700

    Code cleanup

--- src/js/actions/GroupsIndexActions.js
+++ src/js/actions/GroupsIndexActions.js
@@ -3,8 +3,7 @@
  */
 
 import consts from './ActionTypes';
-// helpers
-import * as groupsIndexHelpers from '../helpers/groupsIndexHelpers';
+
 /**
  * @description This action sends all of the group Ids and
  * group names to the groupsIndexReducer
@@ -16,7 +15,7 @@ export const loadGroupsIndex = (_groupsIndex) => {
     const groupsIndex = _groupsIndex;
     dispatch({
       type: consts.LOAD_GROUPS_INDEX,
-      groupsIndex
+      _groupsIndex
     });
   });
 };
