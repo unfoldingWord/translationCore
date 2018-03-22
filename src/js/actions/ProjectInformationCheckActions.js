5399994ed813c3fcdd50d5d7110f38478f06e3b2
mannycolon <colonmanuel7@gmail.com>
2018-03-22 17:04:15 -0400

    Fixed contributors list loading for project details modal

--- src/js/actions/ProjectInformationCheckActions.js
+++ src/js/actions/ProjectInformationCheckActions.js
@@ -249,6 +249,7 @@ export function clearProjectInformationReducer() {
 export function openOnlyProjectDetailsScreen(projectPath) {
   return ((dispatch) => {
     const manifest = manifestHelpers.getProjectManifest(projectPath);
+    dispatch(setContributorsInProjectInformationReducer(manifest.translators));
     dispatch(ProjectLoadingActions.loadProjectDetails(projectPath, manifest));
     const targetLanguage = manifest.target_language || {};
     dispatch(setLanguageNameInProjectInformationReducer(targetLanguage.name || ''));
