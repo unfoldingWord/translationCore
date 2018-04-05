e73d6217b4ac59abbca7180f3105075e01edfee0
Richard Mahn <rich.mahn@unfoldingword.org>
2018-04-05 15:39:57 -0600

    Updated to use InvadliatedIcon

--- src/js/helpers/statusBadgeHelpers.js
+++ src/js/helpers/statusBadgeHelpers.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import ReactTooltip from 'react-tooltip';
 import { Glyphicon } from 'react-bootstrap';
+import InvalidatedIcon from '../components/svgIcons/InvalidatedIcon';
 import path from 'path-extra';
 import * as style from '../components/groupMenu/Style';
-
-const SVG_ICONS = ['invalidated'];
 
 /**
  * @description - Takes an array of strings that are glyph names and gets the proper React component to render them
@@ -14,13 +13,9 @@ const SVG_ICONS = ['invalidated'];
 export function getGlyphIcons(glyphs) {
   const glyphicons = [];
   if (glyphs && glyphs.length) {
-    glyphs.forEach((glyph, index)=>{
-      if (SVG_ICONS.indexOf(glyph) >= 0) {
-        let color = 'light';
-        if (index > 0)
-          color = 'dark';
-        const svgPath = path.join(__dirname, '../../images', glyph+'-'+color+'.svg');
-        glyphicons.push(<img src={svgPath} className={"glyphicon glyphicon-"+glyph} style={style.menuItem.statusIcon.invalidated} />);
+    glyphs.forEach((glyph)=>{
+      if (glyph === 'invalidated') {
+        glyphicons.push(<span className={"glyphicon glyphicon-"+glyph}><InvalidatedIcon height="16px" width="16px" /></span>);
       } else {
         glyphicons.push(<Glyphicon key={glyph} glyph={glyph} style={style.menuItem.statusIcon[glyph]} />);
       }
