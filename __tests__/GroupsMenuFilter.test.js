142dd304fcc1c15e68a4f4c4485396c34c529345
Richard Mahn <rich.mahn@unfoldingword.org>
2018-04-08 09:21:47 -0600

    Update tests

--- __tests__/GroupsMenuFilter.test.js
+++ __tests__/GroupsMenuFilter.test.js
@@ -6,28 +6,6 @@ import {shallow} from 'enzyme';
 import toJson from 'enzyme-to-json';
 
 describe('Tests for GroupsMenuFilter', () => {
-  test('Test GroupsMenuFilter for wordAlignment', () => {
-    const props = {
-      currentToolName: 'wordAlignment',
-      translate: k=>k,
-      setFilter: jest.fn(),
-      showInvalidated: true,
-      showBookmarks: true,
-      showSelections: true,
-      showNoSelections: true,
-      showVerseEdits: true,
-      showComments: true
-    };
-
-    // when
-    const wrapper = shallow(
-      <GroupsMenuFilter {...props} />
-    );
-
-    // then
-    expect(toJson(wrapper)).toMatchSnapshot();
-  });
-
   test('Test GroupsMenuFilter for translationWords', () => {
     const props = {
       currentToolName: 'translationWords',
