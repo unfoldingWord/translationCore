614c040ac124678648a01128195ee13b8f61ce0a
Richard Mahn <rich.mahn@unfoldingword.org>
2018-04-08 18:04:31 -0600

    Update to GroupsMenuFilter test

--- __tests__/GroupsMenuFilter.test.js
+++ __tests__/GroupsMenuFilter.test.js
@@ -6,7 +6,7 @@ import {shallow} from 'enzyme';
 import toJson from 'enzyme-to-json';
 
 describe('Tests for GroupsMenuFilter', () => {
-  test('Test GroupsMenuFilter for translationWords', () => {
+  test('Test GroupsMenuFilter for translationWords expanded', () => {
     const props = {
       currentToolName: 'translationWords',
       translate: k=>k,
@@ -16,7 +16,31 @@ describe('Tests for GroupsMenuFilter', () => {
       showSelections: true,
       showNoSelections: true,
       showVerseEdits: true,
-      showComments: true
+      showComments: true,
+      expandFilter: true
+    };
+
+    // when
+    const wrapper = shallow(
+      <GroupsMenuFilter {...props} />
+    );
+
+    // then
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  test('Test GroupsMenuFilter for translationWords collapsed', () => {
+    const props = {
+      currentToolName: 'translationWords',
+      translate: k=>k,
+      setFilter: jest.fn(),
+      showInvalidated: true,
+      showBookmarks: true,
+      showSelections: true,
+      showNoSelections: true,
+      showVerseEdits: true,
+      showComments: true,
+      expandFilter: false
     };
 
     // when
