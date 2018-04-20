7b83a732bf3a87f2c97f0b16bf8c8717592ec179
Joel Lonbeck <joel@neutrinographics.com>
2018-04-19 17:13:03 -0700

    stubbed out read and write api for project data

--- src/js/containers/ToolsContainer.js
+++ src/js/containers/ToolsContainer.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import path from 'path';
+import fs from 'fs-extra';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 // actions
@@ -6,12 +8,24 @@ import { showPopover } from '../actions/PopoverActions';
 import { addComment } from '../actions/CommentsActions';
 import { editTargetVerse } from '../actions/VerseEditActions';
 import { toggleReminder } from '../actions/RemindersActions';
-import { changeSelections, validateSelections } from '../actions/SelectionsActions';
-import { changeCurrentContextId, loadCurrentContextId, changeToNextContextId, changeToPreviousContextId } from '../actions/ContextIdActions';
+import {
+  changeSelections,
+  validateSelections
+} from '../actions/SelectionsActions';
+import {
+  changeCurrentContextId,
+  changeToNextContextId,
+  changeToPreviousContextId,
+  loadCurrentContextId
+} from '../actions/ContextIdActions';
 import { addGroupData } from '../actions/GroupsDataActions';
 import { setGroupsIndex } from '../actions/GroupsIndexActions';
 import { setToolSettings } from '../actions/SettingsActions';
-import { openAlertDialog, openOptionDialog, closeAlertDialog } from '../actions/AlertModalActions';
+import {
+  closeAlertDialog,
+  openAlertDialog,
+  openOptionDialog
+} from '../actions/AlertModalActions';
 import { selectModalTab } from '../actions/ModalActions';
 import * as ResourcesActions from '../actions/ResourcesActions';
 import * as WordAlignmentActions from '../actions/WordAlignmentActions';
@@ -19,47 +33,85 @@ import * as WordAlignmentActions from '../actions/WordAlignmentActions';
 import * as ResourcesHelpers from '../helpers/ResourcesHelpers';
 import * as VerseObjectHelpers from '../helpers/VerseObjectHelpers';
 import * as LexiconHelpers from '../helpers/LexiconHelpers';
+import { getProjectSaveLocation } from '../selectors';
 
 class ToolsContainer extends React.Component {
 
-  componentDidMount() {
-    let { contextId } = this.props.contextIdReducer;
+  constructor (props) {
+    super(props);
+    this.onWriteProjectData = this.onWriteProjectData.bind(this);
+    this.onReadProjectData = this.onReadProjectData.bind(this);
+  }
+
+  componentDidMount () {
+    let {contextId} = this.props.contextIdReducer;
     if (!contextId) this.props.actions.loadCurrentContextId();
   }
 
-  componentWillReceiveProps(nextProps) {
-    let { contextId } = nextProps.contextIdReducer;
-    let { currentToolName } = nextProps.toolsReducer;
+  componentWillReceiveProps (nextProps) {
+    let {contextId} = nextProps.contextIdReducer;
+    let {currentToolName} = nextProps.toolsReducer;
     // if contextId does not match current tool, then remove contextId
     if (contextId && contextId.tool !== currentToolName) {
       nextProps.actions.changeCurrentContextId(undefined);
     }
   }
 
-  render() {
+  /**
+   * Returns a handler for writing project data.
+   *
+   * @param {string} toolId - the id of the tool that is writing data
+   * @return {Function} - a write handler that returns a promise while writing
+   */
+  onWriteProjectData (toolId) {
+    return (filePath, data) => {
+      const {projectSaveLocation} = this.props;
+      const writePath = path.join(projectSaveLocation,
+        '.apps/translationCore/tools/', toolId, filePath);
+      return fs.outputFile(writePath, data);
+    };
+  }
+
+  /**
+   * Returns a handler for reading project data.
+   * @param {string} toolId - the id of the tool that is writing data
+   * @return {Function} - a read handler that returns a promise while reading
+   */
+  onReadProjectData (toolId) {
+    return (filePath) => {
+      // TODO: read the file
+      return Promise.reject(toolId, filePath);
+    };
+  }
+
+  render () {
     const {currentLanguage} = this.props;
-    let { currentToolViews, currentToolName } = this.props.toolsReducer;
+    let {currentToolViews, currentToolName} = this.props.toolsReducer;
     let Tool = currentToolViews[currentToolName];
 
     const {code} = currentLanguage;
 
     return (
       <Tool {...this.props}
+            writeProjectData={this.onWriteProjectData(currentToolName)}
+            onReadProjectData={this.onReadProjectData(currentToolName)}
             appLanguage={code}
-            currentToolViews={currentToolViews} />
+            currentToolViews={currentToolViews}/>
     );
   }
 }
 
 ToolsContainer.propTypes = {
-    toolsReducer: PropTypes.any.isRequired,
-    actions: PropTypes.any.isRequired,
-    contextIdReducer: PropTypes.any.isRequired,
-    currentLanguage: PropTypes.object.isRequired
+  projectSaveLocation: PropTypes.string.isRequired,
+  toolsReducer: PropTypes.any.isRequired,
+  actions: PropTypes.any.isRequired,
+  contextIdReducer: PropTypes.any.isRequired,
+  currentLanguage: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => {
   return {
+    projectSaveLocation: getProjectSaveLocation(state),
     toolsReducer: state.toolsReducer,
     loginReducer: state.loginReducer,
     settingsReducer: state.settingsReducer,
@@ -93,7 +145,8 @@ const mapDispatchToProps = (dispatch) => {
         dispatch(ResourcesActions.addNewBible(bibleName, bibleData));
       },
       loadResourceArticle: (resourceType, articleId, languageId) => {
-        dispatch(ResourcesActions.loadResourceArticle(resourceType, articleId, languageId));
+        dispatch(ResourcesActions.loadResourceArticle(resourceType, articleId,
+          languageId));
       },
       loadLexiconEntry: (lexiconId, entryId) => {
         dispatch(ResourcesActions.loadLexiconEntry(lexiconId, entryId));
@@ -114,7 +167,8 @@ const mapDispatchToProps = (dispatch) => {
         dispatch(selectModalTab(tab, section, vis));
       },
       editTargetVerse: (chapter, verse, before, after, tags, username) => {
-        dispatch(editTargetVerse(chapter, verse, before, after, tags, username));
+        dispatch(
+          editTargetVerse(chapter, verse, before, after, tags, username));
       },
       changeCurrentContextId: (contextId) => {
         dispatch(changeCurrentContextId(contextId));
@@ -129,22 +183,27 @@ const mapDispatchToProps = (dispatch) => {
         dispatch(setGroupsIndex(groupsIndex));
       },
       setToolSettings: (NAMESPACE, settingsPropertyName, toolSettingsData) => {
-        dispatch(setToolSettings(NAMESPACE, settingsPropertyName, toolSettingsData));
+        dispatch(
+          setToolSettings(NAMESPACE, settingsPropertyName, toolSettingsData));
       },
       openAlertDialog: (message) => {
         dispatch(openAlertDialog(message));
       },
       openOptionDialog: (alertMessage, callback, button1Text, button2Text) => {
-        dispatch(openOptionDialog(alertMessage, callback, button1Text, button2Text));
+        dispatch(
+          openOptionDialog(alertMessage, callback, button1Text, button2Text));
       },
       closeAlertDialog: () => {
         dispatch(closeAlertDialog());
       },
       moveWordBankItemToAlignment: (DropBoxItemIndex, WordBankItem) => {
-        dispatch(WordAlignmentActions.moveWordBankItemToAlignment(DropBoxItemIndex, WordBankItem));
+        dispatch(
+          WordAlignmentActions.moveWordBankItemToAlignment(DropBoxItemIndex,
+            WordBankItem));
       },
       moveTopWordItemToAlignment: (topWordItem, fromAlignmentIndex, toAlignmentIndex) => {
-        dispatch(WordAlignmentActions.moveTopWordItemToAlignment(topWordItem, fromAlignmentIndex, toAlignmentIndex));
+        dispatch(WordAlignmentActions.moveTopWordItemToAlignment(topWordItem,
+          fromAlignmentIndex, toAlignmentIndex));
       },
       moveBackToWordBank: (wordBankItem) => {
         dispatch(WordAlignmentActions.moveBackToWordBank(wordBankItem));
