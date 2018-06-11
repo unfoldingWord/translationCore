b05d2d614e8d8994de6511ee55f6a057cc08c3d9
Bruce McLean <mcleanb@photo-nomad.net>
2018-06-11 09:31:22 -0400

    Added link button support to Alert Modal Option dialog.

--- src/js/actions/AlertModalActions.js
+++ src/js/actions/AlertModalActions.js
@@ -22,17 +22,19 @@ export function openAlertDialog(alertMessage, loading) {
  * @description opens the option dialog with the specified alert message, and options.
  * @param {String} alertMessage - message to be displayed inside the alert dialog.
  * @param {function} callback - message to be displayed inside the alert dialog.
- * @param {String} button1Text - message to be displayed inside the alert dialog.
- * @param {String} button2Text - message to be displayed inside the alert dialog.
+ * @param {String} button1Text - button text to show right button.
+ * @param {String} button2Text - button text to show left of right button. (optional - if not present button is not added)
+ * @param {String} buttonLinkText - button text to show on left link button. (optional - if not present button is not added)
  * @return {Object} action content.
  */
-export function openOptionDialog(alertMessage, callback, button1Text, button2Text) {
+export function openOptionDialog(alertMessage, callback, button1Text, button2Text, buttonLinkText = null) {
   return {
     type: consts.OPEN_OPTION_DIALOG,
     alertMessage,
     callback,
     button1Text,
-    button2Text
+    button2Text,
+    buttonLinkText
   };
 }
 
--- src/js/reducers/alertModalReducer.js
+++ src/js/reducers/alertModalReducer.js
@@ -6,6 +6,7 @@ const initialState = {
   alertText: null,
   button1: null,
   button2: null,
+  buttonLink: null,
   callback: null
 };
 
@@ -19,6 +20,7 @@ const alertModalReducer = (state = initialState, action) => {
         alertText: action.alertMessage,
         button1: null,
         button2: null,
+        buttonLink: null,
         callback: null
       };
     case consts.OPEN_OPTION_DIALOG:
@@ -29,6 +31,7 @@ const alertModalReducer = (state = initialState, action) => {
         alertText: action.alertMessage,
         button1: action.button1Text,
         button2: action.button2Text,
+        buttonLink: action.buttonLinkText,
         callback: action.callback
       };
     case consts.CLOSE_ALERT_DIALOG:
