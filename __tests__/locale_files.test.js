f67069a47e9a0519523c22e7df2a66202b592a74
Richard Mahn <rich.mahn@unfoldingword.org>
2018-07-06 10:53:09 -0600

    Adds hindi translations for QA

--- __tests__/locale_files.test.js
+++ __tests__/locale_files.test.js
@@ -12,4 +12,40 @@ describe('Tests for locale files', () => {
     // then
     expect(invalidFiles.length).toEqual(expectedLength);
   });
+
+  test('Make sure English and Hindi have the same keys', () => {
+    // given
+    const localeDir = path.join(__dirname, '../src/locale');
+    // when
+    const englishLocale = fs.readJSONSync(path.join(localeDir, 'English-en_US.json'));
+    const hindiLocale = fs.readJSONSync(path.join(localeDir, 'Hindi-hi_IN.json'));
+    const flattenedEnglishLocale = flattenObject(englishLocale);
+    const flattenedHindiLocale = flattenObject(hindiLocale);
+    const englishKeys = Object.keys(flattenedEnglishLocale).sort();
+    const hindiKeys = Object.keys(flattenedHindiLocale).sort();
+    // then
+    expect(englishKeys).toEqual(hindiKeys);
+  });
 });
+
+// heleprs
+
+var flattenObject = function(ob) {
+	var toReturn = {};
+
+	for (var i in ob) {
+		if (!ob.hasOwnProperty(i)) continue;
+
+		if ((typeof ob[i]) == 'object') {
+			var flatObject = flattenObject(ob[i]);
+			for (var x in flatObject) {
+				if (!flatObject.hasOwnProperty(x)) continue;
+
+				toReturn[i + '.' + x] = flatObject[x];
+			}
+		} else {
+			toReturn[i] = ob[i];
+		}
+	}
+	return toReturn;
+};
