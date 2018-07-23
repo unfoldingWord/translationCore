ef297836bb6a3f85b4a2e6fad5a67d0fc46a6f12
mannycolon <colonmanuel7@gmail.com>
2018-07-23 17:46:58 -0400

    fixed unit tests

--- src/js/components/home/projectsManagement/ProjectCardMenu.js
+++ src/js/components/home/projectsManagement/ProjectCardMenu.js
@@ -2,9 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Popover from 'material-ui/Popover/Popover';
 import { Glyphicon } from 'react-bootstrap';
-import ReimportProjectIcon from '../../svgIcons/ReimportProjectIcon';
-
-// components
 
 class ProjectCardMenu extends React.Component {
 
