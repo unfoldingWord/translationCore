7b574fd790c3069fc5f4392857d092a686e84f69
RoyalSix <jay.a.scottjr@biola.edu>
2018-08-06 05:43:18 -0700

    updated tests

--- __tests__/ToolCardHelpers.test.js
+++ __tests__/ToolCardHelpers.test.js
@@ -5,29 +5,27 @@ import * as ToolCardHelpers from "../src/js/helpers/ToolCardHelpers";
 describe('Test ToolCardHelpers.getToolCardLaunchStatus() for correct launch status',()=>{
   const translate = (key) => key;
 
-  test('Should return the status that the book is not supported for translationWords', () => {
+  test('Should return the status that the book is supported for translationWords', () => {
     //given
-    const toolName = 'translationWords';
     const bookId = 'rom';
     const developerMode = false;
 
     //when
-    const status = ToolCardHelpers.isToolSupported(toolName, bookId, developerMode);
+    const status = ToolCardHelpers.isToolSupported(bookId, developerMode);
 
     //then
-    expect(status).toEqual(false);
+    expect(status).toEqual(true);
   });
 
   test('In developerMode, should return a GL needs to be selected for translationWords', () => {
     //given
-    const toolName = 'translationWords';
     const langId = null;
     const bookId = 'rom';
     const developerMode = true;
     const expectedStatus = 'tools.please_select_gl';
 
     //when
-    const status = ToolCardHelpers.getToolCardLaunchStatus(toolName, langId, bookId, developerMode, translate);
+    const status = ToolCardHelpers.getToolCardLaunchStatus(langId, bookId, developerMode, translate);
 
     //then
     expect(status).toEqual(expectedStatus);
@@ -35,13 +33,12 @@ describe('Test ToolCardHelpers.getToolCardLaunchStatus() for correct launch stat
 
   test('Even with a langId, should return the status that the book is not supported for translationWords', () => {
     //given
-    const toolName = 'translationWords';
     const bookId = 'rom';
     const developerMode = false;
-    const expectedStatus = false;
+    const expectedStatus = true;
 
     //when
-    const status = ToolCardHelpers.isToolSupported(toolName, bookId, developerMode);
+    const status = ToolCardHelpers.isToolSupported(bookId, developerMode);
 
     //then
     expect(status).toEqual(expectedStatus);
@@ -49,14 +46,13 @@ describe('Test ToolCardHelpers.getToolCardLaunchStatus() for correct launch stat
 
   test('In developerMode, it should return null for an unsupported book for translationWords', () => {
     //given
-    const toolName = 'translationWords';
     const langId = 'en';
     const bookId = 'rom';
     const developerMode = true;
     const expectedStatus = null;
 
     //when
-    const status = ToolCardHelpers.getToolCardLaunchStatus(toolName, langId, bookId, developerMode, translate);
+    const status = ToolCardHelpers.getToolCardLaunchStatus(langId, bookId, developerMode, translate);
 
     //then
     expect(status).toEqual(expectedStatus);
@@ -64,14 +60,13 @@ describe('Test ToolCardHelpers.getToolCardLaunchStatus() for correct launch stat
 
   test('Should return a GL needs to be selected for wordAlignment', () => {
     //given
-    const toolName = 'wordAlignment';
     const langId = null;
     const bookId = 'rom';
     const developerMode = false;
     const expectedStatus = 'tools.please_select_gl';
 
     //when
-    const status = ToolCardHelpers.getToolCardLaunchStatus(toolName, langId, bookId, developerMode, translate);
+    const status = ToolCardHelpers.getToolCardLaunchStatus(langId, bookId, developerMode, translate);
 
     //then
     expect(status).toEqual(expectedStatus);
@@ -79,14 +74,13 @@ describe('Test ToolCardHelpers.getToolCardLaunchStatus() for correct launch stat
 
   test('Even in developerMode, should return a GL needs to be selected for wordAlignment', () => {
     //given
-    const toolName = 'wordAlignment';
     const langId = null;
     const bookId = 'rom';
     const developerMode = true;
     const expectedStatus = 'tools.please_select_gl';
 
     //when
-    const status = ToolCardHelpers.getToolCardLaunchStatus(toolName, langId, bookId, developerMode, translate);
+    const status = ToolCardHelpers.getToolCardLaunchStatus(langId, bookId, developerMode, translate);
 
     //then
     expect(status).toEqual(expectedStatus);
@@ -94,14 +88,13 @@ describe('Test ToolCardHelpers.getToolCardLaunchStatus() for correct launch stat
 
   test('Should return null status for wordAlignment and a language', () => {
     //given
-    const toolName = 'wordAlignment';
     const langId = 'en';
     const bookId = 'rom';
     const developerMode = false;
     const expectedStatus = null;
 
     //when
-    const status = ToolCardHelpers.getToolCardLaunchStatus(toolName, langId, bookId, developerMode, developerMode, translate);
+    const status = ToolCardHelpers.getToolCardLaunchStatus(langId, bookId, developerMode, developerMode, translate);
 
     //then
     expect(status).toEqual(expectedStatus);
@@ -109,13 +102,12 @@ describe('Test ToolCardHelpers.getToolCardLaunchStatus() for correct launch stat
 
   test('Should return book not supported status for wordAlignment with an OT book', () => {
     //given
-    const toolName = 'wordAlignment';
     const bookId = 'psa';
     const developerMode = false;
     const expectedStatus = false;
 
     //when
-    const status = ToolCardHelpers.isToolSupported(toolName, bookId, developerMode);
+    const status = ToolCardHelpers.isToolSupported(bookId, developerMode);
 
     //then
     expect(status).toEqual(expectedStatus);
@@ -123,14 +115,13 @@ describe('Test ToolCardHelpers.getToolCardLaunchStatus() for correct launch stat
 
   test('Should return null status for translationWords tool, a language and bookId of "tit"', () => {
     //given
-    const toolName = 'translationWords';
     const langId = 'en';
     const bookId = 'tit';
     const developerMode = false;
     const expectedStatus = null;
 
     //when
-    const status = ToolCardHelpers.getToolCardLaunchStatus(toolName, langId, bookId, developerMode, translate);
+    const status = ToolCardHelpers.getToolCardLaunchStatus(langId, bookId, developerMode, translate);
 
     //then
     expect(status).toEqual(expectedStatus);
