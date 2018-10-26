94f2da398a3cd686242a3c76811335db8a8166e4
Joel Lonbeck <joel@neutrinographics.com>
2018-10-26 14:39:03 -0700

    reverted travis changes

---
+++ scripts/travis_tests.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+  eslint ./src || exit 1;
+  jest --coverage || exit 1;
+  codecov;
+  if [[ "$TRAVIS_PULL_REQUEST" == "false" ]] && [[ "$TRAVIS_BRANCH" != "develop" ]]; then
+    gulp build || exit 1;
+    gulp release || exit 1;
+  fi
+elif [[ "$TRAVIS_OS_NAME" == "osx" ]] && [[ "$TRAVIS_BRANCH" =~ ^release-.*$ ]] && [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+  gulp build --osx || exit 1;
+  gulp release --osx || exit 1;
+fi
