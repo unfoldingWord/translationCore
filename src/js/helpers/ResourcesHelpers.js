390d32979f3fc66966b9c4e9b7e56402d04948df
mannycolon <colonmanuel7@gmail.com>
2018-12-06 16:04:02 -0500

    fixed bug with userResources file list where I was using the wrong constant variable

--- src/js/helpers/ResourcesHelpers.js
+++ src/js/helpers/ResourcesHelpers.js
@@ -508,7 +508,7 @@ export function getMissingResources() {
   // resources files found in the user's resources directory
   const userResources = fs.readdirSync(USER_RESOURCES_PATH)
     .filter(file => file !== '.DS_Store')
-    .filter(file => fs.lstatSync(path.join(STATIC_RESOURCES_PATH, file)).isDirectory());
+    .filter(file => fs.lstatSync(path.join(USER_RESOURCES_PATH, file)).isDirectory());
 
   tcResourcesFiles.forEach((languageId) => {
     // if a resource package with tC executable file is missing in the user resource directory
