1b1f6a9cc208ae32249baf098ce0b90cdd8978ce
mannycolon <colonmanuel7@gmail.com>
2018-12-07 19:34:31 -0500

    Reolved merged conflicts

--- src/js/pages/app.js
+++ src/js/pages/app.js
@@ -53,15 +53,12 @@ class Main extends Component {
       loadTools
     } = this.props;
 
-<<<<<<< HEAD
-    if (localStorage.getItem('version') !== packageJson.version) {
-=======
     loadTools(path.join(__dirname, '../../../tC_apps'));
-    const tcResourcesPath = path.join(ospath.home(), 'translationCore', 'resources');
-    if (localStorage.getItem('version') !== packageJson.version || !fs.existsSync(tcResourcesPath)) {
->>>>>>> develop
+
+    if (localStorage.getItem('version') !== packageJson.version) {
       localStorage.setItem('version', packageJson.version);
     }
+
     migrateResourcesFolder();
     // migration logic for toolsSettings in settings.json
     migrateToolsSettings();
