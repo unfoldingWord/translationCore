8fb5739d1e38458b780fcfba3e6c1b0c052b19b5
RoyalSix <jay.a.scottjr@biola.edu>
2018-12-10 09:55:44 -0800

    fixed tests

--- __tests__/exportHelpers.test.js
+++ __tests__/exportHelpers.test.js
@@ -28,49 +28,26 @@ describe('exportHelpers.getUsfmExportName', () => {
 });
 
 describe('exportHelpers.getHeaderTags', () => {
+  const bookName = 'tit';
+  const project_name = `en_${bookName}`;
+  const projectSaveLocation = path.join(PROJECTS_PATH, project_name);
+  beforeEach(() => {
+    const sourcePath = path.join('__tests__', 'fixtures', 'project');
+    const copyFiles = [project_name];
+    fs.__resetMockFS();
+    fs.__loadFilesIntoMockFs(copyFiles, sourcePath, PROJECTS_PATH);
+  });
+  afterEach(() => {
+    fs.__resetMockFS();
+  });
   it('should get the usfm header of a titus project according to the standard', () => {
-    manifestHelpers.getProjectManifest = jest.fn( () => { 
-      const manifest = {
-        "target_language": {
-          "id": "en",
-          "name": "English",
-          "direction": "ltr",
-          "book": {
-            "name": "2 Peter"
-          }
-        },
-        "project": {
-          "id": "2PE"
-        },
-        "source_translations": [
-          {
-            "language_id": "en",
-            "resource_id": "ult"
-          }
-        ],
-        "resource": {
-          "id": "xyz",
-          "name": "xyz is translation id"
-        }
-      };
-    
-      return manifest;  
-    });
-
-    let projectSaveLocation = path.join('fixtures', 'project', 'en_2pe');
-    const expectedResult = "2PE EN_XYZ en_English_ltr Not-a-real-date tc";
+    const expectedResult = "TIT N/A en_English_ltr Not-a-real-date tc";
     const header = exportHelpers.getHeaderTags(projectSaveLocation);
     expect(header[0].content).toBe(expectedResult);
   });
 
   it('should preserve old header id tag', () => {
     const preservedIDContent = 'unfoldingWord Literal Text';
-    const bookName = 'tit';
-    const project_name = `en_${bookName}`;
-    const sourcePath = path.join('__tests__', 'fixtures', 'project');
-    const copyFiles = [project_name];
-    fs.__loadFilesIntoMockFs(copyFiles, sourcePath, PROJECTS_PATH);
-    const projectSaveLocation = path.join(PROJECTS_PATH, project_name);
     fs.writeJSONSync(path.join(projectSaveLocation, bookName, 'headers.json'), [{"tag":"id", "content":`2TI ${preservedIDContent}`}]);
     const headers = exportHelpers.getHeaderTags(projectSaveLocation);
     expect(headers[0].content.includes(preservedIDContent)).toBeTruthy();
