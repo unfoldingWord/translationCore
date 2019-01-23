8f0fd497d171de09254ab52352ce28cab2a0473f
Bruce McLean <mcleanb@photo-nomad.net>
2019-01-23 10:50:45 -0500

    fixed unit tests to handle latest lexicon data version.

--- src/js/helpers/LexiconHelpers.js
+++ src/js/helpers/LexiconHelpers.js
@@ -9,8 +9,8 @@ export function getLexiconData(lexiconId, entryId) {
   try {
     const languageId = 'en'; // TODO: need to add other language support
     // generate path from resourceType and articleId and get latest version
-    const latestVersion = ResourceAPI.getLatestVersion(path.join(USER_RESOURCES_PATH, languageId, 'lexicons', lexiconId));
-    const entryPath = path.join(latestVersion, 'content', entryId + '.json');
+    const latestVersionPath = ResourceAPI.getLatestVersion(path.join(USER_RESOURCES_PATH, languageId, 'lexicons', lexiconId));
+    const entryPath = path.join(latestVersionPath, 'content', entryId + '.json');
     let entryData;
     if (fs.existsSync(entryPath)) {
       entryData = fs.readJsonSync(entryPath, 'utf8'); // get file from fs
