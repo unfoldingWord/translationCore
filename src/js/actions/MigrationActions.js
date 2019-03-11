cc3f7a540065d39fece02a1e5e2707c5afbb29fd
mannycolon <colonmanuel7@gmail.com>
2019-03-11 03:08:27 -0400

    Added better comment description to migrateResourcesFolder

--- src/js/actions/MigrationActions.js
+++ src/js/actions/MigrationActions.js
@@ -14,9 +14,10 @@ const USER_RESOURCES_PATH = path.join(ospath.home(), 'translationCore/resources'
 export function migrateResourcesFolder() {
   return (() => {
     if (!fs.existsSync(path.join(USER_RESOURCES_PATH, 'source-content-updater-manifest.json'))) {
-      fs.removeSync(USER_RESOURCES_PATH);
-      copySourceContentUpdaterManifest();
-      getMissingResources();
+      // If found an old resources folder before source-content-updater implementation.
+      fs.removeSync(USER_RESOURCES_PATH);// Delete old resources folder
+      copySourceContentUpdaterManifest();// Add source-content-updater-manifest.json
+      getMissingResources();// Get missing resources.
     } else {
       getMissingResources();
     }
