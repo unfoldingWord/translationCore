d1ed0563e4b9cbd5063933f671f20948ea7cb88c
RoyalSix <jay.a.scottjr@biola.edu>
2019-04-18 11:30:53 -0700

    updated tests

--- src/js/helpers/__mocks__/ProjectAPI.js
+++ src/js/helpers/__mocks__/ProjectAPI.js
@@ -6,6 +6,10 @@ export const mockGetBookId = jest.fn();
 export const mockGetSelectedCategories = jest.fn();
 export const mockSetSelectedCategories = jest.fn();
 export const mockSetCategoryGroupIds = jest.fn();
+export const mockResetCategoryGroupIds = jest.fn();
+export const mockHasNewGroupsData = jest.fn();
+export const mockResetLoadedCategories = jest.fn();
+export const mockRemoveStaleCategoriesFromCurrent = jest.fn();
 
 const mock = jest.fn().mockImplementation(() => {
   return {
@@ -17,6 +21,10 @@ const mock = jest.fn().mockImplementation(() => {
     getBookId: mockGetBookId,
     getSelectedCategories: mockGetSelectedCategories,
     setSelectedCategories: mockSetSelectedCategories,
+    resetCategoryGroupIds: mockResetCategoryGroupIds,
+    hasNewGroupsData: mockHasNewGroupsData,
+    resetLoadedCategories: mockResetLoadedCategories,
+    removeStaleCategoriesFromCurrent: mockRemoveStaleCategoriesFromCurrent
   };
 });
 
