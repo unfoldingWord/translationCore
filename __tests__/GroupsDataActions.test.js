e91bbab87e672069c8110fb584be46fe111bfe47
Bruce McLean <mcleanb@photo-nomad.net>
2019-05-16 10:33:51 -0400

    adding unit tests for verifyGroupDataMatchesWithFs().

--- __tests__/GroupsDataActions.test.js
+++ __tests__/GroupsDataActions.test.js
@@ -6,6 +6,7 @@ import path from 'path-extra';
 import { generateTimestamp } from '../src/js/helpers';
 import * as GroupsDataActions from '../src/js/actions/GroupsDataActions';
 import * as saveMethods from '../src/js/localStorage/saveMethods';
+import {delay} from "../src/js/helpers/bodyUIHelpers";
 
 jest.mock('redux-batched-actions', () => ({
   batchActions: (actionsBatch) => {
@@ -40,7 +41,7 @@ describe('GroupsDataActions.verifyGroupDataMatchesWithFs', () => {
     removeSpy(saveOtherContextSpy);
   });
 
-  it('should succeed without external verse edits', () => {
+  it('should succeed without external verse edits', async () => {
     // given
     const bookId = 'tit';
     const groupsDataReducer = fs.readJsonSync(path.join(CHECK_DATA_PATH, 'en_tit', 'groupsDataReducer.json'));
@@ -59,17 +60,49 @@ describe('GroupsDataActions.verifyGroupDataMatchesWithFs', () => {
       }
     };
     const store = mockStore(initStore);
-    const expectedSelectionChanges = 0;
 
     // when
     store.dispatch(GroupsDataActions.verifyGroupDataMatchesWithFs());
+    await delay(1000);
 
     // then
     const actions = store.getActions();
     expect(cleanOutDates(actions)).toMatchSnapshot();
-    expect(saveOtherContextSpy).toHaveBeenCalledTimes(expectedSelectionChanges);
   });
-});
+
+  it('should succeed with external verse edits', async () => {
+    // given
+    const bookId = 'tit';
+    const groupsDataReducer = fs.readJsonSync(path.join(CHECK_DATA_PATH, 'en_tit', 'groupsDataReducer.json'));
+    const initStore = {
+      groupsDataReducer,
+      toolsReducer: {
+        selectedTool: 'translationWords'
+      },
+      projectDetailsReducer: {
+        manifest: {
+          project: {
+            id: bookId
+          }
+        },
+        projectSaveLocation: PROJECTS_PATH
+      }
+    };
+    const store = mockStore(initStore);
+    const verseEdit = {"verseBefore":"To Titus, a true son in our common faith. Grace and peace from God the Father and Christ Jesus our savior.\n\\p","verseAfter":"To Titus, a true son in our common faith. Grace and peace from God the Father and Christ Jesus our savior.\n\\p Edit 1:4","tags":["other"],"userName":"photonomad1","activeBook":"tit","activeChapter":1,"activeVerse":1,"modifiedTimestamp":"2019-05-16T12:11:45.970Z","gatewayLanguageCode":"en","gatewayLanguageQuote":"","contextId":{"reference":{"bookId":"tit","chapter":1,"verse":4},"tool":"wordAlignment","groupId":"chapter_1"}};
+    const verseEditPath = path.join(PROJECTS_PATH, ".apps/translationCore/checkData", "verseEdits", bookId, "1", "4");
+    fs.ensureDirSync(verseEditPath);
+    const fileName = generateTimestamp() + ".json";
+    fs.outputJsonSync(path.join(verseEditPath, fileName), verseEdit);
+
+    // when
+    store.dispatch(GroupsDataActions.verifyGroupDataMatchesWithFs());
+    await delay(1000);
+
+    // then
+    const actions = store.getActions();
+    expect(cleanOutDates(actions)).toMatchSnapshot();
+  });});
 
 describe('GroupsDataActions.validateBookSelections', () => {
   const bookId = 'tit';
