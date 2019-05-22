3bd34c8ba0f1cb341aae91479bff99d91588ac37
Bruce McLean <mcleanb@photo-nomad.net>
2019-05-22 15:56:14 -0400

    fixed to reduce duplicate verse edits. grouped verse edits by groupId added operation TOGGLE_MULTIPLE_VERSE_EDITS_IN_GROUPDATA to allow passing an array of verse edits in one call removed unneeded slow JSON clone operation in getToggledGroupData()

--- src/js/actions/VerseEditActions.js
+++ src/js/actions/VerseEditActions.js
@@ -70,7 +70,7 @@ export const writeTranslationWordsVerseEditToFile = (verseEdit) => {
  * batch setting verse edit flags for all tw checks in verse if not set
  * @param {Object} state - current state
  * @param {Object} contextId - of verse edit
- * @param {Array} actionsBatch - array append with verse edits to be batched
+ * @param {Object} actionsBatch - gets loaded with verse edits to be batched (indexed by groupId)
  */
 export const getCheckVerseEditsInGroupData = (state, contextId, actionsBatch) => {
   // in group data reducer set verse edit flag for every check of the verse edited
@@ -83,7 +83,11 @@ export const getCheckVerseEditsInGroupData = (state, contextId, actionsBatch) =>
         for (let j = 0, lenGI = groupItem.length; j < lenGI; j++) {
           const check = groupItem[j];
           if (!check.verseEdits) { // only set if not yet set
-            actionsBatch.push({
+            const groupId = check.contextId.groupId;
+            if (!actionsBatch[groupId]) {
+              actionsBatch[groupId] = [];
+            }
+            actionsBatch[groupId].push({
               type: types.TOGGLE_VERSE_EDITS_IN_GROUPDATA,
               contextId: check.contextId
             });
@@ -96,21 +100,43 @@ export const getCheckVerseEditsInGroupData = (state, contextId, actionsBatch) =>
 
 /**
  * make sure verse edit flag is set for all tw checks in verses
- * @param {Array} twVerseEdits - of contextIds for each verse edit
+ * @param {Object} twVerseEdits - indexed by verse - contextIds for each verse edit
  * @return {Function}
  */
 export const ensureCheckVerseEditsInGroupData = (twVerseEdits) => {
   return async (dispatch, getState) => {
-    if (twVerseEdits && twVerseEdits.length) {
+    const versesEdited = Object.keys(twVerseEdits);
+    if (versesEdited && versesEdited.length) {
       const state = getState();
-      const actionsBatch = [];
-      for (let i = 0, lenVE = twVerseEdits.length; i < lenVE; i++) {
-        const contextId = twVerseEdits[i];
-        getCheckVerseEditsInGroupData(state, contextId, actionsBatch);
+      const editedChecks = {};
+      for (let i = 0, lenVE = versesEdited.length; i < lenVE; i++) {
+        const contextId = twVerseEdits[versesEdited[i]];
+        getCheckVerseEditsInGroupData(state, contextId, editedChecks);
       }
-      if (actionsBatch.length) {
+      const batch = [];
+      if (editedChecks) {
+        const groupIds = Object.keys(editedChecks);
+        let count = 0;
         await delay(500);
-        dispatch(batchActions(actionsBatch));
+        // process by group
+        for (let j = 0, l = groupIds.length; j < l; j++) {
+          const groupId = groupIds[j];
+          // combine multiple verse edits into one call
+          const verseEdits = editedChecks[groupId];
+          const references = verseEdits.map(item => (item.contextId.reference));
+          batch.push({
+            type: types.TOGGLE_MULTIPLE_VERSE_EDITS_IN_GROUPDATA,
+            groupId,
+            references
+          });
+          count += references.length;
+        }
+        if (batch.length) {
+          console.log("ensureCheckVerseEditsInGroupData() - edited verses=" + versesEdited.length);
+          dispatch(batchActions(batch));
+          console.log("ensureCheckVerseEditsInGroupData() - total checks changed=" + count);
+          console.log("ensureCheckVerseEditsInGroupData() - batch finished, groupId's edited=" + groupIds.length);
+        }
       }
     }
   };
--- src/js/reducers/groupsDataReducer.js
+++ src/js/reducers/groupsDataReducer.js
@@ -1,5 +1,7 @@
 import consts from '../actions/ActionTypes';
 import {getToggledGroupData} from '../helpers/getToggledGroupData';
+import isEqual from "deep-equal";
+import _ from "lodash";
 
 const initialState = {
   groupsData: {},
@@ -65,6 +67,14 @@ const groupsDataReducer = (state = initialState, action) => {
           [action.contextId.groupId]: getToggledGroupData(state, action, "verseEdits")
         }
       };
+    case consts.TOGGLE_MULTIPLE_VERSE_EDITS_IN_GROUPDATA:
+      return {
+        ...state,
+        groupsData: {
+          ...state.groupsData,
+          [action.groupId]: setMultipleVerseEdits(state, action)
+        }
+      };
     case consts.TOGGLE_COMMENTS_IN_GROUPDATA:
       return {
         ...state,
@@ -80,7 +90,29 @@ const groupsDataReducer = (state = initialState, action) => {
   }
 };
 
-export const getGroupsData = (state) => 
+/**
+ *
+ * @param {Object} state
+ * @param {{groupId:String, references:Array}} action
+ * @return {*} updated group data for groupId
+ */
+function setMultipleVerseEdits(state, action) {
+  let groupData = state.groupsData[action.groupId];
+  if (!groupData) return groupData;
+  const newGroupData = _.cloneDeep(groupData);
+  for (let i = 0, l = action.references.length; i < l; i++) {
+    const reference = action.references[i];
+    for (let k = 0, lGD = newGroupData.length; k < lGD; k++) {
+      const item = newGroupData[k];
+      if (isEqual(item.contextId.reference, reference)) {
+        item.verseEdits = true;
+      }
+    }
+  }
+  return newGroupData;
+}
+
+export const getGroupsData = (state) =>
   state.groupsData;
 
 export default groupsDataReducer;
