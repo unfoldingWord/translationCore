caa64a30b0ab7500fa753f7d665c5b1cd307236e
mannycolon <colonmanuel7@gmail.com>
2019-07-24 00:41:53 -0400

    Made necessary changes in ToolCardBoxes to support GL category changes

--- src/js/components/home/toolsManagement/ToolCardBoxes.js
+++ src/js/components/home/toolsManagement/ToolCardBoxes.js
@@ -1,77 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Checkbox from '@material-ui/core/Checkbox';
-import isEqual from 'deep-equal';
-import { withStyles } from '@material-ui/core/styles';
 import {Glyphicon} from 'react-bootstrap';
-import {tNotesCategories} from "tsv-groupdata-parser";
-import * as ResourcesActions from "../../../actions/ResourcesActions";
-import {parseArticleAbstract} from "../../../helpers/ToolCardHelpers";
+// components
+import CategoryCheckbox from './CategoryCheckbox';
+import SubcategoryCheckbox from './SubcategoryCheckbox';
 import Hint from "../../Hint";
-import { toolCardCategories, TRANSLATION_WORDS, TRANSLATION_ACADEMY } from '../../../common/constants';
+// helpers
+import {loadArticleData} from "../../../helpers/ResourcesHelpers";
+import {parseArticleAbstract} from "../../../helpers/ToolCardHelpers";
+import {
+  flattenNotesCategories,
+  hasAllNeededData,
+  sortSubcategories
+} from '../../../helpers/toolCardBoxesHelpers';
+// constants
+import { TRANSLATION_WORDS, TRANSLATION_ACADEMY } from '../../../common/constants';
 
 const styles = {
-  root: {
-    color: 'var(--accent-color-dark)',
-    '&$checked': {
-      color: 'var(--accent-color-dark)',
-    },
-    padding: '0px',
+  categoriesDiv: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    margin: '0 0 5 0',
+    width: '100%'
   },
-  checked: {},
+  glyphicon: {
+    fontSize: '18px',
+    margin: '0 12px 0 0',
+    width: '20px',
+    textAlign: 'right'
+  },
+  subcategoriesDiv: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    alignItems: 'left',
+    marginBottom: 5,
+    width: '100%'
+  }
 };
-
-/**
- * turn group object of category objects into set of objects
- */
-function flattenNotesCategories() {
-  let lookupNames = {};
-  Object.keys(tNotesCategories).forEach(item => {
-    lookupNames[item] = 'tool_card_categories.' + item;
-    Object.keys(tNotesCategories[item]).forEach(subItem => {
-      lookupNames[subItem] = 'tool_card_categories.' + subItem;
-    });
-  });
-
-  Object.keys(toolCardCategories).forEach(item => {
-    lookupNames[item] = 'tool_card_categories.' + item;
-  });
-  return lookupNames;
-}
-
-
-/**
- * Display a preconfigured checkbox with passed params
- * @param {*} selectedCategories
- * @param {*} id
- * @param {*} toolName
- * @param {*} onChecked
- */
-function localCheckBox(classes, selectedCategories, id, toolName, onChecked, availableSubcategories = []) {
-  const isParent = !!availableSubcategories.length;
-  const currentCategoriesSelected = availableSubcategories.filter((subcategory) => selectedCategories.includes(subcategory));
-  const allChildrenSelected = isEqual(availableSubcategories, currentCategoriesSelected);
-  const allChildrenUnselected = currentCategoriesSelected.length === 0;
-  const showIndeterminate = !allChildrenUnselected && currentCategoriesSelected.length > 0 && !allChildrenSelected;
-
-  return (
-    <Checkbox
-      checked={selectedCategories.includes(id) || (isParent && allChildrenSelected)}
-      indeterminate={isParent && showIndeterminate && toolName !== TRANSLATION_WORDS}
-      classes={{
-        root: classes.root,
-        checked: classes.checked,
-      }}
-      onChange={(e) => {
-        if (isParent) {
-          onChecked(availableSubcategories, e.target.checked, toolName);
-        } else {
-          onChecked(id, e.target.checked, toolName);
-        }
-      }}
-    />
-  );
-}
 
 class ToolCardBoxes extends React.Component {
   constructor(props) {
@@ -92,22 +57,22 @@ class ToolCardBoxes extends React.Component {
   loadArticles(){
     let fullText = "";
     let articles = {};
+    const { availableCategories, selectedGL } = this.props;
 
-    for(var cat in this.props.availableCategories) {
-      const category = this.props.availableCategories[cat];
-
-      for(var group in category ) {
-        fullText = ResourcesActions.loadArticleData(
+    for(const cat in availableCategories) {
+      const category = availableCategories[cat];
+      for(const group in category) {
+        fullText = loadArticleData(
           TRANSLATION_ACADEMY,
-          category[group],
-          this.props.selectedGL
+          category[group].id,
+          selectedGL
         );
 
-        articles[category[group]] = fullText.substr(0,600) ;
+        articles[category[group].id] = fullText.substr(0,600) ;
       }
     }
 
-    this.setState( {"articles": articles} );
+    this.setState({ articles });
   }
 
   showExpanded(id) {
@@ -119,8 +84,8 @@ class ToolCardBoxes extends React.Component {
     });
   }
 
-  getArticleText(category) {
-    const fullText = this.state.articles[category];
+  getArticleText(categoryId) {
+    const fullText = this.state.articles[categoryId];
     const {title, intro} = parseArticleAbstract(fullText);
     return title + ": " + intro;
   }
@@ -130,9 +95,9 @@ class ToolCardBoxes extends React.Component {
       availableCategories = {},
       toolName,
       selectedCategories,
-      onChecked,
       translate,
-      classes,
+      onCategoryChecked,
+      onSubcategoryChecked,
     } = this.props;
     const lookupNames = flattenNotesCategories();
 
@@ -141,63 +106,57 @@ class ToolCardBoxes extends React.Component {
         {
           Object.keys(availableCategories).map((parentCategory, index) => {
             const subcategories = availableCategories[parentCategory];
-            // use other_terms as parentCategory for the othet category for translationWords tool
-            parentCategory = parentCategory === 'other' && toolName === TRANSLATION_WORDS ?
-              'other_terms' : parentCategory;
-            const hasAllNeededData = !!lookupNames[parentCategory] && subcategories.length > 0 &&
-              (tNotesCategories[parentCategory] || toolName === TRANSLATION_WORDS);
+            // use other_terms as parentCategory for the category 'other' for translationWords tool
+            parentCategory = parentCategory === 'other' && toolName === TRANSLATION_WORDS ? 'other_terms' : parentCategory;
 
-            if (hasAllNeededData) {
+            if (hasAllNeededData(toolName, parentCategory, subcategories, lookupNames)) {
               return (
-                <div style={{display: 'flex', flexWrap: 'wrap', margin: '0 0 5 0', width: '100%'}} key={index}>
+                <div key={index} style={styles.categoriesDiv}>
                   <div style={{display: 'flex', width: '92%'}}>
-                    <div style={{width: '38px'}} >
-                      {localCheckBox(classes, selectedCategories, parentCategory, toolName, onChecked, subcategories)}
+                    <div style={{width: '38px'}}>
+                      <CategoryCheckbox
+                        toolName={toolName}
+                        onCategoryChecked={onCategoryChecked}
+                        selectedCategories={selectedCategories}
+                        availableSubcategories={subcategories}
+                      />
                     </div>
                     <div>
-                      {translate(lookupNames[parentCategory])   /* category label */}
+                      {/* category label */translate(lookupNames[parentCategory])}
                     </div>
                   </div>
                   <React.Fragment>
-                    {toolName !== TRANSLATION_WORDS ? (
+                    {toolName !== TRANSLATION_WORDS &&
                       <div style={{alignSelf: 'flex-end'}}>
-                        <Glyphicon // ^ or v
-                          style={{
-                            fontSize: '18px', margin: '0 12px 0 0',
-                            width: '20px', textAlign: 'right'
-                          }}
+                        <Glyphicon
+                          style={styles.glyphicon}
                           glyph={this.state.expanded[parentCategory] ? 'chevron-up' : 'chevron-down'}
                           onClick={() => this.showExpanded(parentCategory)}
                         />
                       </div>
-                    ) : null
                     }
-                    {this.state.expanded[parentCategory] ? (
-                      <div style={{
-                        display: 'flex', flexWrap: 'wrap', alignItems: 'left',
-                        marginBottom: 5, width: '100%'
-                      }} key={index}>
-                        {subcategories.sort((a, b) => {
-                          a = translate(lookupNames[a]);
-                          b = translate(lookupNames[b]);
-                          if (a < b) return -1;
-                          else if (a > b) return 1;
-                          return 0;
-                        }).map((subcategory, index) => (
-                          <div style={{display: 'flex', width: '48%'}} key={index} >
+                    {this.state.expanded[parentCategory] && (
+                      <div key={index} style={styles.subcategoriesDiv}>
+                        {sortSubcategories(subcategories).map((subcategory, index) => (
+                          <div key={index} style={{display: 'flex', width: '48%'}}>
                             <div style={{marginLeft: '36px', marginRight: '10px'}}>
-                              {localCheckBox(classes, selectedCategories, subcategory, toolName, onChecked)}
+                              <SubcategoryCheckbox
+                                subcategory={subcategory}
+                                toolName={toolName}
+                                onSubcategoryChecked={onSubcategoryChecked}
+                                selectedCategories={selectedCategories}
+                              />
                             </div>
-                            <Hint position={((index % 2) === 1) ? 'top-left' : 'top-right'} label={this.getArticleText(subcategory)} size={'large'}>
+                            <Hint position={((index % 2) === 1) ? 'top-left' : 'top-right'} label={this.getArticleText(subcategory.id)} size={'large'}>
                               <div style={{cursor: "pointer"}} >
-                                {translate(lookupNames[subcategory])}
+                                {subcategory.name}
                               </div>
                             </Hint>
                           </div>
                         ))
                         }
                       </div>
-                    ) : null}
+                    )}
                   </React.Fragment>
                 </div>
               );
@@ -213,7 +172,8 @@ class ToolCardBoxes extends React.Component {
 
 ToolCardBoxes.propTypes = {
   availableCategories: PropTypes.object.isRequired,
-  onChecked: PropTypes.func,
+  onCategoryChecked: PropTypes.func.isRequired,
+  onSubcategoryChecked: PropTypes.func.isRequired,
   selectedCategories: PropTypes.array.isRequired,
   toolName: PropTypes.string.isRequired,
   translate: PropTypes.func.isRequired,
@@ -221,7 +181,6 @@ ToolCardBoxes.propTypes = {
   selectedGL: PropTypes.string.isRequired,
   showPopover: PropTypes.func.isRequired,
   closePopover: PropTypes.func,
-  classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(ToolCardBoxes);
+export default ToolCardBoxes;
