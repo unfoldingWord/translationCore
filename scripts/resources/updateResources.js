d2e10e75050d1dd311ab9873c9df0d9b825f39dc
Bruce McLean 1 <photonomad1@noreply.door43.org>
2019-12-26 16:49:55 -0500

    added check to see how recently resources were updated to speed up automated builds.

--- scripts/resources/updateResources.js
+++ scripts/resources/updateResources.js
@@ -35,32 +35,98 @@ const updateResources = async (languages, resourcesPath) => {
   }
 };
 
-const executeResourcesUpdate = async (languages, resourcesPath) => {
-  const importsPath = path.join(resourcesPath, 'imports'); // Remove old imports folder
+/**
+ * get last update resources time
+ * @param {String} resourcesPath
+ * @return {null|Date}
+ */
+const getResourceUpdateTime = (resourcesPath) => {
+  const sourceContentManifestPath = path.join(resourcesPath, 'source-content-updater-manifest.json');
+  let manifest = {};
 
-  if (fs.existsSync(importsPath)) { // do safe folder delete of imports
-    const tempPath = importsPath + '.temp';
-    fs.moveSync(importsPath, tempPath);
-    fs.removeSync(tempPath);
+  if (fs.existsSync(sourceContentManifestPath)) {
+    manifest = fs.readJSONSync(sourceContentManifestPath);
   }
 
-  let errors = await updateResources(languages, resourcesPath);
-
-  if (errors) {
-    console.log('Errors on downloading updated resources!!');
+  if (manifest && manifest.modified) {
+    return new Date(manifest.modified);
   }
-  console.log('Zipping up updated resources');
+  return null;
+};
 
-  languages.forEach(async (languageId) => {
-    try {
-      await zipResourcesContent(resourcesPath, languageId);
-    } catch (e) {
-      errors += e.toString() + '\n';
+/**
+ * returns true if resources were recently updated
+ * @param {String} resourcesPath
+ * @return {Boolean}
+ */
+const areResourcesRecent = (resourcesPath) => {
+  const threshold = 60 * 60; // threshold is 1 hour
+  const updateTime = getResourceUpdateTime(resourcesPath);
+
+  if (updateTime) {
+    const currentTime = new Date();
+    let secondsDif = (currentTime.getTime() - updateTime.getTime()) / 1000;
+
+    if (secondsDif < 0) {
+      secondsDif = 0;
     }
-  });
 
-  // update source content updater manifest, but don't clobber tCore version
-  UpdateResourcesHelpers.updateSourceContentUpdaterManifest(resourcesPath);
+    if (secondsDif < 60) {
+      console.log(`areResourcesRecent() - ${secondsDif} seconds elapsed since last update`);
+    } else {
+      const minutesDif = secondsDif / 60;
+
+      if (minutesDif < 60) {
+        console.log(`areResourcesRecent() - ${minutesDif} minutes elapsed since last update`);
+      } else {
+        const hoursDif = minutesDif / 60;
+
+        if (hoursDif < 60) {
+          console.log(`areResourcesRecent() - ${hoursDif} hours elapsed since last update`);
+        } else {
+          const daysDif = hoursDif / 24;
+          console.log(`areResourcesRecent() - ${daysDif} days elapsed since last update`);
+        }
+      }
+    }
+    return secondsDif < threshold;
+  }
+
+  return false;
+};
+
+const executeResourcesUpdate = async (languages, resourcesPath) => {
+  let errors = false;
+
+  if (areResourcesRecent(resourcesPath)) {
+    console.log('Resources recently updated, so nothing to do');
+  } else {
+    const importsPath = path.join(resourcesPath, 'imports');// Remove old imports folder
+
+    if (fs.existsSync(importsPath)) { // do safe folder delete of imports
+      const tempPath = importsPath + '.temp';
+      fs.moveSync(importsPath, tempPath);
+      fs.removeSync(tempPath);
+    }
+
+    errors = await updateResources(languages, resourcesPath);
+
+    if (errors) {
+      console.log('Errors on downloading updated resources!!');
+    }
+    console.log('Zipping up updated resources');
+
+    languages.forEach(async (languageId) => {
+      try {
+        await zipResourcesContent(resourcesPath, languageId);
+      } catch (e) {
+        errors += e.toString() + '\n';
+      }
+    });
+
+    // update source content updater manifest, but don't clobber tCore version
+    UpdateResourcesHelpers.updateSourceContentUpdaterManifest(resourcesPath);
+  }
 
   if (errors) {
     console.log('Errors on downloading updated resources:\n' + errors);
