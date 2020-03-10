5438223c4b02b82550d9fbfbc8a26638d38cbe4a
Bruce McLean <mcleanb@photo-nomad.net>
2020-03-10 14:27:53 -0400

    fix for calling isToolUsingCurrentOriginalLanguage() when launching tN

--- src/js/components/home/toolsManagement/ToolCard.js
+++ src/js/components/home/toolsManagement/ToolCard.js
@@ -148,6 +148,7 @@ class ToolCard extends Component {
     const {
       tool,
       translate,
+      isToolUsingCurrentOriginalLanguage,
       actions: {
         openOptionDialog,
         closeAlertDialog,
