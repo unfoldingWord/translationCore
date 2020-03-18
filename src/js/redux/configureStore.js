736fb989615dfd0a8107e3e026aa7764c1491ffe
Bruce McLean 1 <photonomad1@noreply.door43.org>
2020-03-18 15:56:36 -0400

    restore redux-logger

--- src/js/redux/configureStore.js
+++ src/js/redux/configureStore.js
@@ -11,15 +11,38 @@ let middlewares = [
   promise,
 ];
 
+// TRICKY: this is to limit nesting in logging to prevent crashing console.log()
+const maxStateLevel = 4; // maximum depth for state logging, can set up to 5 (warning this will run more slowly and consume more memory)
+const showFullDepth = true; // set this to true to display deep objects as JSON strings rather than ellipsis (warning this will run more slowly and consume more memory)
+const stateTransformer = (state, level = maxStateLevel) => {
+  if (level <= 0) {
+    return showFullDepth ? stringifySafe(state) : "…"; // at this point replace with string to protect console.log() from objects too deep
+  }
+
+  let newState = {};
+  let keys = (typeof state === "object" && state !== null && Object.keys(state)) || [];
+
+  if (keys.length) {
+    for (let i = 0, l = keys.length; i < l; i++) {
+      const key = keys[i];
+      newState[key] = stateTransformer(state[key], level - 1);
+    }
+  } else {
+    newState = state;
+  }
+
+  return newState;
+};
+
 if (process.env.REDUX_LOGGER || process.env.NODE_ENV === 'development') {
-  //TODO: this is a hack to keep redux logger from crashing, but still saw crash when opened project
-  // middlewares.push(createLogger(
-  //   {
-  //     diff: false,
-  //     stateTransformer: (state) => (stringifySafe(state,
-  //       '[error loading system information]'))
-  //   }
-  // ));
+  middlewares.push(createLogger(
+    {
+      diff: false,
+      logErrors: false, // disable catch and rethrow of errors in redux-logger
+      stateTransformer,
+      actionTransformer: stateTransformer,
+    }
+  ));
 }
 
 export default function configureStore(persistedState) {
