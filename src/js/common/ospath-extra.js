8da2f32475c50efea6fea8f6e5af277369f35009
Bruce McLean 1 <photonomad1@noreply.door43.org>
2020-03-18 16:59:08 -0400

    cleanup

--- src/js/common/ospath-extra.js
+++ src/js/common/ospath-extra.js
@@ -7,7 +7,7 @@ var os = require('os');
 
 var processEnv = require('./env').getEnv();
 
-function data () {
+function data() {
   switch (this.__platform || process.platform) {
     case 'win32': return path.resolve(processEnv.APPDATA);
     case 'darwin': return path.resolve(path.join(home.call(this), 'Library/Application Support/'));
@@ -17,21 +17,23 @@ function data () {
   }
 }
 
-function desktop () {
+function desktop() {
   return path.join(home.call(this), 'Desktop');
 }
 
-function home () {
+function home() {
   // io.js >= 2.3
-  if ('homedir' in os) return os.homedir();
+  if ('homedir' in os) {
+    return os.homedir();
+  }
 
   switch (this.__platform || process.platform) {
     case 'win32': return path.resolve(processEnv.USERPROFILE);
-    default: return path.resolve(processEnv.HOME)
+    default: return path.resolve(processEnv.HOME);
   }
 }
 
-function tmp () {
+function tmp() {
   switch (this.__platform || process.platform) {
     case 'win32': return path.resolve(processEnv.TEMP);
     default: return path.resolve('/tmp');
@@ -43,7 +45,7 @@ var ospath = {
   data: data,
   desktop: desktop,
   home: home,
-  tmp: tmp
+  tmp: tmp,
 };
 
 module.exports = ospath;
--- src/js/redux/configureStore.js
+++ src/js/redux/configureStore.js
@@ -2,50 +2,24 @@ import { createStore, applyMiddleware } from 'redux';
 import { enableBatching } from 'redux-batched-actions';
 import thunkMiddleware from 'redux-thunk';
 import promise from 'redux-promise';
-import { createLogger } from 'redux-logger';
+// import { createLogger } from 'redux-logger';
 import rootReducers from '../reducers/index.js';
+// import {stringifySafe} from "../helpers/FeedbackHelpers";
 
 let middlewares = [
   thunkMiddleware,
   promise,
 ];
 
-// TRICKY: this is to limit nesting in logging to prevent crashing console.log()
-const maxStateLevel = 4; // maximum depth for state logging, can set up to 5 (warning this will run more slowly and consume more memory)
-const showFullDepth = true; // set this to true to display deep objects as JSON strings rather than ellipsis (warning this will run more slowly and consume more memory)
-const stateTransformer = (state, level = maxStateLevel) => {
-  if (level <= 0) {
-    try {
-      return showFullDepth ? JSON.stringify(state) : "…"; // at this point replace with string to protect console.log() from objects too deep
-    } catch (e) {
-      return `Crash converting to JSON: ${e.toString()}`;
-    }
-  }
-
-  let newState = {};
-  let keys = (typeof state === "object" && state !== null && Object.keys(state)) || [];
-
-  if (keys.length) {
-    for (let i = 0, l = keys.length; i < l; i++) {
-      const key = keys[i];
-      newState[key] = stateTransformer(state[key], level - 1);
-    }
-  } else {
-    newState = state;
-  }
-
-  return newState;
-};
-
 if (process.env.REDUX_LOGGER || process.env.NODE_ENV === 'development') {
-  middlewares.push(createLogger(
-    {
-      diff: false,
-      logErrors: false, // disable catch and rethrow of errors in redux-logger
-      stateTransformer,
-      actionTransformer: stateTransformer,
-    }
-  ));
+  //TODO: this is a hack to keep redux logger from crashing, but still saw crash when opened project
+  // middlewares.push(createLogger(
+  //   {
+  //     diff: false,
+  //     stateTransformer: (state) => (stringifySafe(state,
+  //       '[error loading system information]'))
+  //   }
+  // ));
 }
 
 export default function configureStore(persistedState) {
