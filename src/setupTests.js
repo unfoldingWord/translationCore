d5de844f7817ae6a6e5ccfdc8ba2aabad50fd17a
mannycolon <colonmanuel7@gmail.com>
2020-03-18 15:04:47 -0400

    Updated src/setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,3 +2,6 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+global.requestAnimationFrame = (callback) => {
+  setTimeout(callback, 0);
+};
