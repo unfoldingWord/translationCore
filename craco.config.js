aaa6b5f0bcd793ac75057e7db6c9cc643985112c
Manny Colon <colonmanuel7@gmail.com>
2020-04-09 11:20:36 -0400

    Added source code mapping for easier debugging  (#6835)
    * Upgraded to latest craco
    
    * Added source code mapping on development, removed .env copy pattern from dev mode

--- craco.config.js
+++ craco.config.js
@@ -1,25 +1,37 @@
 const { ESLINT_MODES } = require('@craco/craco');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  webpack: {
-    configure: {
-      target: 'electron-renderer',
-      node: {
+module.exports = (mode) => {
+  console.log('Environment:', mode.env);
+  const isProduction = mode.env === 'production';
+  const copyPatterns = [
+    { from: './tcResources', to: 'static/tCResources' },
+    { from: './src/tC_apps', to: 'static/tC_apps' },
+    { from: './src/locale', to: 'static/locale' },
+    { from: './package.json', to: 'package.json' },
+  ];
+
+  if (isProduction) {
+    copyPatterns.push({ from: '.env', to: './' });
+  }
+
+  return {
+    webpack: {
+      configure: {
+        target: 'electron-renderer',
+        // eval-source-map allows to set inline breakpoints, and step debug at statement-level.
+        devtool: isProduction ? undefined : 'eval-source-map',
+        node: {
         // TRICKY: don't let webpack hard-code these
-        __dirname: false,
-        __filename: false,
+          __dirname: false,
+          __filename: false,
+        },
+        plugins: [
+          new CopyWebpackPlugin(copyPatterns),
+        ],
       },
-      plugins: [
-        new CopyWebpackPlugin([
-          { from: './tcResources', to: 'static/tCResources' },
-          { from: './src/tC_apps', to: 'static/tC_apps' },
-          { from: './src/locale', to: 'static/locale' },
-          { from: './package.json', to: 'package.json' },
-          { from: '.env', to: './' },
-        ]),
-      ],
     },
-  },
-  eslint: { mode: ESLINT_MODES.file },
+    eslint: { mode: ESLINT_MODES.file },
+  };
 };
+
