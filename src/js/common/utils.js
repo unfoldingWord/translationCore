b94258a84f450d5afa61f4b9b79418b22a69989e
Bruce McLean 1 <photonomad1@noreply.door43.org>
2020-04-29 13:50:49 -0400

    memory logging

--- src/js/common/utils.js
+++ src/js/common/utils.js
@@ -8,20 +8,23 @@ export function delay(ms) {
 
 // noinspection JSUnusedLocalSymbols
 export async function logMemory(title, intial = false) {
-  const emphasis = '############\n';
   const localProcess = process;
-  let output = `\n\n${emphasis} ${title}\nlogMemory()\n`;
-  const remoteProcess = electron && electron.remote && electron.remote.process;
 
-  if (remoteProcess) {
-    output += `electron.remote.process.getHeapStatistics() = ${JSON.stringify(remoteProcess.getHeapStatistics(), null, 2)}\n`;
-    output += `electron.remote.process.getSystemMemoryInfo() = ${JSON.stringify(remoteProcess.getSystemMemoryInfo(), null, 2)}\n`;
-    output += `process.getHeapStatistics() = ${JSON.stringify(localProcess.getHeapStatistics(), null, 2)}\n`;
-    output += `process.getSystemMemoryInfo() = ${JSON.stringify(localProcess.getSystemMemoryInfo(), null, 2)}\n`;
+  if (localProcess.getProcessMemoryInfo) {
+    const emphasis = '############\n';
+    let output = `\n\n${emphasis} ${title}\nlogMemory()\n`;
+    const remoteProcess = electron && electron.remote && electron.remote.process;
+
+    if (remoteProcess) {
+      output += `electron.remote.process.getHeapStatistics() = ${JSON.stringify(remoteProcess.getHeapStatistics(), null, 2)}\n`;
+      output += `electron.remote.process.getSystemMemoryInfo() = ${JSON.stringify(remoteProcess.getSystemMemoryInfo(), null, 2)}\n`;
+      output += `process.getHeapStatistics() = ${JSON.stringify(localProcess.getHeapStatistics(), null, 2)}\n`;
+      output += `process.getSystemMemoryInfo() = ${JSON.stringify(localProcess.getSystemMemoryInfo(), null, 2)}\n`;
+    }
+
+    const processMemoryInfo = await localProcess.getProcessMemoryInfo();
+    output += `process.getProcessMemoryInfo() = ${JSON.stringify(processMemoryInfo, null, 2)}\n`;
+    output += emphasis + '\n';
+    console.log(output);
   }
-
-  const processMemoryInfo = await localProcess.getProcessMemoryInfo();
-  output += `process.getProcessMemoryInfo() = ${JSON.stringify(processMemoryInfo, null, 2)}\n`;
-  output += emphasis + '\n';
-  console.log(output);
 }
