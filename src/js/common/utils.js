3e4ec9f063f4334522f4af4f254682ddde8c3a98
Bruce McLean 1 <photonomad1@noreply.door43.org>
2020-04-29 13:34:51 -0400

    memory logging

--- src/js/common/utils.js
+++ src/js/common/utils.js
@@ -8,12 +8,16 @@ export function delay(ms) {
 
 // noinspection JSUnusedLocalSymbols
 export async function logMemory(title, intial = false) {
-  const localProcess = process;
-  const process_ = electron.remote.process;
   const emphasis = '############\n';
+  const localProcess = process;
   let output = `\n\n${emphasis} ${title}\nlogMemory()\n`;
-  output += `electron.remote.process.getHeapStatistics() = ${JSON.stringify(process_.getHeapStatistics(), null, 2)}\n`;
-  output += `electron.remote.process.getSystemMemoryInfo() = ${JSON.stringify(process_.getSystemMemoryInfo(), null, 2)}\n`;
+  const remoteProcess = electron && electron.remote && electron.remote.process;
+
+  if (remoteProcess) {
+    output += `electron.remote.process.getHeapStatistics() = ${JSON.stringify(remoteProcess.getHeapStatistics(), null, 2)}\n`;
+    output += `electron.remote.process.getSystemMemoryInfo() = ${JSON.stringify(remoteProcess.getSystemMemoryInfo(), null, 2)}\n`;
+  }
+
   const processMemoryInfo2 = await localProcess.getProcessMemoryInfo();
   output += `process.getProcessMemoryInfo() = ${JSON.stringify(processMemoryInfo2, null, 2)}\n`;
   output += emphasis + '\n';
