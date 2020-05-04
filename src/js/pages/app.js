55a3c090620c8f438093df7033d8fd90f62b4af9
mannycolon <colonmanuel7@gmail.com>
2020-05-04 19:01:04 -0400

    Removed console log

--- src/js/pages/app.js
+++ src/js/pages/app.js
@@ -36,10 +36,6 @@ import {
 } from '../common/constants';
 import ConfirmationDialog from '../middleware/confirmation/ConfirmationDialog';
 
-console.log('====================================');
-console.log('process.env.PUBLIC_URL', process.env.PUBLIC_URL);
-console.log('====================================');
-
 if (process.env.NODE_ENV === 'production') {
   const version = `v${APP_VERSION} (${getBuild()})`;
   injectFileLogging(LOG_FILES_PATH, version);
