1033754586901e755a652b42afad42fcefc2bd3e
Bruce McLean <mcleanb@photo-nomad.net>
2023-02-22 07:48:09 -0500

    support universal builds

--- gulpfile.js
+++ gulpfile.js
@@ -400,6 +400,48 @@ gulp.task('release-macos', () => {
  * Releases a macOS build
  * @param out - the path to which the release will be saved.
  */
+gulp.task('release-macos-universal', () => {
+  const p = require('./package');
+  const exec = require('child_process').exec;
+  const isLinux = /^linux/.test(process.platform);
+  const isMacOS = /^darwin/.test(process.platform);
+
+  const outPath = argv.out;
+
+  if (!outPath || typeof outPath !== 'string') {
+    throw new Error('The --out argument is required.');
+  }
+
+  if (!isLinux && !isMacOS) {
+    throw new Error('You must be on Linux or macOS to create macOS releases');
+  }
+
+  mkdirp.sync('release');
+  const buildPath = BUILD_DIR + p.name + '-darwin-universal/';
+
+  if (!fs.existsSync(buildPath)) {
+    throw new Error(`The build path "${buildPath}" does not exist`);
+  }
+
+  return new Promise((resolve, reject) => {
+    const dest = path.normalize(outPath);
+    mkdirp.sync(path.dirname(dest));
+    let cmd = `scripts/osx/makedmg.sh "${p.name}" ${buildPath} ${dest}`;
+    console.log(cmd);
+    exec(cmd, function (err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+});
+
+/**
+ * Releases a macOS build
+ * @param out - the path to which the release will be saved.
+ */
 gulp.task('release-macos-arm64', () => {
   const p = require('./package');
   const exec = require('child_process').exec;
