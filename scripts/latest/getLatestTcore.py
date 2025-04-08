58019ccf618ee36020313908d9f163face74c076
yakob-aleksandrovich <jakobaleksandrovich@gmail.com>
2025-04-08 13:43:34 +0200

    Fixed another small display issue.

--- scripts/latest/getLatestTcore.py
+++ scripts/latest/getLatestTcore.py
@@ -199,8 +199,8 @@ def getStr(key):
   return keyToText.get(key, key)
 
 
-def json_to_html(json_data, key):
-  html = f'\n<p class="" style="margin: 0px; color: black; line-height: 1.3em; font-weight: 400;">\n<strong>{getStr(key)}</strong>\n'
+def json_to_html(json_data, key, style_extra=''):
+  html = f'\n<p class="" style="margin: 0px; color: black; line-height: 1.3em; font-weight: 400; {style_extra}">\n<strong>{getStr(key)}</strong>\n'
 
   for os in ["win", "macos", "linux"]:
     # for os, arch_data in json_data.items():
@@ -228,7 +228,7 @@ installers = {
 # json_data = json.dumps(installers, indent=4)
 # print(json_data)
 
-html = json_to_html(max_installers, 'max')
+html = json_to_html(max_installers, 'max', style_extra='margin-top: -30px;')
 print (html)
 print ('<br>')
 
